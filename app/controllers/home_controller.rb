53e17209dc977460a913e306a64a7a85fdb05df9
Eric-Guo <eric.guocz@gmail.com>
2019-04-28 16:54:39 +0800

    Show sidebar in home page.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,15 @@
 
 class HomeController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_page_layout_data, if: -> { request.format.html? }
 
   def index
+  end
+
+  protected
+
+  def set_page_layout_data
+    prepare_meta_tags title: t("home.show.title")
+    @_sidebar_name = "application"
   end
 end
