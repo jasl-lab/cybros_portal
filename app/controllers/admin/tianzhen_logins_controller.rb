ed212f2ebfff1cf40162e16b8a562dbadc7ad1c5
Eric-Guo <eric.guocz@gmail.com>
2019-05-07 17:38:49 +0800

    Show last 500 line of logs.

--- app/controllers/admin/tianzhen_logins_controller.rb
+++ app/controllers/admin/tianzhen_logins_controller.rb
@@ -3,6 +3,20 @@ class Admin::TianzhenLoginsController < Admin::ApplicationController
 
   def index
     prepare_meta_tags title: t(".title")
+    last_500_lines = `tail -n 500 /mnt/tianzhen_log/天正软件.log`.encode('gbk','utf-8',{:invalid => :replace, :undef => :replace, :replace => '?'})
+    @array = []
+    r = Regexp.new('^(\d+\-\d+\-\d+\s\d+\:\d+\:\d+)\s\[(\w*)\]\s(\d+\.\d+\.\d+\.\d+)\s(\w+@\w+)\s(.*)$')
+    last_500_lines.split("\n").each do |l|
+      r.match(l) do |m|
+        time = m[1]
+        action = m[2]
+        ip = m[3]
+        pc = m[4]
+        line = m[5]
+        @array << [time, action, ip, pc, line]
+      end
+    end
+    @array = @array.reverse
   end
 
   private
