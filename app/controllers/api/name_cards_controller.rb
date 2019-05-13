f3e61e3b7008ed2aa0adb90d8af19639ff37199f
Eric-Guo <eric.guocz@gmail.com>
2019-05-13 17:36:31 +0800

    Add is_success as json return.

--- app/controllers/api/name_cards_controller.rb
+++ app/controllers/api/name_cards_controller.rb
@@ -2,7 +2,7 @@ module Api
   class NameCardsController < ActionController::API
     def create
       name_card_apply = NameCardApply.find_by id: params[:name_card_id]
-      return render json: { error_message: '找不到记录' }, status: :not_found unless name_card_apply.present?
+      return render json: { is_success: 404, error_message: '找不到记录' }, status: :not_found unless name_card_apply.present?
 
       name_card_apply.bpm_message = params[:message]
       if params[:approval_result].to_i == 1
@@ -11,9 +11,9 @@ module Api
         name_card_apply.status = '否决'
       end
       if name_card_apply.save
-        head :no_content, status: :ok
+        render json: { is_success: true }, status: :ok
       else
-        render json: { error_message: name_card_apply.errors.full_messages, status: :bad_request }
+        render json: { is_success: 400, error_message: name_card_apply.errors.full_messages, status: :bad_request }
       end
     end
   end
