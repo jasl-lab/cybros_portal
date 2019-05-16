01a22cb09ea073311057e3b1cb9c6d549b07c296
Eric-Guo <eric.guocz@gmail.com>
2019-05-16 19:52:16 +0800

    Review office address.

--- app/models/department.rb
+++ app/models/department.rb
@@ -3,10 +3,9 @@ class Department < ApplicationRecord
   has_many :users, through: :department_users
 
   def self.office_addresses
-    ['上海市徐汇区中山西路1800号兆丰环球大厦',
-     '上海市钦州北路1188号漕河泾科汇大厦',
+    ['上海市徐汇区中山西路1800号 兆丰环球大厦',
+     '上海市钦州北路1188号 漕河泾科汇大厦',
      '上海市柳州路928号 百丽国际广场3楼',
-     '上海市田州路159号',
-     '上海市杨浦区杨树浦路1066号滨江国际广场2号楼']
+     '上海市杨浦区杨树浦路1066号 滨江国际广场2号楼']
   end
 end
