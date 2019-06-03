0249c8940705fe4ab1e6837925f099b5b5508ecf
Eric-Guo <eric.guocz@gmail.com>
2019-06-03 22:29:34 +0800

    New way to report tianzhen login status.

--- app/controllers/report/tianzhen_logins_controller.rb
+++ app/controllers/report/tianzhen_logins_controller.rb
@@ -1,23 +1,21 @@
 class Report::TianzhenLoginsController < ApplicationController
-  before_action :authenticate_user!
   before_action :set_page_layout_data, if: -> { request.format.html? }
   before_action :set_breadcrumbs, only: %i[index], if: -> { request.format.html? }
 
   def index
     prepare_meta_tags title: t(".title")
-    last_500_lines = `tail -n 500 /mnt/tianzhen_log/天正软件.log`.encode('gbk','utf-8',{:invalid => :replace, :undef => :replace, :replace => '?'})
-    @hash = {}
-    r = Regexp.new('^(\d+\-\d+\-\d+\s\d+\:\d+\:\d+)\s\[(\w*)\]\s(\d+\.\d+\.\d+\.\d+)\s(\w+@\w+)\s(.*)$')
-    last_500_lines.split("\n").each do |l|
-      r.match(l) do |m|
-        time = DateTime.parse(m[1])
-        action = m[2]
-        ip = m[3]
-        pc = m[4]
-        line = m[5]
-        hv = @hash.fetch("#{ip} #{pc}", [])
-        hv << [action, time, line]
-        @hash["#{ip} #{pc}"] = hv
+    response = HTTP.get(Rails.application.credentials.tianzhen_admin_web_url!)
+    if response.code == 200
+      html_doc = Nokogiri::HTML(response.body.to_s)
+      trs = html_doc.xpath('//*[@id="content_middle"]/table/tbody/tr')
+      @rows = []
+      trs.each do |tr|
+        idx = tr.elements[0].content.strip
+        user = tr.elements[1].content.strip
+        ip = tr.elements[2].content.strip
+        login_time = DateTime.parse(tr.elements[3].content.strip)
+        last_access_time =  DateTime.parse(tr.elements[4].content.strip)
+        @rows << [idx, user, ip, login_time, last_access_time]
       end
     end
   end
