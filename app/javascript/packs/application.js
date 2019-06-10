fea6fde56a1397a46a036dd32d1d3521a4470217
Eric-Guo <eric.guocz@gmail.com>
2019-06-10 10:13:02 +0800

    no core.

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,6 @@ const importAll = (r) => r.keys().map(r)
 importAll(require.context('images', false, /\.(png|jpe?g|svg)$/i));
 //importAll(require.context('@fortawesome/fontawesome-free/webfonts', false, /\.(eot|svg|ttf|woff2?)$/i));
 
-import "core-js/stable";
 import "regenerator-runtime/runtime";
 import "@stimulus/polyfills";
 
