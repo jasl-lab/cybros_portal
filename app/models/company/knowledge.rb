98d21378bdd59a1dbf953a55e7bd6ac3dc24664c
Eric-Guo <eric.guocz@gmail.com>
2019-06-11 18:07:58 +0800

    Add synonym dict support.

--- app/models/company/knowledge.rb
+++ app/models/company/knowledge.rb
@@ -5,7 +5,9 @@ module Company
     def self.answer(question)
       question_word = Current.jieba_keyword.extract(question, 2)
       qw1 = question_word.collect(&:first).first
+      qw1 = user_synonym.fetch(qw1, qw1)
       qw2 = question_word.collect(&:first).second
+      qw2 = user_synonym.fetch(qw2, qw2)
       ans = if qw2.present?
         Company::Knowledge.where('question LIKE ?', "%#{qw1}%#{qw2}%").or(Company::Knowledge.where('question LIKE ?', "%#{qw2}%#{qw1}%"))
       else
@@ -22,6 +24,20 @@ module Company
       else
         Company::Knowledge.where('question LIKE ?', "%#{qw1}%").first
       end
+    end
+
+    def self.user_synonym
+      @@h ||= {}
+      return @@h if @@h.present?
+      File.readlines(Rails.root.join('config', 'synonym.dict.utf8')).each do |l|
+        words = l.strip.split(' ')
+        to_change_word = words[0]
+        words.each_with_index do |w, i|
+          next if i == 0
+          @@h[w] = to_change_word
+        end
+      end
+      @@h
     end
   end
 end
