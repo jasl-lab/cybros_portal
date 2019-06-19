a9c9eafa87aa2262a365a3de7bcebcf68affb82e
Eric-Guo <eric.guocz@gmail.com>
2019-06-19 15:18:13 +0800

    Add 公司应收款

--- app/controllers/report/subsidiary_receives_controller.rb
+++ app/controllers/report/subsidiary_receives_controller.rb
@@ -19,9 +19,23 @@ class Report::SubsidiaryReceivesController < ApplicationController
       .where.not(businessltdname: '上海天华建筑设计有限公司')
       .select('businessltdname, SUM(real_receive) real_receive')
       .group(:businessltdname)
-    @all_company_names = @real_data.collect(&:businessltdname)
-    @all_company_short_names = @all_company_names.collect { |c| Bi::StaffCount.company_short_names.fetch(c, c) }
+    @real_company_names = @real_data.collect(&:businessltdname)
+    @real_company_short_names = @real_company_names.collect { |c| Bi::StaffCount.company_short_names.fetch(c, c) }
     @real_receives = @real_data.collect { |d| (d.real_receive/10000.0).round(0) }
+
+    @need_data = if current_user_companies.include?('上海天华建筑设计有限公司')
+      Bi::SubCompanyNeedReceive.all
+    else
+      Bi::SubCompanyNeedReceive.where(businessltdname: current_user_companies)
+    end.where.not(businessltdname: '上海天华建筑设计有限公司') # .where('date <= ?', @end_of_month)
+      .select('businessltdname, SUM(unsign_receive) unsign_receive, SUM(sign_receive) sign_receive, SUM(long_account_receive) long_account_receive, SUM(short_account_receive) short_account_receive')
+      .group(:businessltdname)
+    @need_company_names = @need_data.collect(&:businessltdname)
+    @need_company_short_names = @need_company_names.collect { |c| Bi::StaffCount.company_short_names.fetch(c, c) }
+    @need_long_account_receives = @need_data.collect { |d| (d.long_account_receive/10000.0).round(0) }
+    @need_short_account_receives = @need_data.collect { |d| (d.short_account_receive/10000.0).round(0) }
+    @need_should_receives = @need_data.collect { |d| ((d.unsign_receive.to_f+d.sign_receive.to_f)/10000.0).round(0) }
+
   end
 
   private
--- app/javascript/controllers/subsidiary_receives_controller.js
+++ app/javascript/controllers/subsidiary_receives_controller.js
@@ -1,16 +1,24 @@
 import { Controller } from "stimulus"
 
 let subsidiaryRealReceivesChart;
+let subsidiaryNeedReceivesChart;
 
 export default class extends Controller {
   connect() {
     subsidiaryRealReceivesChart = echarts.init(document.getElementById('subsidiary-real-receives-chart'));
 
-var xAxisData = JSON.parse(this.data.get("x_axis"));
+var realXAxisData = JSON.parse(this.data.get("real_x_axis"));
 var realReceives = JSON.parse(this.data.get("real_receives"));
 var staffRealReceiveRef = this.data.get("staff_real_receive_ref");
 
-var option = {
+    subsidiaryNeedReceivesChart = echarts.init(document.getElementById('subsidiary-need-receives-chart'));
+
+var needXAxisData = JSON.parse(this.data.get("need_x_axis"));
+var needLongAccountReceives = JSON.parse(this.data.get("need_long_account_receives"));
+var needShortAccountReceives = JSON.parse(this.data.get("need_short_account_receives"));
+var needShouldReceives = JSON.parse(this.data.get("need_should_receives"));
+
+var real_option = {
     legend: {
         data: ['本年累积实收款（万元）'],
         align: 'left'
@@ -31,7 +39,7 @@ var option = {
     },
     tooltip: {},
     xAxis: {
-      data: xAxisData,
+      data: realXAxisData,
       silent: true,
       axisLabel: {
         interval: 0,
@@ -62,17 +70,110 @@ var option = {
     }]
 };
 
-    subsidiaryRealReceivesChart.setOption(option, false);
+var need_option = {
+    legend: {
+        data: ['超长帐龄','扣除超长帐龄以外的财务应收','业务应收款'],
+        align: 'left'
+    },
+    tooltip: {
+      trigger: 'axis',
+      axisPointer: {
+        type: 'cross'
+      }
+    },
+    grid: {
+      left: 70,
+      right: 110,
+      top: 50,
+      bottom: 60
+    },
+    toolbox: {
+      feature: {
+        dataView: {},
+        saveAsImage: {
+            pixelRatio: 2
+        }
+      }
+    },
+    xAxis: {
+      data: needXAxisData,
+      silent: true,
+      axisLabel: {
+        interval: 0,
+        rotate: -40
+      },
+      splitLine: {
+          show: false
+      }
+    },
+    yAxis: [{
+      type: 'value',
+      name: '应收款（万元）',
+      position: 'left',
+      axisLabel: {
+        formatter: '{value}万'
+      }
+    }],
+    series: [{
+      name: '超长帐龄',
+      type: 'bar',
+      stack: '应收',
+      data: needLongAccountReceives,
+      itemStyle: {
+        color: '#FA9291'
+      },
+      label: {
+        normal: {
+          show: true,
+          position: 'insideTop'
+        }
+      }
+    },{
+      name: '扣除超长帐龄以外的财务应收',
+      type: 'bar',
+      stack: '应收',
+      data: needShortAccountReceives,
+      itemStyle: {
+        color: '#A1D189'
+      },
+      label: {
+        normal: {
+          show: true,
+          position: 'insideTop'
+        }
+      }
+    },{
+      name: '业务应收款',
+      type: 'bar',
+      stack: '应收',
+      data: needShouldReceives,
+      itemStyle: {
+        color: '#738496'
+      },
+      label: {
+        normal: {
+          show: true,
+          position: 'top'
+        }
+      }
+    }]
+};
+
+    subsidiaryRealReceivesChart.setOption(real_option, false);
+    subsidiaryNeedReceivesChart.setOption(need_option, false);
     setTimeout(() => {
       subsidiaryRealReceivesChart.resize();
+      subsidiaryNeedReceivesChart.resize();
     }, 200);
   }
 
   layout() {
     subsidiaryRealReceivesChart.resize();
+    subsidiaryNeedReceivesChart.resize();
   }
 
   disconnect() {
     subsidiaryRealReceivesChart.dispose();
+    subsidiaryNeedReceivesChart.dispose();
   }
 }
