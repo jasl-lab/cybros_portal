09627f4d5c7e4bc011fb4b3cc8cc5de1c2861bb2
Eric-Guo <eric.guocz@gmail.com>
2019-06-19 15:45:03 +0800

    Possible have two same company name.

--- app/policies/bi/base_policy.rb
+++ app/policies/bi/base_policy.rb
@@ -2,7 +2,7 @@ module Bi
   class BasePolicy < ApplicationPolicy
     def show?
       %(冯巧容 王玥 崔立杰 亢梦婕 付焕鹏 曾嵘 过纯中).include?(user.chinese_name) && \
-        user.departments.pluck(:company_name) == ['上海天华建筑设计有限公司']
+        user.departments.pluck(:company_name).uniq == ['上海天华建筑设计有限公司']
     end
 
     def export?
