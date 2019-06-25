199494d5fe62f7316606101f6c3cc45374549a4a
Eric-Guo <eric.guocz@gmail.com>
2019-06-25 14:15:35 +0800

    Display month and year only.

--- app/controllers/report/yearly_subsidiary_workloadings_controller.rb
+++ app/controllers/report/yearly_subsidiary_workloadings_controller.rb
@@ -18,7 +18,7 @@ class Report::YearlySubsidiaryWorkloadingsController < ApplicationController
     beginning_of_month = Date.parse(all_month_names.first).beginning_of_month
     end_of_month = Date.parse(all_month_names.last).end_of_month
     data = Bi::WorkHoursCountOrg.where(date: beginning_of_month..end_of_month, businessltdname: @selected_company_name)
-    @dates = data.collect(&:date)
+    @dates = data.collect { |d| d.date.to_s(:month_and_year) }
     @day_rate = data.collect { |d| ((d.date_real / d.date_need.to_f) * 100).round(2) rescue 0 }
     @day_rate_ref = params[:day_rate_ref] || 90
     @planning_day_rate = data.collect { |d| ((d.blue_print_real / d.blue_print_need.to_f) * 100).round(2) rescue 0 }
