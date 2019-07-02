5c187118358b05cd06f651addcb4ee582528e8ae
Eric-Guo <eric.guocz@gmail.com>
2019-07-02 10:29:52 +0800

    New maintainer

--- app/models/user.rb
+++ app/models/user.rb
@@ -35,7 +35,7 @@ class User < ApplicationRecord
   end
 
   def knowledge_maintainer?
-    email == 'chenzifan@thape.com.cn' || %(忻琳 聂玲玲 冯可 季建杰 柳怡帆 许潇红 吴婷 郑贤来 邢侃 袁士捷 张雨).include?(chinese_name)
+    email == 'chenzifan@thape.com.cn' || %(忻琳 聂玲玲 冯可 季建杰 柳怡帆 许潇红 吴婷 郑贤来 邢侃 袁士捷 张雨 冯巧容).include?(chinese_name)
   end
 
   def report_maintainer?
