3303076ee5a2347d44de54d27e8057e4cb7589cc
Eric-Guo <eric.guocz@gmail.com>
2019-07-08 08:17:47 +0800

    refresh schema.

--- db/schema.rb
+++ db/schema.rb
@@ -12,10 +12,38 @@
 
 ActiveRecord::Schema.define(version: 2020_06_04_010332) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+  create_table "action_text_rich_texts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin", force: :cascade do |t|
+    t.string "name", null: false
+    t.text "body", size: :long
+    t.string "record_type", null: false
+    t.bigint "record_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["record_type", "record_id", "name"], name: "index_action_text_rich_texts_uniqueness", unique: true
+  end
 
-  create_table "allowlisted_jwts", force: :cascade do |t|
+  create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "record_type", null: false
+    t.bigint "record_id", null: false
+    t.bigint "blob_id", null: false
+    t.datetime "created_at", null: false
+    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
+    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
+  end
+
+  create_table "active_storage_blobs", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin", force: :cascade do |t|
+    t.string "key", null: false
+    t.string "filename", null: false
+    t.string "content_type"
+    t.text "metadata"
+    t.bigint "byte_size", null: false
+    t.string "checksum", null: false
+    t.datetime "created_at", null: false
+    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "allowlisted_jwts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin", force: :cascade do |t|
     t.string "jti", null: false
     t.string "aud", null: false
     t.datetime "exp", null: false
@@ -24,7 +52,273 @@ ActiveRecord::Schema.define(version: 2020_06_04_010332) do
     t.index ["user_id"], name: "index_allowlisted_jwts_on_user_id"
   end
 
-  create_table "users", force: :cascade do |t|
+  create_table "cad_operations", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin", force: :cascade do |t|
+    t.string "session_id"
+    t.string "cmd_name"
+    t.integer "cmd_seconds"
+    t.json "cmd_data"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "seg_name"
+    t.string "seg_function"
+    t.index ["user_id"], name: "index_cad_operations_on_user_id"
+  end
+
+  create_table "cad_sessions", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin", force: :cascade do |t|
+    t.string "session"
+    t.string "operation"
+    t.string "ip_address"
+    t.string "mac_address"
+    t.bigint "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "begin_operation"
+    t.string "end_operation"
+    t.index ["user_id"], name: "index_cad_sessions_on_user_id"
+  end
+
+  create_table "copy_of_business_license_applies", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.string "employee_name"
+    t.string "clerk_code"
+    t.string "belong_company_name"
+    t.string "belong_department_name"
+    t.string "contract_belong_company"
+    t.string "stamp_to_place"
+    t.string "stamp_comment"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "begin_task_id"
+    t.boolean "backend_in_processing"
+    t.text "bpm_message"
+    t.string "belong_company_code"
+    t.string "belong_department_code"
+    t.string "contract_belong_company_code"
+    t.string "status"
+    t.index ["user_id"], name: "index_copy_of_business_license_applies_on_user_id"
+  end
+
+  create_table "department_users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin", force: :cascade do |t|
+    t.bigint "department_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["department_id"], name: "index_department_users_on_department_id"
+    t.index ["user_id"], name: "index_department_users_on_user_id"
+  end
+
+  create_table "departments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin", force: :cascade do |t|
+    t.string "name", null: false
+    t.integer "position"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "company_name"
+    t.string "dept_code"
+    t.string "company_code"
+  end
+
+  create_table "direct_question_answers", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin", force: :cascade do |t|
+    t.bigint "knowledge_id", null: false
+    t.bigint "direct_question_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["direct_question_id"], name: "index_direct_question_answers_on_direct_question_id"
+    t.index ["knowledge_id"], name: "index_direct_question_answers_on_knowledge_id"
+  end
+
+  create_table "direct_questions", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin", force: :cascade do |t|
+    t.string "question"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "knowledge_likes", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.integer "like_count"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_knowledge_likes_on_user_id"
+  end
+
+  create_table "knowledges", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin", force: :cascade do |t|
+    t.string "category_1"
+    t.string "category_2"
+    t.string "question", collation: "utf8_general_ci"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "category_3"
+    t.boolean "shanghai_only", default: false
+  end
+
+  create_table "name_card_applies", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.string "english_name"
+    t.string "department_name"
+    t.string "en_department_name"
+    t.string "title"
+    t.string "en_title"
+    t.string "phone_ext"
+    t.string "fax_no"
+    t.string "mobile"
+    t.integer "print_out_box_number"
+    t.string "begin_task_id"
+    t.text "bpm_message"
+    t.string "status"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "company_name"
+    t.string "en_company_name"
+    t.string "comment"
+    t.string "office_address"
+    t.string "office_level"
+    t.string "professional_title"
+    t.string "en_professional_title"
+    t.string "chinese_name"
+    t.string "email"
+    t.boolean "backend_in_processing", default: false
+    t.string "thickness"
+    t.string "back_color"
+    t.index ["user_id"], name: "index_name_card_applies_on_user_id"
+  end
+
+  create_table "name_card_black_titles", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin", force: :cascade do |t|
+    t.string "original_title"
+    t.string "required_title"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "name_card_white_titles", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin", force: :cascade do |t|
+    t.string "original_title"
+    t.string "required_title"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "official_stamp_usage_applies", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin", force: :cascade do |t|
+    t.bigint "user_id"
+    t.string "employee_name"
+    t.string "clerk_code"
+    t.string "belong_company_name"
+    t.string "belong_company_code"
+    t.string "belong_department_name"
+    t.string "belong_department_code"
+    t.string "application_class"
+    t.string "stamp_to_place"
+    t.string "stamp_comment"
+    t.string "begin_task_id"
+    t.string "backend_in_processing"
+    t.text "bpm_message"
+    t.string "status"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "application_subclasses", default: "--- []\n"
+    t.index ["user_id"], name: "index_official_stamp_usage_applies_on_user_id"
+  end
+
+  create_table "pending_questions", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.string "question"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.bigint "owner_id"
+    t.index ["owner_id"], name: "index_pending_questions_on_owner_id"
+    t.index ["user_id"], name: "index_pending_questions_on_user_id"
+  end
+
+  create_table "proof_of_employment_applies", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin", force: :cascade do |t|
+    t.bigint "user_id"
+    t.string "employee_name"
+    t.string "clerk_code"
+    t.string "belong_company_name"
+    t.string "belong_department_name"
+    t.string "contract_belong_company"
+    t.string "stamp_to_place"
+    t.string "stamp_comment"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "begin_task_id"
+    t.boolean "backend_in_processing"
+    t.text "bpm_message"
+    t.string "belong_company_code"
+    t.string "belong_department_code"
+    t.string "contract_belong_company_code"
+    t.string "status"
+    t.index ["user_id"], name: "index_proof_of_employment_applies_on_user_id"
+  end
+
+  create_table "proof_of_income_applies", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.string "employee_name"
+    t.string "clerk_code"
+    t.string "belong_company_name"
+    t.string "belong_department_name"
+    t.string "contract_belong_company"
+    t.string "stamp_to_place"
+    t.string "stamp_comment"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "begin_task_id"
+    t.boolean "backend_in_processing"
+    t.text "bpm_message"
+    t.string "belong_company_code"
+    t.string "belong_department_code"
+    t.string "contract_belong_company_code"
+    t.string "status"
+    t.index ["user_id"], name: "index_proof_of_income_applies_on_user_id"
+  end
+
+  create_table "public_rental_housing_applies", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.string "employee_name"
+    t.string "clerk_code"
+    t.string "belong_company_name"
+    t.string "belong_department_name"
+    t.string "contract_belong_company"
+    t.string "stamp_to_place"
+    t.string "stamp_comment"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "begin_task_id"
+    t.boolean "backend_in_processing"
+    t.text "bpm_message"
+    t.string "belong_company_code"
+    t.string "belong_department_code"
+    t.string "contract_belong_company_code"
+    t.string "status"
+    t.index ["user_id"], name: "index_public_rental_housing_applies_on_user_id"
+  end
+
+  create_table "role_users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin", force: :cascade do |t|
+    t.bigint "role_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["role_id"], name: "index_role_users_on_role_id"
+    t.index ["user_id"], name: "index_role_users_on_user_id"
+  end
+
+  create_table "roles", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin", force: :cascade do |t|
+    t.string "role_name"
+    t.boolean "report_viewer"
+    t.boolean "report_reviewer"
+    t.boolean "knowledge_maintainer"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.boolean "cad_session"
+    t.boolean "report_view_all"
+    t.boolean "project_map_viewer"
+    t.boolean "hr_group_rt_reader"
+    t.boolean "hr_subsidiary_rt_reader"
+    t.boolean "hr_report_writer"
+    t.boolean "hr_group_reader"
+    t.boolean "hr_subsidiary_reader"
+    t.boolean "report_company_viewer"
+    t.boolean "report_company_detail_viewer"
+  end
+
+  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
@@ -52,6 +346,11 @@ ActiveRecord::Schema.define(version: 2020_06_04_010332) do
     t.integer "invitations_count", default: 0
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "position_title"
+    t.string "clerk_code"
+    t.string "chinese_name"
+    t.string "desk_phone"
+    t.string "job_level"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["invitation_token"], name: "index_users_on_invitation_token", unique: true
@@ -60,5 +359,30 @@ ActiveRecord::Schema.define(version: 2020_06_04_010332) do
     t.index ["unlock_token"], name: "index_users_on_unlock_token", unique: true
   end
 
+  create_table "wechat_sessions", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin", force: :cascade do |t|
+    t.string "openid", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "greating_time"
+    t.index ["openid"], name: "index_wechat_sessions_on_openid", unique: true
+  end
+
+  create_table "yingjianke_overrun_users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin", force: :cascade do |t|
+    t.datetime "time"
+    t.string "device"
+    t.decimal "stay_timespan", precision: 10
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "allowlisted_jwts", "users", on_delete: :cascade
+  add_foreign_key "cad_operations", "users"
+  add_foreign_key "copy_of_business_license_applies", "users"
+  add_foreign_key "direct_question_answers", "direct_questions"
+  add_foreign_key "direct_question_answers", "knowledges"
+  add_foreign_key "knowledge_likes", "users"
+  add_foreign_key "name_card_applies", "users"
+  add_foreign_key "proof_of_income_applies", "users"
+  add_foreign_key "public_rental_housing_applies", "users"
 end
