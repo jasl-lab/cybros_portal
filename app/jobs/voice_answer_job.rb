401d14609410f944258ac5aaed16b8d04d189f9f
Eric-Guo <eric.guocz@gmail.com>
2019-07-09 15:11:25 +0800

    Log the result to debug.

--- app/jobs/voice_answer_job.rb
+++ app/jobs/voice_answer_job.rb
@@ -8,13 +8,19 @@ class VoiceAnswerJob < ApplicationJob
     out_temp.close
     `ffmpeg -i #{in_voice_file.path} -acodec mp3 -y -ac 1 -ar 16000 #{out_temp.path}`
     upload_res = Wechat.api(:svca).addvoicetorecofortext(voice_id, File.open(out_temp.path))
+    Rails.logger.debug "VoiceAnswerJob addvoicetorecofortext: #{upload_res}"
     if upload_res['errcode'] == 0
       res = Wechat.api(:svca).queryrecoresultfortext(voice_id)
+      Rails.logger.debug "VoiceAnswerJob queryrecoresultfortext: #{res}"
       question = res['result']
       ask_user = User.find user_id
       if ask_user.present?
         openid = ask_user.email.split('@')[0]
-        Wechat.api.custom_message_send Wechat::Message.to(openid).text(question)
+        if question.present?
+          Wechat.api.custom_message_send Wechat::Message.to(openid).text(question)
+        else
+          Wechat.api.custom_message_send Wechat::Message.to(openid).text("微信聆听没听出来。。(#{voice_id})")
+        end
       end
     end
   end
