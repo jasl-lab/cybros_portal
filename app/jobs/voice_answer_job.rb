a81e8ec74e9aa0d91669a250802fde28bc7bc9e2
Eric-Guo <eric.guocz@gmail.com>
2019-07-09 13:34:37 +0800

    do convert.

--- app/jobs/voice_answer_job.rb
+++ app/jobs/voice_answer_job.rb
@@ -3,5 +3,19 @@ class VoiceAnswerJob < ApplicationJob
 
   def perform(voice_id, user_id)
     Rails.logger.debug "VoiceAnswerJob got: #{voice_id} and #{user_id}"
+    in_voice_file = Wechat.api.media(voice_id)
+    out_temp = Tempfile.new(['UserVoice', '.mp3'])
+    out_temp.close
+    `ffmpeg -i #{in_voice_file.path} -acodec mp3 -y -ac 1 -ar 16000 #{out_temp.path}`
+    upload_res = Wechat.api(:svca).addvoicetorecofortext(voice_id, File.open(out_temp.path))
+    if upload_res['errcode'] == 0
+      res = Wechat.api(:svca).queryrecoresultfortext(voice_id)
+      question = res['result']
+      ask_user = User.find user_id
+      if ask_user.present?
+        openid = ask_user.email.split('@')[0]
+        Wechat.api.custom_message_send Wechat::Message.to(openid).text(question)
+      end
+    end
   end
 end
