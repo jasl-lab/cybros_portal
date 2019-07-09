83327803c9d289c4b81976d15f56adabd23e8583
Eric-Guo <eric.guocz@gmail.com>
2019-07-09 17:19:45 +0800

    Make is prompt faster.

--- config/sidekiq.rb
+++ config/sidekiq.rb
@@ -1,4 +1,5 @@
 Sidekiq.configure_server do |config|
+  config.poll_interval = 2
   config.redis = { url: "redis://#{Rails.application.secrets.redis_server!}:#{Rails.application.secrets.redis_port!}/#{Rails.application.secrets.redis_db_num!}" }
 end
 
