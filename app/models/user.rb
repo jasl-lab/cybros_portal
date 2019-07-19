d48e102c7b3359e7509eb820d1d96c5449ae0872
Eric-Guo <eric.guocz@gmail.com>
2019-07-19 09:22:30 +0800

    Hide login form and register for productions.

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :timeoutable and :omniauthable
   devise :database_authenticatable,
-         :registerable, :lockable, :invitable,
+         :lockable, :invitable,
          :recoverable, :rememberable, :confirmable, :trackable, :validatable,
          :jwt_authenticatable, jwt_revocation_strategy: self
 
