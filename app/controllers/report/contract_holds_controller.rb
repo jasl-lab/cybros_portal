e3e4276cd0ba01c94cc5cc25247db4b2a397e8ee
Eric-Guo <eric.guocz@gmail.com>
2019-07-24 09:49:16 +0800

    Display words for contract holds.

--- app/controllers/report/contract_holds_controller.rb
+++ app/controllers/report/contract_holds_controller.rb
@@ -7,9 +7,9 @@ class Report::ContractHoldsController < Report::BaseController
   after_action :cors_set_access_control_headers, if: -> { params[:in_iframe].present? }
 
   def show
-    last_available_date = Bi::ContractHold.last_available_date
+    @last_available_date = Bi::ContractHold.last_available_date
 
-    data = policy_scope(Bi::ContractHold).where(date: last_available_date)
+    data = policy_scope(Bi::ContractHold).where(date: @last_available_date)
       .select('projectitemdeptcode, SUM(busiretentcontract) busiretentcontract, SUM(busiretentnocontract) busiretentnocontract')
       .group(:projectitemdeptcode)
 
@@ -22,16 +22,21 @@ class Report::ContractHoldsController < Report::BaseController
     end
 
     @biz_retent_contract = only_have_data_dept.collect do |dept_code|
-      d = data.find { |d| d.projectitemdeptcode == dept_code }
+      d = data.find { |c| c.projectitemdeptcode == dept_code }
       (d.busiretentcontract / 10000.to_f).round(0)
     end
     @biz_retent_no_contract = only_have_data_dept.collect do |dept_code|
-      d = data.find { |d| d.projectitemdeptcode == dept_code }
+      d = data.find { |c| c.projectitemdeptcode == dept_code }
       (d.busiretentnocontract.to_f / 10000.to_f).round(0)
     end
 
-    @biz_retent_totals = @biz_retent_contract.zip(@biz_retent_no_contract).map { |d| d[0]+d[1] }
-
+    @biz_retent_totals = @biz_retent_contract.zip(@biz_retent_no_contract).map { |d| d[0] + d[1] }
+    @biz_retent_totals_sum = @biz_retent_contract.sum()
+    @biz_retent_totals_sum_per_staff = @biz_retent_totals_sum /
+      (@deptnames_in_order.inject(0) do |sum, deptname|
+        staff_per_company = Bi::StaffCount.staff_per_company
+        sum + staff_per_company.fetch(deptname, 1000)
+      end).to_f
   end
 
   private
