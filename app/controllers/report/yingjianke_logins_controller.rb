72193f94e2e30de5415aea2de6475c76b9ef9e25
Eric-Guo <eric.guocz@gmail.com>
2019-07-31 08:55:33 +0800

    Better logic to count the yjk users

--- app/controllers/report/yingjianke_logins_controller.rb
+++ app/controllers/report/yingjianke_logins_controller.rb
@@ -5,22 +5,11 @@ class Report::YingjiankeLoginsController < Report::BaseController
   def index
     prepare_meta_tags title: t(".title")
     @rows = YingjiankeOverrunUser.rows.sort { |a, b| (b[2] - b[1]) <=> (a[2] - a[1]) }
-    if DateTime.now.new_offset('+8:00').hour >= 8
-      @user_num = Array.new((DateTime.now.new_offset('+8:00').hour - 8) / 2 + 1, [0, 0, 0])
-    else
-      @user_num = []
-    end
-    YingjiankeOverrunUser.all.each do |u|
-      if u.time.in_time_zone.to_date === DateTime.now.new_offset('+8:00').to_date
-        if u.stay_timespan >= 18
-          @user_num[(u.time.in_time_zone.hour - 8)/2][2] += 1
-        elsif u.stay_timespan >= 12
-          @user_num[(u.time.in_time_zone.hour - 8)/2][1] += 1
-        elsif u.stay_timespan >= 6
-          @user_num[(u.time.in_time_zone.hour - 8)/2][1] += 1
-        end
-      end
-    end
+    @user_8am = count_yjk_user(Time.current.beginning_of_day + 8 * 60 * 60)
+    @user_10am = count_yjk_user(Time.current.beginning_of_day + 10 * 60 * 60)
+    @user_12am = count_yjk_user(Time.current.beginning_of_day + 12 * 60 * 60)
+    @user_14pm = count_yjk_user(Time.current.beginning_of_day + 14 * 60 * 60)
+    @user_17pm = count_yjk_user(Time.current.beginning_of_day + 17 * 60 * 60)
   end
 
   def export
@@ -43,6 +32,20 @@ class Report::YingjiankeLoginsController < Report::BaseController
   end
 
   private
+
+  def count_yjk_user(time)
+    YingjiankeOverrunUser.where(created_at: (time - 5.minutes)..(time + 5.minutes)).reduce([0, 0, 0]) do |h, yjk|
+      case
+      when yjk.stay_timespan > 18
+        h[2] += 1
+      when yjk.stay_timespan > 12
+        h[1] += 1
+      else
+        h[0] += 1
+      end
+      h
+    end
+  end
 
   def set_breadcrumbs
     @_breadcrumbs = [
