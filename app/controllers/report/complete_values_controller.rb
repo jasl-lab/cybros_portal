8d7839b8a7a197ef2751400b3ebb11508800b52e
Eric-Guo <eric.guocz@gmail.com>
2019-08-16 14:38:27 +0800

    Add summary of the 集团工作完成情况

--- app/controllers/report/complete_values_controller.rb
+++ app/controllers/report/complete_values_controller.rb
@@ -22,7 +22,9 @@ class Report::CompleteValuesController < Report::BaseController
     end
     @all_company_short_names = @all_company_names.collect { |c| Bi::StaffCount.company_short_names.fetch(c, c) }
     @complete_value_totals = @data.collect { |d| (d.sum_total / 10000).round(0) }
+    @sum_complete_value_totals = (@complete_value_totals.sum / 10000.0).round(1)
     @complete_value_year_totals = @complete_value_totals.collect { |d| (d / (@end_of_month.month / 12.0)).round(0) }
+    @sum_complete_value_year_totals = (@complete_value_year_totals.sum / 10000.0).round(1)
     @staff_per_company = Bi::StaffCount.staff_per_short_company_name
     @complete_value_totals_per_staff = @data.collect do |d|
       company_name = Bi::PkCodeName.mapping2orgcode.fetch(d.orgcode, d.orgcode)
@@ -30,7 +32,9 @@ class Report::CompleteValuesController < Report::BaseController
       staff_number = @staff_per_company.fetch(short_name, 1000)
       (d.sum_total / (staff_number * 10000).to_f).round(0)
     end
+    @avg_complete_value_totals_per_staff = (@complete_value_totals_per_staff.sum.to_f / @complete_value_totals_per_staff.size).round(0)
     @complete_value_year_totals_per_staff = @complete_value_totals_per_staff.collect { |d| (d / (@end_of_month.month / 12.0)).round(0) }
+    @avg_complete_value_year_totals_per_staff = (@complete_value_year_totals_per_staff.sum.to_f / @complete_value_year_totals_per_staff.size).round(0)
   end
 
   private
