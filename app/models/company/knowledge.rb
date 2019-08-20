c23a05a89679189d249d5eeb3303b02a82bcfee1
Eric-Guo <eric.guocz@gmail.com>
2019-08-20 08:18:05 +0800

    New knowledge maintain.

--- app/models/company/knowledge.rb
+++ app/models/company/knowledge.rb
@@ -1,6 +1,6 @@
 module Company
   class Knowledge < ApplicationRecord
-    KNOWLEDGE_MAINTAINER = %w(忻琳 聂玲玲 冯可 季建杰 柳怡帆 吴婷 郑贤来 邢侃 袁士捷 张雨 龙默涵 杨琼 季建杰 龚筑媛 吴冬丹).freeze
+    KNOWLEDGE_MAINTAINER = %w(忻琳 聂玲玲 冯可 季建杰 柳怡帆 吴婷 郑贤来 邢侃 袁士捷 张雨 龙默涵 杨琼 季建杰 龚筑媛 吴冬丹 王婷玉 高於).freeze
     attr_accessor :q_user_id
 
     NO_ANSWER_FOUND_1 = %w(我还小~还不知道呢 你。。。你问到我了 嗯，这个问题有意思).freeze
