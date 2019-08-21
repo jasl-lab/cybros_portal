3a8bf7fb0e3462a965fa0f1ad36fa481a37e13bb
Eric-Guo <eric.guocz@gmail.com>
2019-08-21 13:41:20 +0800

    BI Data refresh at 5:30 AM, so restart the server after that.

--- config/schedule.rb
+++ config/schedule.rb
@@ -16,4 +16,8 @@
 every 1.day, at: ['8:00', '10:00', '12:00', '14:00', '17:00'] do
   rake "sync_yingjianke:sync_overrun_users"
 end
+
+every 1.day, at: "5:55" do
+  command "RAILS_ENV=production; cd /var/www/cybros/current/ && bundle exec pumactl -S /var/www/cybros/shared/tmp/pids/puma.state -F /var/www/cybros/shared/puma.rb restart"
+end
 # Learn more: http://github.com/javan/whenever
