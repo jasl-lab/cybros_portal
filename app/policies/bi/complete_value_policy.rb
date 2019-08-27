c53b7dafb3d14731c462ad5d30392bbde1e8c915
Eric-Guo <eric.guocz@gmail.com>
2019-08-27 13:40:37 +0800

    Fix contract sign problem in sub company.

--- app/policies/bi/complete_value_policy.rb
+++ app/policies/bi/complete_value_policy.rb
@@ -1,6 +1,7 @@
+# frozen_string_literal: true
+
 module Bi
   class CompleteValuePolicy < BasePolicy
-
     def show?
       user&.report_maintainer?
     end
--- app/policies/bi/contract_sign_dept_policy.rb
+++ app/policies/bi/contract_sign_dept_policy.rb
@@ -2,11 +2,7 @@ module Bi
   class ContractSignDeptPolicy < BasePolicy
     class Scope < Scope
       def resolve
-        if user&.report_maintainer?
-          scope.all
-        else
-          scope.where(businessltdname: user.departments.pluck(:company_name))
-        end
+        scope.all
       end
     end
   end
