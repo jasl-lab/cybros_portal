4b5f1ce5f3ad037e585578ce52dfb54843d05648
Eric-Guo <eric.guocz@gmail.com>
2019-08-28 13:16:34 +0800

    Allow select org in 集团子公司收款情况

--- app/controllers/report/subsidiary_receives_controller.rb
+++ app/controllers/report/subsidiary_receives_controller.rb
@@ -12,18 +12,24 @@ class Report::SubsidiaryReceivesController < Report::BaseController
     @all_month_names = Bi::SubCompanyRealReceive.all_month_names
     @month_name = params[:month_name]&.strip || @all_month_names.last
     @end_of_month = Date.parse(@month_name).end_of_month
+    @orgs_options = params[:orgs]
 
     current_user_companies = current_user.user_company_names
-    @real_data = policy_scope(Bi::SubCompanyRealReceive).where("realdate <= ?", @end_of_month)
+    real_data = policy_scope(Bi::SubCompanyRealReceive).where("realdate <= ?", @end_of_month)
       .select("orgcode, org_order, SUM(real_receive) real_receive")
       .joins("LEFT JOIN ORG_ORDER on ORG_ORDER.org_code = SUB_COMPANY_REAL_RECEIVE.orgcode")
       .group(:orgcode, :org_order)
       .order("ORG_ORDER.org_order DESC")
-    @real_company_names = @real_data.collect do |rd|
-      Bi::PkCodeName.mapping2orgcode.fetch(rd.orgcode, rd.orgcode)
+    @only_have_real_data_orgs = real_data.collect(&:orgcode)
+    @real_company_names = @only_have_real_data_orgs.collect do |orgcode|
+      Bi::PkCodeName.mapping2orgcode.fetch(orgcode, orgcode)
     end
 
     @real_company_short_names = @real_company_names.collect { |c| Bi::StaffCount.company_short_names.fetch(c, c) }
+    @orgs_options = real_data.where.not(orgcode: "000101").pluck(:orgcode) if @orgs_options.blank?
+    @organization_options = @real_company_short_names.zip(@only_have_real_data_orgs)
+
+    @real_data = real_data.where(orgcode: @orgs_options)
     @real_receives = @real_data.collect { |d| (d.real_receive / 10000.0).round(0) }
 
     @need_data = policy_scope(Bi::SubCompanyNeedReceive) \
@@ -32,6 +38,7 @@ class Report::SubsidiaryReceivesController < Report::BaseController
       .joins("LEFT JOIN ORG_ORDER on ORG_ORDER.org_code = SUB_COMPANY_NEED_RECEIVE.orgcode")
       .group(:orgcode, :org_order)
       .order("ORG_ORDER.org_order DESC")
+      .where(orgcode: @orgs_options)
     @need_company_names = @need_data.collect do |nd|
       Bi::PkCodeName.mapping2orgcode.fetch(nd.orgcode, nd.orgcode)
     end
