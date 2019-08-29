435374d1f2fa3617e89aba01f8012afd85c2c67e
Eric-Guo <eric.guocz@gmail.com>
2019-08-29 09:52:12 +0800

    Return user info from /api/me.json

--- app/controllers/api/application_controller.rb
+++ app/controllers/api/application_controller.rb
@@ -5,7 +5,18 @@ module API
     before_action :authenticate_user!
 
     def user_info
-      render json: { email: current_user.email }
+      u = current_user
+      departments = u.departments.collect do |department|
+        { id: department.id, name: department.name, company_name: department.company_name }
+      end
+      render json: {
+        email: u.email,
+        position_title: u.position_title,
+        clerk_code: u.clerk_code,
+        chinese_name: u.chinese_name,
+        desk_phone: u.desk_phone,
+        departments: departments
+      }
     end
   end
 end
