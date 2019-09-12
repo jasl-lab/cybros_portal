296f1b246a38a0d68962354d54036a645f7e7ce0
Eric-Guo <eric.guocz@gmail.com>
2019-09-12 18:59:30 +0800

    Using default setting so 臧晓磊 can not see Shanghai report

--- app/policies/bi/contract_hold_policy.rb
+++ app/policies/bi/contract_hold_policy.rb
@@ -7,10 +7,5 @@ module Bi
         scope.all
       end
     end
-
-    def show?
-      return false unless user.present?
-      user.roles.pluck(:report_viewer).any? || user.roles.pluck(:report_reviewer).any? || user.admin?
-    end
   end
 end
