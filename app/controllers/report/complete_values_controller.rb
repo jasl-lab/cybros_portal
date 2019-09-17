3e40a54302831cdf0dd6e37f51b8ad6265b95303
Eric-Guo <eric.guocz@gmail.com>
2019-09-17 16:56:12 +0800

    Add additional params to do orgcode_sum later.

--- app/controllers/report/complete_values_controller.rb
+++ app/controllers/report/complete_values_controller.rb
@@ -12,25 +12,28 @@ class Report::CompleteValuesController < Report::BaseController
     @month_name = params[:month_name]&.strip || @all_month_names.last
     @end_of_month = Date.parse(@month_name).end_of_month
     @show_shanghai_hq = params[:show_shanghai_hq]&.presence
+    @view_orgcode_sum = params[:view_orgcode_sum] == "true"
 
     last_available_date = policy_scope(Bi::CompleteValue).last_available_date(@end_of_month)
-    @data = policy_scope(Bi::CompleteValue).where(date: last_available_date)
+    data = policy_scope(Bi::CompleteValue).where(date: last_available_date)
       .select("orgcode, org_order, SUM(total) sum_total")
       .joins("LEFT JOIN ORG_ORDER on ORG_ORDER.org_code = COMPLETE_VALUE.orgcode")
       .group(:orgcode, :org_order)
       .having("SUM(total) > 0")
       .order("ORG_ORDER.org_order DESC")
-    all_company_names = @data.collect(&:orgcode).collect do |c|
+
+
+    all_company_names = data.collect(&:orgcode).collect do |c|
       Bi::PkCodeName.mapping2orgcode.fetch(c, c)
     end
     @all_company_short_names = all_company_names.collect { |c| Bi::StaffCount.company_short_names.fetch(c, c) }
-    @complete_value_totals = @data.collect { |d| (d.sum_total / 100_0000.0).round(0) }
+    @complete_value_totals = data.collect { |d| (d.sum_total / 100_0000.0).round(0) }
     @fix_sum_complete_value_totals = ((Bi::CompleteValue.where(date: last_available_date).select("SUM(total) sum_total").first.sum_total || 0) / 10000_0000.0).round(1)
     @complete_value_year_totals = @complete_value_totals.collect { |d| (d / (@end_of_month.month / 12.0)).round(0) }
     @complete_value_year_totals_remain = @complete_value_year_totals.zip(@complete_value_totals).map { |d| d[0] - d[1] }
     @fix_sum_complete_value_year_totals = (@complete_value_year_totals.sum / 100.0).round(1)
     @staff_per_company = Bi::StaffCount.staff_per_short_company_name(@end_of_month)
-    @complete_value_totals_per_staff = @data.collect do |d|
+    @complete_value_totals_per_staff = data.collect do |d|
       company_name = Bi::PkCodeName.mapping2orgcode.fetch(d.orgcode, d.orgcode)
       short_name = Bi::StaffCount.company_short_names.fetch(company_name, company_name)
       staff_number = @staff_per_company.fetch(short_name, 1000_0000)
