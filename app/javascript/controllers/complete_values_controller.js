b7d2bd2c49ac73776c1eda6832c2654b1b5b5df5
Eric-Guo <eric.guocz@gmail.com>
2019-09-19 13:46:35 +0800

    No logic change, JS file change.

--- app/javascript/controllers/complete_values_controller.js
+++ app/javascript/controllers/complete_values_controller.js
@@ -16,179 +16,179 @@ export default class extends Controller {
     const completeValueTotalsPerStaff = JSON.parse(this.data.get("complete_value_totals_per_staff"));
     const completeValueYearTotalsPerStaff = JSON.parse(this.data.get("complete_value_year_totals_per_staff"));
 
-var option_total = {
-    legend: {
-        data: ['本年累计完成产值（百万元）','预计全年完成产值（百万元）'],
-        align: 'left'
-    },
-    tooltip: {
-      trigger: 'axis',
-      axisPointer: {
-        type: 'cross'
-      }
-    },
-    grid: {
-      left: 70,
-      right: 110,
-      top: 50,
-      bottom: 60
-    },
-    toolbox: {
-      feature: {
-        dataView: {},
-        saveAsImage: {
-            pixelRatio: 2
-        }
-      }
-    },
-    xAxis: {
-      data: xAxisData,
-      silent: true,
-      axisLabel: {
-        interval: 0,
-        rotate: -40
-      },
-      splitLine: {
-          show: false
-      }
-    },
-    yAxis: [{
-      type: 'value',
-      name: '累计完成产值（百万元）',
-      position: 'left',
-      axisLabel: {
-        formatter: '{value}百万'
-      }
-    }],
-    series: [{
-      name: '预计全年完成产值（百万元）',
-      type: 'bar',
-      barWidth: 26,
-      barGap: '-100%',
-      data: completeValueYearTotals,
-      itemStyle: {
-        color: '#DDDDDD'
-      },
-      label: {
-        normal: {
-          show: true,
-          position: 'top',
-          fontWeight: 'bold',
-          fontSize: 15,
-          color: '#000000'
-        }
-      }
-    },{
-      name: '本年累计完成产值（百万元）',
-      type: 'bar',
-      stack: '总量',
-      data: completeValueTotals,
-      itemStyle: {
-        color: '#738496'
-      },
-      label: {
-        normal: {
-          show: true,
-          position: 'inside'
-        }
-      }
-    },{
-      name: '全年剩余需完成产值（百万元）',
-      type: 'bar',
-      stack: '总量',
-      data: completeValueYearTotalsRemain,
-      barWidth: 26,
-      itemStyle: {
-        color: '#DDDDDD'
-      },
-      label: {
-        normal: {
-          show: true,
-          position: 'inside',
-          color: '#353535'
-        }
-      }
-    }]
-};
+    const option_total = {
+        legend: {
+            data: ['本年累计完成产值（百万元）','预计全年完成产值（百万元）'],
+            align: 'left'
+        },
+        tooltip: {
+          trigger: 'axis',
+          axisPointer: {
+            type: 'cross'
+          }
+        },
+        grid: {
+          left: 70,
+          right: 110,
+          top: 50,
+          bottom: 60
+        },
+        toolbox: {
+          feature: {
+            dataView: {},
+            saveAsImage: {
+                pixelRatio: 2
+            }
+          }
+        },
+        xAxis: {
+          data: xAxisData,
+          silent: true,
+          axisLabel: {
+            interval: 0,
+            rotate: -40
+          },
+          splitLine: {
+              show: false
+          }
+        },
+        yAxis: [{
+          type: 'value',
+          name: '累计完成产值（百万元）',
+          position: 'left',
+          axisLabel: {
+            formatter: '{value}百万'
+          }
+        }],
+        series: [{
+          name: '预计全年完成产值（百万元）',
+          type: 'bar',
+          barWidth: 26,
+          barGap: '-100%',
+          data: completeValueYearTotals,
+          itemStyle: {
+            color: '#DDDDDD'
+          },
+          label: {
+            normal: {
+              show: true,
+              position: 'top',
+              fontWeight: 'bold',
+              fontSize: 15,
+              color: '#000000'
+            }
+          }
+        },{
+          name: '本年累计完成产值（百万元）',
+          type: 'bar',
+          stack: '总量',
+          data: completeValueTotals,
+          itemStyle: {
+            color: '#738496'
+          },
+          label: {
+            normal: {
+              show: true,
+              position: 'inside'
+            }
+          }
+        },{
+          name: '全年剩余需完成产值（百万元）',
+          type: 'bar',
+          stack: '总量',
+          data: completeValueYearTotalsRemain,
+          barWidth: 26,
+          itemStyle: {
+            color: '#DDDDDD'
+          },
+          label: {
+            normal: {
+              show: true,
+              position: 'inside',
+              color: '#353535'
+            }
+          }
+        }]
+    };
 
-var option_staff = {
-    legend: {
-        data: ['本年累计人均完成产值（万元）','预计全年人均完成产值（万元）'],
-        align: 'left'
-    },
-    tooltip: {
-      trigger: 'axis',
-      axisPointer: {
-        type: 'cross'
-      }
-    },
-    grid: {
-      left: 70,
-      right: 110,
-      top: 50,
-      bottom: 80
-    },
-    toolbox: {
-      feature: {
-        dataView: {},
-        saveAsImage: {
-            pixelRatio: 2
-        }
-      }
-    },
-    xAxis: {
-      data: xAxisData,
-      offset: 16,
-      silent: true,
-      axisLabel: {
-        interval: 0,
-        rotate: -40
-      },
-      splitLine: {
-          show: false
-      }
-    },
-    yAxis: [{
-      type: 'value',
-      name: '人均完成产值（万元）',
-      position: 'left',
-      axisLabel: {
-        formatter: '{value}万'
-      }
-    }],
-    series: [{
-      name: '本年累计人均完成产值（万元）',
-      type: 'bar',
-      stack: '人均',
-      data: completeValueTotalsPerStaff,
-      itemStyle: {
-        color: '#60A0A8'
-      },
-      label: {
-        normal: {
-          show: true,
-          position: 'insideTop'
-        }
-      }
-    },{
-      name: '预计全年人均完成产值（万元）',
-      type: 'bar',
-      stack: '人均',
-      data: completeValueYearTotalsPerStaff,
-      itemStyle: {
-        color: '#DDDDDD'
-      },
-      barWidth: 26,
-      label: {
-        normal: {
-          show: true,
-          position: 'top',
-          fontWeight: 'bold',
-          color: '#000000'
-        }
-      }
-    }]
-};
+    const option_staff = {
+        legend: {
+            data: ['本年累计人均完成产值（万元）','预计全年人均完成产值（万元）'],
+            align: 'left'
+        },
+        tooltip: {
+          trigger: 'axis',
+          axisPointer: {
+            type: 'cross'
+          }
+        },
+        grid: {
+          left: 70,
+          right: 110,
+          top: 50,
+          bottom: 80
+        },
+        toolbox: {
+          feature: {
+            dataView: {},
+            saveAsImage: {
+                pixelRatio: 2
+            }
+          }
+        },
+        xAxis: {
+          data: xAxisData,
+          offset: 16,
+          silent: true,
+          axisLabel: {
+            interval: 0,
+            rotate: -40
+          },
+          splitLine: {
+              show: false
+          }
+        },
+        yAxis: [{
+          type: 'value',
+          name: '人均完成产值（万元）',
+          position: 'left',
+          axisLabel: {
+            formatter: '{value}万'
+          }
+        }],
+        series: [{
+          name: '本年累计人均完成产值（万元）',
+          type: 'bar',
+          stack: '人均',
+          data: completeValueTotalsPerStaff,
+          itemStyle: {
+            color: '#60A0A8'
+          },
+          label: {
+            normal: {
+              show: true,
+              position: 'insideTop'
+            }
+          }
+        },{
+          name: '预计全年人均完成产值（万元）',
+          type: 'bar',
+          stack: '人均',
+          data: completeValueYearTotalsPerStaff,
+          itemStyle: {
+            color: '#DDDDDD'
+          },
+          barWidth: 26,
+          label: {
+            normal: {
+              show: true,
+              position: 'top',
+              fontWeight: 'bold',
+              color: '#000000'
+            }
+          }
+        }]
+    };
 
     function drill_down_complete_value_total(params) {
       if (params.componentType === 'series') {
--- app/javascript/controllers/contract_signings_controller.js
+++ app/javascript/controllers/contract_signings_controller.js
@@ -8,214 +8,214 @@ export default class extends Controller {
     contractSigningsChart = echarts.init(document.getElementById('contract-signings-chart'));
     contractSigningsAvgChart = echarts.init(document.getElementById('contract-signings-avg-chart'));
 
-var xAxisData = JSON.parse(this.data.get("x_axis"));
-var currentUserCompaniesShortNames = JSON.parse(this.data.get("current_user_companies_short_names"));
-var companyName = this.data.get("company_name");
-var secondLevelDrill = this.data.get("second_level_drill");
-var sumContractAmounts = JSON.parse(this.data.get("sum_contract_amounts"));
-var sumContractAmountMax = JSON.parse(this.data.get("sum_contract_amount_max"));
-var avgPeriodMean = JSON.parse(this.data.get("avg_period_mean"));
-var avgPeriodMeanMax = JSON.parse(this.data.get("avg_period_mean_max"));
-var contractAmountsPerStaff = JSON.parse(this.data.get("contract_amounts_per_staff"));
-var periodMeanRef = this.data.get("period_mean_ref");
-var contractAmountsPerStaffRef = this.data.get("contract_amounts_per_staff_ref");
+    const xAxisData = JSON.parse(this.data.get("x_axis"));
+    const currentUserCompaniesShortNames = JSON.parse(this.data.get("current_user_companies_short_names"));
+    const companyName = this.data.get("company_name");
+    const secondLevelDrill = this.data.get("second_level_drill");
+    const sumContractAmounts = JSON.parse(this.data.get("sum_contract_amounts"));
+    const sumContractAmountMax = JSON.parse(this.data.get("sum_contract_amount_max"));
+    const avgPeriodMean = JSON.parse(this.data.get("avg_period_mean"));
+    const avgPeriodMeanMax = JSON.parse(this.data.get("avg_period_mean_max"));
+    const contractAmountsPerStaff = JSON.parse(this.data.get("contract_amounts_per_staff"));
+    const periodMeanRef = this.data.get("period_mean_ref");
+    const contractAmountsPerStaffRef = this.data.get("contract_amounts_per_staff_ref");
 
-function differentColor(amount) {
-  let color;
+    function differentColor(amount) {
+      let color;
 
-  if(contractAmountsPerStaffRef > amount) {
-    color = '#BB332E';
-  } else {
-    color = '#7E91A5';
-  }
+      if(contractAmountsPerStaffRef > amount) {
+        color = '#BB332E';
+      } else {
+        color = '#7E91A5';
+      }
 
-  return { value: amount, itemStyle: { color: color }}
-}
+      return { value: amount, itemStyle: { color: color }}
+    }
 
-var contractAmountsPerStaffWithColor = contractAmountsPerStaff.map(differentColor);
+    const contractAmountsPerStaffWithColor = contractAmountsPerStaff.map(differentColor);
 
-var option = {
-    legend: {
-        data: ['签约周期','本年累计合同额'],
-        align: 'left'
-    },
-    tooltip: {
-      trigger: 'axis',
-      axisPointer: {
-        type: 'cross'
-      }
-    },
-    grid: {
-      left: 70,
-      right: 110,
-      top: 50,
-      bottom: 60
-    },
-    toolbox: {
-      feature: {
-        dataView: {},
-        saveAsImage: {
-            pixelRatio: 2
-        }
-      }
-    },
-    xAxis: {
-      data: xAxisData,
-      silent: true,
-      axisLabel: {
-        interval: 0,
-        rotate: -40
-      },
-      splitLine: {
-          show: false
-      }
-    },
-    yAxis: [{
-      type: 'value',
-      name: '本年累计合同额（百万元）',
-      position: 'left',
-      min: 0,
-      max: sumContractAmountMax,
-      interval: Math.ceil(sumContractAmountMax / 5),
-      axisLabel: {
-        formatter: '{value}百万'
-      }
-    },{
-      type: 'value',
-      name: '签约周期（天）',
-      position: 'right',
-      min: 0,
-      max: avgPeriodMeanMax,
-      interval: Math.ceil(avgPeriodMeanMax / 5),
-      axisLine: {
-        lineStyle: {
-          color: '#675BBA'
-        }
-      },
-      axisLabel: {
-        formatter: '{value}天'
-      }
-    }],
-    series: [{
-      name: '签约周期',
-      type: 'line',
-      yAxisIndex: 1,
-      symbol: 'circle',
-      symbolSize: 8,
-      data: avgPeriodMean,
-      color: '#675BBA',
-      label: {
-        normal: {
-          show: true,
-          position: 'top'
-        }
-      },
-      markLine: {
-        label: {
-          formatter: '签约周期{c}天线'
+    const option = {
+        legend: {
+            data: ['签约周期','本年累计合同额'],
+            align: 'left'
         },
-        lineStyle: {
-          type: 'solid',
-          width: 1
-        },
-        data: [
-          {
-            yAxis: periodMeanRef
+        tooltip: {
+          trigger: 'axis',
+          axisPointer: {
+            type: 'cross'
           }
-        ]
-      }
-    },{
-      name: '本年累计合同额',
-      type: 'bar',
-      data: sumContractAmounts,
-      color: '#738496',
-      barMaxWidth: 80,
-      label: {
-        normal: {
-          show: true,
-          position: 'top'
-        }
-      }
-    }]
-};
-
-var option_avg = {
-    legend: {
-        data: ['本年累计人均合同额'],
-        align: 'left'
-    },
-    tooltip: {
-      trigger: 'axis',
-      axisPointer: {
-        type: 'cross'
-      }
-    },
-    grid: {
-      left: 80,
-      right: 110,
-      top: 50,
-      bottom: 60
-    },
-    toolbox: {
-      feature: {
-        dataView: {},
-        saveAsImage: {
-            pixelRatio: 2
-        }
-      }
-    },
-    xAxis: {
-      data: xAxisData,
-      silent: true,
-      axisLabel: {
-        interval: 0,
-        rotate: -40
-      },
-      splitLine: {
-          show: false
-      }
-    },
-    yAxis: [{
-      type: 'value',
-      name: '本年累计人均合同额（万元）',
-      position: 'left',
-      axisLabel: {
-        formatter: '{value}万'
-      }
-    }],
-    series: [{
-      name: '本年累计人均合同额',
-      type: 'bar',
-      data: contractAmountsPerStaffWithColor,
-      barMaxWidth: 80,
-      label: {
-        normal: {
-          show: true,
-          position: 'top'
-        }
-      },
-      markLine: {
-        label: {
-          formatter: '人均合同额预警'
         },
-        lineStyle: {
-          type: 'solid',
-          width: 1
+        grid: {
+          left: 70,
+          right: 110,
+          top: 50,
+          bottom: 60
         },
-        data: [
-          {
-            yAxis: contractAmountsPerStaffRef
+        toolbox: {
+          feature: {
+            dataView: {},
+            saveAsImage: {
+                pixelRatio: 2
+            }
           }
-        ]
-      }
-    }]
-};
+        },
+        xAxis: {
+          data: xAxisData,
+          silent: true,
+          axisLabel: {
+            interval: 0,
+            rotate: -40
+          },
+          splitLine: {
+              show: false
+          }
+        },
+        yAxis: [{
+          type: 'value',
+          name: '本年累计合同额（百万元）',
+          position: 'left',
+          min: 0,
+          max: sumContractAmountMax,
+          interval: Math.ceil(sumContractAmountMax / 5),
+          axisLabel: {
+            formatter: '{value}百万'
+          }
+        },{
+          type: 'value',
+          name: '签约周期（天）',
+          position: 'right',
+          min: 0,
+          max: avgPeriodMeanMax,
+          interval: Math.ceil(avgPeriodMeanMax / 5),
+          axisLine: {
+            lineStyle: {
+              color: '#675BBA'
+            }
+          },
+          axisLabel: {
+            formatter: '{value}天'
+          }
+        }],
+        series: [{
+          name: '签约周期',
+          type: 'line',
+          yAxisIndex: 1,
+          symbol: 'circle',
+          symbolSize: 8,
+          data: avgPeriodMean,
+          color: '#675BBA',
+          label: {
+            normal: {
+              show: true,
+              position: 'top'
+            }
+          },
+          markLine: {
+            label: {
+              formatter: '签约周期{c}天线'
+            },
+            lineStyle: {
+              type: 'solid',
+              width: 1
+            },
+            data: [
+              {
+                yAxis: periodMeanRef
+              }
+            ]
+          }
+        },{
+          name: '本年累计合同额',
+          type: 'bar',
+          data: sumContractAmounts,
+          color: '#738496',
+          barMaxWidth: 80,
+          label: {
+            normal: {
+              show: true,
+              position: 'top'
+            }
+          }
+        }]
+    };
+
+    const option_avg = {
+        legend: {
+            data: ['本年累计人均合同额'],
+            align: 'left'
+        },
+        tooltip: {
+          trigger: 'axis',
+          axisPointer: {
+            type: 'cross'
+          }
+        },
+        grid: {
+          left: 80,
+          right: 110,
+          top: 50,
+          bottom: 60
+        },
+        toolbox: {
+          feature: {
+            dataView: {},
+            saveAsImage: {
+                pixelRatio: 2
+            }
+          }
+        },
+        xAxis: {
+          data: xAxisData,
+          silent: true,
+          axisLabel: {
+            interval: 0,
+            rotate: -40
+          },
+          splitLine: {
+              show: false
+          }
+        },
+        yAxis: [{
+          type: 'value',
+          name: '本年累计人均合同额（万元）',
+          position: 'left',
+          axisLabel: {
+            formatter: '{value}万'
+          }
+        }],
+        series: [{
+          name: '本年累计人均合同额',
+          type: 'bar',
+          data: contractAmountsPerStaffWithColor,
+          barMaxWidth: 80,
+          label: {
+            normal: {
+              show: true,
+              position: 'top'
+            }
+          },
+          markLine: {
+            label: {
+              formatter: '人均合同额预警'
+            },
+            lineStyle: {
+              type: 'solid',
+              width: 1
+            },
+            data: [
+              {
+                yAxis: contractAmountsPerStaffRef
+              }
+            ]
+          }
+        }]
+    };
 
     function drill_down_on_click(params) {
       if (params.componentType === 'series') {
         if (params.seriesType === 'bar') {
           let url = '/report/subsidiary_contract_signing';
-          const series_company = xAxisData[params.dataIndex]
+          const series_company = xAxisData[params.dataIndex];
           if (url.indexOf('?') > -1) {
             url += '&company_name=' + encodeURIComponent(series_company);
           } else {
