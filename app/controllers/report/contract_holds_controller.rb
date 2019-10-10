d99d2ece500967ef41fe64006886e2f1c2c459a2
Eric-Guo <eric.guocz@gmail.com>
2019-10-10 14:20:25 +0800

    Link to SH_REPORT_DEPT_ORDER.

--- app/controllers/report/contract_holds_controller.rb
+++ app/controllers/report/contract_holds_controller.rb
@@ -16,16 +16,16 @@ class Report::ContractHoldsController < Report::BaseController
 
     data = policy_scope(Bi::ContractHold)
       .where(date: @last_available_date)
-      .order("SH_REPORT_DEPT_ORDER.dept_asc")
+      .order("ORG_REPORT_DEPT_ORDER.部门排名, ORG_REPORT_DEPT_ORDER.编号")
 
     data = if @view_deptcode_sum
-      data.select("CONTRACT_HOLD.deptcode_sum deptcode, SH_REPORT_DEPT_ORDER.dept_asc, SUM(busiretentcontract) busiretentcontract, SUM(busiretentnocontract) busiretentnocontract")
-        .joins("LEFT JOIN SH_REPORT_DEPT_ORDER on SH_REPORT_DEPT_ORDER.deptcode = CONTRACT_HOLD.deptcode_sum")
-        .group("CONTRACT_HOLD.deptcode_sum, SH_REPORT_DEPT_ORDER.dept_asc")
+      data.select("CONTRACT_HOLD.deptcode_sum deptcode, ORG_REPORT_DEPT_ORDER.部门排名, SUM(busiretentcontract) busiretentcontract, SUM(busiretentnocontract) busiretentnocontract")
+        .joins("LEFT JOIN ORG_REPORT_DEPT_ORDER on ORG_REPORT_DEPT_ORDER.编号 = CONTRACT_HOLD.deptcode_sum")
+        .group("CONTRACT_HOLD.deptcode_sum, ORG_REPORT_DEPT_ORDER.部门排名")
     else
-      data.select("CONTRACT_HOLD.deptcode, SH_REPORT_DEPT_ORDER.dept_asc, SUM(busiretentcontract) busiretentcontract, SUM(busiretentnocontract) busiretentnocontract")
-        .joins("LEFT JOIN SH_REPORT_DEPT_ORDER on SH_REPORT_DEPT_ORDER.deptcode = CONTRACT_HOLD.deptcode")
-        .group("CONTRACT_HOLD.deptcode, SH_REPORT_DEPT_ORDER.dept_asc")
+      data.select("CONTRACT_HOLD.deptcode, ORG_REPORT_DEPT_ORDER.部门排名, SUM(busiretentcontract) busiretentcontract, SUM(busiretentnocontract) busiretentnocontract")
+        .joins("LEFT JOIN ORG_REPORT_DEPT_ORDER on ORG_REPORT_DEPT_ORDER.编号 = CONTRACT_HOLD.deptcode")
+        .group("CONTRACT_HOLD.deptcode, ORG_REPORT_DEPT_ORDER.部门排名")
     end
 
     @dept_options = if @dept_options.blank? && @view_deptcode_sum
