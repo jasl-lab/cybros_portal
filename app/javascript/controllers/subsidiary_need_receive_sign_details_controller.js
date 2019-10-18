3e768a66b92481e7ae4db85e1b7c487918735f24
Eric-Guo <eric.guocz@gmail.com>
2019-10-18 13:12:06 +0800

    Default sort on 合同业务应收，on 已签约项目业务应收

--- app/javascript/controllers/subsidiary_need_receive_sign_details_controller.js
+++ app/javascript/controllers/subsidiary_need_receive_sign_details_controller.js
@@ -26,7 +26,7 @@ export default class extends Controller {
       "ajax": $('#subsidiary-need-receive-sign-details-datatable').data('source'),
       "pagingType": "full_numbers",
       "columns": (canHideItem ? adminColumns : normalColumns),
-      "order": [[ 5, 'desc' ]],
+      "order": [[ 8, 'desc' ]],
       stateSave: true,
       stateSaveCallback: function(settings, data) {
           localStorage.setItem('DataTables_subsidiary-need-receive-sign-details', JSON.stringify(data));
