afd12ea0454246e64533c37e90f6944eb6e77436
Eric-Guo <eric.guocz@gmail.com>
2019-10-25 11:32:03 +0800

    Enable loading dynamic.

--- app/controllers/company/contracts_controller.rb
+++ app/controllers/company/contracts_controller.rb
@@ -6,7 +6,6 @@ class Company::ContractsController < ApplicationController
 
   def index
     prepare_meta_tags title: t(".title")
-    @using_lbs_qq = true
   end
 
   protected
--- app/javascript/controllers/full_map_controller.js
+++ app/javascript/controllers/full_map_controller.js
@@ -1,14 +1,28 @@
 import { Controller } from "stimulus"
 
+window.initFullMap = function () {
+  const center = new TMap.LatLng(31.228177,121.487003); //设置中心点坐标
+  //初始化地图
+  window.full_map = new TMap.Map("full-map", {
+      center: center
+  });
+}
+
 export default class extends Controller {
   connect() {
-    const center = new TMap.LatLng(31.228177,121.487003); //设置中心点坐标
-    //初始化地图
-    const map = new TMap.Map("full-map", {
-        center: center
-    });
+    if (typeof TMap != 'object') {
+      //创建script标签，并设置src属性添加到body中
+      var script = document.createElement("script");
+      script.type = "text/javascript";
+      script.src = "https://map.qq.com/api/gljs?v=1.exp&key=EJPBZ-7TEKU-DLQVP-2K6EE-XZ6O6-NWBKM&callback=initFullMap";
+      document.body.appendChild(script);
+    } else {
+      window.initFullMap();
+    }
   }
 
   disconnect() {
+    window.full_map.destroy();
+    window.full_map = null;
   }
 }
