6566e6b040a1d2a019feb9378c89149fbe2331e5
Eric-Guo <eric.guocz@gmail.com>
2019-10-29 11:31:35 +0800

    Should consider user not login problems.

--- app/policies/bi/sh_refresh_rate_policy.rb
+++ app/policies/bi/sh_refresh_rate_policy.rb
@@ -7,8 +7,10 @@ module Bi
         if user.present? && (user.roles.pluck(:report_viewer).any? || user.roles.pluck(:report_reviewer).any? || user.admin?) && \
            user.departments.pluck(:company_name).uniq == ["上海天华建筑设计有限公司"]
           scope.all
-        else
+        elsif user.present?
           scope.where(orgcode: user.user_company_orgcode)
+        else
+          scope.none
         end
       end
     end
