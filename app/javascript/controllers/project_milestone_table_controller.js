152b94727d2bbb4506601a7cd58f3299e1b501a8
Eric-Guo <eric.guocz@gmail.com>
2019-10-30 15:37:55 +0800

    Drill down the department code as well.

--- app/javascript/controllers/project_milestone_table_controller.js
+++ app/javascript/controllers/project_milestone_table_controller.js
@@ -3,10 +3,12 @@ import { Controller } from "stimulus"
 export default class extends Controller {
   drill_down(event) {
     const work_no = $(event.target).data("project-milestone-table-work_no");
+    const dept_code = $(event.target).data("project-milestone-table-dept_code");
     const month_name = $('#month_name').val();
 
     const sent_data = {
       work_no,
+      dept_code,
       month_name
     };
     const drill_down_url = '/report/project_milestore/detail_table_drill_down';
