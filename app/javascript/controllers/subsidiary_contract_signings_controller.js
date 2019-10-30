02b979b873eed13a1a9b4411f4d21c2264ebc3a6
Eric-Guo <eric.guocz@gmail.com>
2019-10-30 14:14:27 +0800

    Hide 签约合同额，只显示生产合同额。

--- app/javascript/controllers/subsidiary_contract_signings_controller.js
+++ app/javascript/controllers/subsidiary_contract_signings_controller.js
@@ -1,12 +1,17 @@
 import { Controller } from "stimulus"
 
+let inIFrame;
 let subsidiaryContractSigningsChart;
 let subsidiaryContractProductionChart;
 let subsidiaryContractSigningsAvgChart;
 
 export default class extends Controller {
   connect() {
-    subsidiaryContractSigningsChart = echarts.init(document.getElementById('subsidiary-contract-signings-chart'));
+    inIFrame = this.data.get("in_iframe");
+
+    if (inIFrame != "true") {
+      subsidiaryContractSigningsChart = echarts.init(document.getElementById('subsidiary-contract-signings-chart'));
+    }
     subsidiaryContractProductionChart = echarts.init(document.getElementById('subsidiary-contract-production-chart'));
     subsidiaryContractSigningsAvgChart = echarts.init(document.getElementById('subsidiary-contract-signings-avg-chart'));
 
@@ -25,257 +30,258 @@ export default class extends Controller {
     const cpDepartmentNames = JSON.parse(this.data.get("cp_department_names"));
     const cpContractAmounts = JSON.parse(this.data.get("cp_contract_amounts"));
 
-function differentColor(amount) {
-  let color;
+    function differentColor(amount) {
+      let color;
 
-  if(contractAmountsPerStaffRef > amount) {
-    color = '#BB332E';
-  } else {
-    color = '#7E91A5';
-  }
+      if(contractAmountsPerStaffRef > amount) {
+        color = '#BB332E';
+      } else {
+        color = '#7E91A5';
+      }
 
-  return { value: amount, itemStyle: { color: color }}
-}
+      return { value: amount, itemStyle: { color: color }}
+    }
 
-var contractAmountsPerStaffWithColor = contractAmountsPerStaff.map(differentColor);
+    const contractAmountsPerStaffWithColor = contractAmountsPerStaff.map(differentColor);
 
-var option = {
-    legend: {
-        data: ['签约周期','本年累计签约合同额'],
-        align: 'left'
-    },
-    tooltip: {
-      trigger: 'axis',
-      axisPointer: {
-        type: 'cross'
-      }
-    },
-    grid: {
-      left: 80,
-      right: 110,
-      top: 50,
-      bottom: 60
-    },
-    toolbox: {
-      feature: {
-        dataView: {},
-        saveAsImage: {
-            pixelRatio: 2
-        }
-      }
-    },
-    xAxis: {
-      data: xAxisData,
-      silent: true,
-      axisLabel: {
-        interval: 0,
-        rotate: -40
-      },
-      splitLine: {
-          show: false
-      }
-    },
-    yAxis: [{
-      type: 'value',
-      name: '本年累计签约合同额（万元）',
-      position: 'left',
-      min: 0,
-      max: sumContractAmountMax,
-      interval: Math.ceil(sumContractAmountMax / 5),
-      axisLabel: {
-        formatter: '{value}万'
-      }
-    },{
-      type: 'value',
-      name: '签约周期（天）',
-      position: 'right',
-      min: 0,
-      max: avgPeriodMeanMax,
-      interval: Math.ceil(avgPeriodMeanMax / 5),
-      axisLine: {
-        lineStyle: {
-          color: '#675BBA'
-        }
-      },
-      axisLabel: {
-        formatter: '{value}天'
-      }
-    }],
-    series: [{
-      name: '签约周期',
-      type: 'line',
-      yAxisIndex: 1,
-      symbol: 'circle',
-      symbolSize: 8,
-      data: avgPeriodMean,
-      color: '#675BBA',
-      barMaxWidth: 40,
-      label: {
-        normal: {
-          show: true,
-          position: 'top'
-        }
-      },
-      markLine: {
-        label: {
-          formatter: '签约周期{c}天线'
+    const option = {
+        legend: {
+            data: ['签约周期','本年累计签约合同额'],
+            align: 'left'
         },
-        lineStyle: {
-          type: 'solid',
-          width: 1
-        },
-        data: [
-          {
-            yAxis: periodMeanRef
+        tooltip: {
+          trigger: 'axis',
+          axisPointer: {
+            type: 'cross'
           }
-        ]
-      }
-    },{
-      name: '本年累计签约合同额',
-      type: 'bar',
-      data: sumContractAmounts,
-      color: '#738496',
-      barMaxWidth: 40,
-      label: {
-        normal: {
-          show: true,
-          position: 'top'
-        }
-      }
-    }]
-};
-
-var cp_option = {
-    legend: {
-        data: ['本年累计生产合同额'],
-        align: 'left'
-    },
-    tooltip: {
-      trigger: 'axis',
-      axisPointer: {
-        type: 'cross'
-      }
-    },
-    grid: {
-      left: 80,
-      right: 110,
-      top: 50,
-      bottom: 60
-    },
-    toolbox: {
-      feature: {
-        dataView: {},
-        saveAsImage: {
-            pixelRatio: 2
-        }
-      }
-    },
-    xAxis: {
-      data: cpDepartmentNames,
-      silent: true,
-      axisLabel: {
-        interval: 0,
-        rotate: -40
-      },
-      splitLine: {
-          show: false
-      }
-    },
-    yAxis: [{
-      type: 'value',
-      name: '本年累计生产合同额（万元）',
-      position: 'left',
-      axisLabel: {
-        formatter: '{value}万'
-      }
-    }],
-    series: [{
-      name: '本年累计生产合同额',
-      type: 'bar',
-      data: cpContractAmounts,
-      color: '#738496',
-      barMaxWidth: 40,
-      label: {
-        normal: {
-          show: true,
-          position: 'top'
-        }
-      }
-    }]
-};
-
-
-var option_avg = {
-    legend: {
-        data: ['本年累计人均生产合同额'],
-        align: 'left'
-    },
-    tooltip: {
-      trigger: 'axis',
-      axisPointer: {
-        type: 'cross'
-      }
-    },
-    grid: {
-      left: 80,
-      right: 110,
-      top: 50,
-      bottom: 60
-    },
-    toolbox: {
-      feature: {
-        dataView: {},
-        saveAsImage: {
-            pixelRatio: 2
-        }
-      }
-    },
-    xAxis: {
-      data: cpDepartmentNames,
-      silent: true,
-      axisLabel: {
-        interval: 0,
-        rotate: -40
-      },
-      splitLine: {
-          show: false
-      }
-    },
-    yAxis: [{
-      type: 'value',
-      name: '本年累计人均生产合同额（万元）',
-      position: 'left',
-      axisLabel: {
-        formatter: '{value}万'
-      }
-    }],
-    series: [{
-      name: '本年累计人均生产合同额',
-      type: 'bar',
-      data: contractAmountsPerStaffWithColor,
-      barMaxWidth: 40,
-      label: {
-        normal: {
-          show: true,
-          position: 'top'
-        }
-      },
-      markLine: {
-        label: {
-          formatter: '人均合同额预警'
         },
-        lineStyle: {
-          type: 'solid',
-          width: 1
+        grid: {
+          left: 80,
+          right: 110,
+          top: 50,
+          bottom: 60
         },
-        data: [
-          {
-            yAxis: contractAmountsPerStaffRef
+        toolbox: {
+          feature: {
+            dataView: {},
+            saveAsImage: {
+                pixelRatio: 2
+            }
           }
-        ]
-      }
-    }]
-};
+        },
+        xAxis: {
+          data: xAxisData,
+          silent: true,
+          axisLabel: {
+            interval: 0,
+            rotate: -40
+          },
+          splitLine: {
+              show: false
+          }
+        },
+        yAxis: [{
+          type: 'value',
+          name: '本年累计签约合同额（万元）',
+          position: 'left',
+          min: 0,
+          max: sumContractAmountMax,
+          interval: Math.ceil(sumContractAmountMax / 5),
+          axisLabel: {
+            formatter: '{value}万'
+          }
+        },{
+          type: 'value',
+          name: '签约周期（天）',
+          position: 'right',
+          min: 0,
+          max: avgPeriodMeanMax,
+          interval: Math.ceil(avgPeriodMeanMax / 5),
+          axisLine: {
+            lineStyle: {
+              color: '#675BBA'
+            }
+          },
+          axisLabel: {
+            formatter: '{value}天'
+          }
+        }],
+        series: [{
+          name: '签约周期',
+          type: 'line',
+          yAxisIndex: 1,
+          symbol: 'circle',
+          symbolSize: 8,
+          data: avgPeriodMean,
+          color: '#675BBA',
+          barMaxWidth: 40,
+          label: {
+            normal: {
+              show: true,
+              position: 'top'
+            }
+          },
+          markLine: {
+            label: {
+              formatter: '签约周期{c}天线'
+            },
+            lineStyle: {
+              type: 'solid',
+              width: 1
+            },
+            data: [
+              {
+                yAxis: periodMeanRef
+              }
+            ]
+          }
+        },{
+          name: '本年累计签约合同额',
+          type: 'bar',
+          data: sumContractAmounts,
+          color: '#738496',
+          barMaxWidth: 40,
+          label: {
+            normal: {
+              show: true,
+              position: 'top'
+            }
+          }
+        }]
+    };
+
+    const cp_option = {
+        legend: {
+            data: ['本年累计生产合同额'],
+            align: 'left'
+        },
+        tooltip: {
+          trigger: 'axis',
+          axisPointer: {
+            type: 'cross'
+          }
+        },
+        grid: {
+          left: 80,
+          right: 110,
+          top: 50,
+          bottom: 60
+        },
+        toolbox: {
+          feature: {
+            dataView: {},
+            saveAsImage: {
+                pixelRatio: 2
+            }
+          }
+        },
+        xAxis: {
+          data: cpDepartmentNames,
+          silent: true,
+          axisLabel: {
+            interval: 0,
+            rotate: -40
+          },
+          splitLine: {
+              show: false
+          }
+        },
+        yAxis: [{
+          type: 'value',
+          name: '本年累计生产合同额（万元）',
+          position: 'left',
+          axisLabel: {
+            formatter: '{value}万'
+          }
+        }],
+        series: [{
+          name: '本年累计生产合同额',
+          type: 'bar',
+          data: cpContractAmounts,
+          color: '#738496',
+          barMaxWidth: 40,
+          label: {
+            normal: {
+              show: true,
+              position: 'top'
+            }
+          }
+        }]
+    };
+
+
+    const option_avg = {
+        legend: {
+            data: ['本年累计人均生产合同额'],
+            align: 'left'
+        },
+        tooltip: {
+          trigger: 'axis',
+          axisPointer: {
+            type: 'cross'
+          }
+        },
+        grid: {
+          left: 80,
+          right: 110,
+          top: 50,
+          bottom: 60
+        },
+        toolbox: {
+          feature: {
+            dataView: {},
+            saveAsImage: {
+                pixelRatio: 2
+            }
+          }
+        },
+        xAxis: {
+          data: cpDepartmentNames,
+          silent: true,
+          axisLabel: {
+            interval: 0,
+            rotate: -40
+          },
+          splitLine: {
+              show: false
+          }
+        },
+        yAxis: [{
+          type: 'value',
+          name: '本年累计人均生产合同额（万元）',
+          position: 'left',
+          axisLabel: {
+            formatter: '{value}万'
+          }
+        }],
+        series: [{
+          name: '本年累计人均生产合同额',
+          type: 'bar',
+          data: contractAmountsPerStaffWithColor,
+          barMaxWidth: 40,
+          label: {
+            normal: {
+              show: true,
+              position: 'top'
+            }
+          },
+          markLine: {
+            label: {
+              formatter: '人均合同额预警'
+            },
+            lineStyle: {
+              type: 'solid',
+              width: 1
+            },
+            data: [
+              {
+                yAxis: contractAmountsPerStaffRef
+              }
+            ]
+          }
+        }]
+    };
+
     function drill_down_on_click(params) {
       if (params.componentType === 'series') {
         const series_department = xAxisData[params.dataIndex]
@@ -297,8 +303,10 @@ var option_avg = {
       }
     }
 
-    subsidiaryContractSigningsChart.setOption(option, false);
-    subsidiaryContractSigningsChart.on('click', drill_down_on_click);
+    if (inIFrame != "true") {
+      subsidiaryContractSigningsChart.setOption(option, false);
+      subsidiaryContractSigningsChart.on('click', drill_down_on_click);
+    }
 
     function cp_drill_down(params) {
       if (params.componentType === 'series') {
@@ -325,20 +333,26 @@ var option_avg = {
     subsidiaryContractSigningsAvgChart.setOption(option_avg, false);
 
     setTimeout(() => {
-      subsidiaryContractSigningsChart.resize();
+      if (inIFrame != "true") {
+        subsidiaryContractSigningsChart.resize();
+      }
       subsidiaryContractProductionChart.resize();
       subsidiaryContractSigningsAvgChart.resize();
     }, 200);
   }
 
   layout() {
-    subsidiaryContractSigningsChart.resize();
+    if (inIFrame != "true") {
+      subsidiaryContractSigningsChart.resize();
+    }
     subsidiaryContractProductionChart.resize();
     subsidiaryContractSigningsAvgChart.resize();
   }
 
   disconnect() {
-    subsidiaryContractSigningsChart.dispose();
+    if (inIFrame != "true") {
+      subsidiaryContractSigningsChart.dispose();
+    }
     subsidiaryContractProductionChart.dispose();
     subsidiaryContractSigningsAvgChart.dispose();
   }
