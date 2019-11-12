7c478fefa706ec306a51f9573279ddee6e2576c4
Eric-Guo <eric.guocz@gmail.com>
2019-11-12 09:44:07 +0800

    Show user name in CIM tools.

--- app/datatables/cad_session_datatable.rb
+++ app/datatables/cad_session_datatable.rb
@@ -10,12 +10,10 @@ class CadSessionDatatable < ApplicationDatatable
 
   def view_columns
     @view_columns ||= {
+      user_name: { source: "User.chinese_name", cond: :string_eq, searchable: true, orderable: true },
       session: { source: "Cad::CadSession.session", cond: :string_eq, searchable: true, orderable: true },
-      begin_operation: { source: "Cad::CadSession.begin_operation", cond: :string_eq, searchable: true, orderable: true },
       operation: { source: "Cad::CadSession.operation", cond: :string_eq, searchable: true, orderable: true },
-      end_operation: { source: "Cad::CadSession.end_operation", cond: :string_eq, searchable: true, orderable: true },
-      ip_address: { source: "Cad::CadSession.ip_address", cond: :like, searchable: true, orderable: true },
-      mac_address: { source: "Cad::CadSession.mac_address", cond: :like, orderable: true },
+      ip_mac_address: { source: "Cad::CadSession.ip_address", cond: :like, searchable: true, orderable: true },
       created_at: { source: "Cad::CadSession.created_at", orderable: true },
       updated_at: { source: "Cad::CadSession.updated_at", orderable: true }
     }
@@ -23,12 +21,10 @@ class CadSessionDatatable < ApplicationDatatable
 
   def data
     records.map do |r|
-      { session: r.session,
-        begin_operation: r.begin_operation,
+      { user_name: r.user.chinese_name,
+        session: r.session,
         operation: r.operation,
-        end_operation: "#{r.end_operation}".html_safe,
-        ip_address: r.ip_address,
-        mac_address: tag.div(r.mac_address, class: "text-center"),
+        ip_mac_address: "#{r.ip_address}#{tag.div(r.mac_address, class: "text-center")}".html_safe,
         created_at: tag.div(r.created_at, class: "text-center"),
         updated_at: tag.div(r.updated_at, class: "text-center")
      }
@@ -36,6 +32,6 @@ class CadSessionDatatable < ApplicationDatatable
   end
 
   def get_raw_records
-    @cad_sessions
+    @cad_sessions.includes(:user)
   end
 end
--- app/javascript/controllers/cim_tools_sessions_controller.js
+++ app/javascript/controllers/cim_tools_sessions_controller.js
@@ -3,12 +3,10 @@ import { Controller } from "stimulus"
 export default class extends Controller {
   connect() {
     const normalColumns = [
+      {"data": "user_name"},
       {"data": "session"},
-      {"data": "begin_operation"},
       {"data": "operation"},
-      {"data": "end_operation"},
-      {"data": "ip_address"},
-      {"data": "mac_address"},
+      {"data": "ip_mac_address"},
       {"data": "created_at"},
       {"data": "updated_at"}
     ];
@@ -20,7 +18,7 @@ export default class extends Controller {
       "ajax": $('#cim-tools-sessions-datatable').data('source'),
       "pagingType": "full_numbers",
       "columns": normalColumns,
-      "order": [[ 6, 'DESC' ]],
+      "order": [[ 3, 'DESC' ]],
       stateSave: true,
       stateSaveCallback: function(settings, data) {
           localStorage.setItem('DataTables_cim-tools-sessions', JSON.stringify(data));
