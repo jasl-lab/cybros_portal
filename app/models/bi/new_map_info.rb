9e438fc4e31fc4eea36833659e29f333d22e7e19
Eric-Guo <eric.guocz@gmail.com>
2019-11-19 14:04:34 +0800

    Now able to access contract documents via has_many.

--- app/models/bi/new_map_info.rb
+++ app/models/bi/new_map_info.rb
@@ -3,6 +3,7 @@
 module Bi
   class NewMapInfo < BiLocalTimeRecord
     self.table_name = "V_TH_NEWMAPINFO"
+    has_many :rels, class_name: "Bi::NewMapInfoRel", primary_key: :id, foreign_key: :projectcode
 
     def self.all_cities
       @_all_cities ||= Bi::NewMapInfo.order(company: :asc).select(:company).distinct.pluck(:company)
---
+++ app/models/bi/new_map_info_rel.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+module Bi
+  class NewMapInfoRel < BiLocalTimeRecord
+    self.table_name = "V_TH_NEWMAPINFO_REL"
+  end
+end
