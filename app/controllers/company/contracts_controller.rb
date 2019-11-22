99288f951f79a1e2774ac76a2461077e4220f996
Eric-Guo <eric.guocz@gmail.com>
2019-11-22 13:49:08 +0800

    Show mark point in map.

--- app/controllers/company/contracts_controller.rb
+++ app/controllers/company/contracts_controller.rb
@@ -17,15 +17,23 @@ class Company::ContractsController < ApplicationController
     @tracestate = params[:tracestate].presence || '跟踪中'
     @query_text = params[:query_text].presence
 
-    @map_infos = Bi::NewMapInfo.where(tracestate: @tracestate).where(company: @city).where.not(coordinate: nil)
-    @map_infos = @map_infos.where("developercompanyname LIKE ?", "%#{@client}%") if @client.present?
-    total_cos = @map_infos.collect(&:coordinate).reject {|c| !c.include?(',')}
+    map_infos = Bi::NewMapInfo.where(tracestate: @tracestate).where(company: @city).where.not(coordinate: nil)
+    map_infos = map_infos.where("developercompanyname LIKE ?", "%#{@client}%") if @client.present?
+
+    @valid_map_infos = map_infos.reject {|m| !m.coordinate.include?(',')}
+
+    total_cos = @valid_map_infos.collect(&:coordinate)
+    @valid_map_point = @valid_map_infos.collect do |m|
+      { title: m.marketinfoname,
+        lat: m.coordinate.split(',')[1].to_f,
+        lng: m.coordinate.split(',')[0].to_f,
+        owner: m.maindeptnamedet }
+    end
+
     lat_total_cos = total_cos.collect { |c| c.split(',')[1].to_f }
     lng_total_cos = total_cos.collect { |c| c.split(',')[0].to_f }
-    @min_lat = lat_total_cos.min
-    @max_lat = lat_total_cos.max
-    @min_lng = lng_total_cos.min
-    @max_lng = lng_total_cos.max
+    @avg_lat = lat_total_cos.sum / lat_total_cos.size.to_f
+    @avg_lng = lng_total_cos.sum / lng_total_cos.size.to_f
   end
 
   protected
--- app/javascript/controllers/full_map_controller.js
+++ app/javascript/controllers/full_map_controller.js
@@ -1,18 +1,53 @@
 import { Controller } from "stimulus"
 
 window.initFullMap = function () {
-  const minLat = $('#full-map').data("full-map-min_lat");
-  const maxLat = $('#full-map').data("full-map-max_lat");
-  const minLng = $('#full-map').data("full-map-min_lng");
-  const maxLng = $('#full-map').data("full-map-max_lng");
+  const mapPoint = $('#full-map').data("full-map-map_point");
+  const avgLat = $('#full-map').data("full-map-avg_lat");
+  const avgLng = $('#full-map').data("full-map-avg_lng");
 
-  const sw = new TMap.LatLng(minLat, minLng);
-  const ne = new TMap.LatLng(maxLat, maxLng);
-  const map_bound = new TMap.LatLngBounds(sw, ne);
+  const center = new TMap.LatLng(avgLat,avgLng); //设置中心点坐标
 
   window.full_map = new TMap.Map("full-map", {
-    boundary: map_bound
+    center
   });
+
+  const geometries = mapPoint.map(function(m) {
+    return { styleId: 'marker',
+      position: new TMap.LatLng(m.lat, m.lng),
+      properties: {
+        title: m.title,
+        owner: m.owner
+      }
+    };
+  });
+
+  var marker = new TMap.MultiMarker({
+      id: 'marker-layer',
+      map: window.full_map,
+      styles: {
+          "marker": new TMap.MarkerStyle({
+              "width": 25,
+              "height": 35,
+              "anchor": { x: 16, y: 32 },
+              "src": '/images/marker_default.png'
+          })
+      },
+      geometries: geometries
+  });
+
+  var infoWindow = new TMap.InfoWindow({
+      map: window.full_map,
+      position: new TMap.LatLng(31.228177,121.487003),
+      offset: { x: -8, y: -32 } //设置信息窗相对position偏移像素
+  });
+  infoWindow.close();//初始关闭信息窗关闭
+  //监听标注点击事件
+  marker.on("click", function (evt) {
+      //设置infoWindow
+      infoWindow.open(); //打开信息窗
+      infoWindow.setPosition(evt.geometry.position);//设置信息窗位置
+      infoWindow.setContent(evt.geometry.properties.title.toString());//设置信息窗内容
+  })
 }
 
 export default class extends Controller {
