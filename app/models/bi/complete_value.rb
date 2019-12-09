4ad7e4d816889361936eef7bda0615e610d656a1
Eric-Guo <eric.guocz@gmail.com>
2019-12-09 16:43:02 +0800

    Based on the month date instead of date(which date record different batch of date.)

--- app/models/bi/complete_value.rb
+++ app/models/bi/complete_value.rb
@@ -5,7 +5,7 @@ module Bi
     self.table_name = "COMPLETE_VALUE"
 
     def self.all_month_names
-      Bi::CompleteValue.order(date: :asc).pluck(:date).collect { |d| d.to_s(:month_and_year) }.uniq
+      Bi::CompleteValue.order(month: :asc).pluck(:month).collect { |d| d.to_s(:month_and_year) }.uniq
     end
 
     def self.last_available_date(end_of_month)
