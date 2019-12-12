be68b2e0be87ecf76e4d6a4a4e2842d5bad3c775
Eric-Guo <eric.guocz@gmail.com>
2019-12-12 15:21:47 +0800

    Sales contract details.

--- app/controllers/company/sales_contracts_controller.rb
+++ app/controllers/company/sales_contracts_controller.rb
@@ -5,6 +5,7 @@ class Company::SalesContractsController < ApplicationController
   before_action :set_page_layout_data, if: -> { request.format.html? }
 
   def show
+    @sc = Bi::SaContract.find_by salescontractid: params[:id]
   end
 
   protected
