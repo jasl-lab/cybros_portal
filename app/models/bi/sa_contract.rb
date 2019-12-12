dcee4739d3de75670c06d3476809823b318782c3
Eric-Guo <eric.guocz@gmail.com>
2019-12-12 16:44:56 +0800

    Contract pay condition

--- app/models/bi/sa_contract.rb
+++ app/models/bi/sa_contract.rb
@@ -5,5 +5,6 @@ module Bi
     self.table_name = "V_TH_SACONTRACT"
 
     has_many :prices, class_name: "Bi::SaContractPrice", primary_key: :salescontractid, foreign_key: :salescontractid
+    has_many :pay_plans, class_name: "Bi::SaContractPayPlan", primary_key: :salescontractid, foreign_key: :salescontractid
   end
 end
---
+++ app/models/bi/sa_contract_pay_plan.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+module Bi
+  class SaContractPayPlan < BiLocalTimeRecord
+    self.table_name = "V_TH_SACONTRACTCOLLPLAN"
+  end
+end
