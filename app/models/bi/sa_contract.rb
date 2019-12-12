c6d34f884243b781ff5bab5240dedcca8ba3afc5
Eric-Guo <eric.guocz@gmail.com>
2019-12-12 16:19:40 +0800

    Display 分项单价 in tables.

--- app/models/bi/sa_contract.rb
+++ app/models/bi/sa_contract.rb
@@ -3,5 +3,7 @@
 module Bi
   class SaContract < BiLocalTimeRecord
     self.table_name = "V_TH_SACONTRACT"
+
+    has_many :prices, class_name: "Bi::SaContractPrice", primary_key: :salescontractid, foreign_key: :salescontractid
   end
 end
---
+++ app/models/bi/sa_contract_price.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+module Bi
+  class SaContractPrice < BiLocalTimeRecord
+    self.table_name = "V_TH_SACONTRACTPRICE"
+  end
+end
