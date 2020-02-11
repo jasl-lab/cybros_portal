b0319bd5effcccb8095cacf637128a93f44263e6
Eric-Guo <eric.guocz@gmail.com>
2020-02-11 16:05:10 +0800

    Only fix at Dec month.

--- app/controllers/report/subsidiary_complete_values_controller.rb
+++ app/controllers/report/subsidiary_complete_values_controller.rb
@@ -58,10 +58,10 @@ class Report::SubsidiaryCompleteValuesController < Report::BaseController
     @complete_value_totals = data.collect { |d| (d.sum_total / 10000.0).round(0) }
     @sum_complete_value_totals = (@complete_value_totals.sum / 10000.0).round(1)
     @complete_value_year_totals = @complete_value_totals.collect { |d| (d / (@end_of_month.month / 12.0)).round(0) }
-    @sum_complete_value_year_totals = if orgcode == "000101"
+    @sum_complete_value_year_totals = if orgcode == '000101' && @end_of_month.month == 12
       (policy_scope(Bi::CompleteValueDept).where(orgcode: orgcode)
         .where(month: @end_of_month.beginning_of_year..@end_of_month).where(date: last_available_date)
-        .select("SUM(total) sum_total").first.sum_total / 10000_0000.0).round(1)
+        .select('SUM(total) sum_total').first.sum_total / 10000_0000.0).round(1)
     else
       (@complete_value_year_totals.sum / 10000.0).round(1)
     end
