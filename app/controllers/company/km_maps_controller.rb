e317b8c910ec4ef8b399bb84af37372fdef71dff
Eric-Guo <eric.guocz@gmail.com>
2020-03-02 13:12:48 +0800

    Add select box for km_maps

--- app/controllers/company/km_maps_controller.rb
+++ app/controllers/company/km_maps_controller.rb
@@ -4,5 +4,10 @@ class Company::KmMapsController < ApplicationController
   before_action :authenticate_user!
 
   def show
+    @biz_category = params[:biz_category]&.strip
+    @prj_category = params[:prj_category]&.strip
+    @department = params[:department]&.strip
+    @service_stage = params[:service_stage]&.strip
+    @scale = params[:scale]&.strip
   end
 end
