581a2cf02137e8e52958982043546e6f46faddc8
Eric-Guo <eric.guocz@gmail.com>
2020-03-03 14:54:28 +0800

    No combined 武汉天华 start with H*

--- app/models/bi/org_short_name.rb
+++ app/models/bi/org_short_name.rb
@@ -5,42 +5,42 @@ module Bi
     self.table_name = "ORG_SHORTNAME"
 
     def self.company_short_names
-      @company_short_names ||= where(isbusinessunit: "Y").reduce({}) do |h, s|
+      @company_short_names ||= where(isbusinessunit: "Y").where.not(code: 'H000109').reduce({}) do |h, s|
         h[s.name] = s.shortname
         h
       end
     end
 
     def self.company_long_names
-      @company_long_names ||= where(isbusinessunit: "Y").reduce({}) do |h, s|
+      @company_long_names ||= where(isbusinessunit: "Y").where.not(code: 'H000109').reduce({}) do |h, s|
         h[s.shortname] = s.name
         h
       end
     end
 
     def self.company_short_names_by_orgcode
-      @company_short_names_by_orgcode ||= where(isbusinessunit: "Y").reduce({}) do |h, s|
+      @company_short_names_by_orgcode ||= where(isbusinessunit: "Y").where.not(code: 'H000109').reduce({}) do |h, s|
         h[s.code] = s.shortname
         h
       end
     end
 
     def self.company_long_names_by_orgcode
-      @company_long_names_by_orgcode ||= where(isbusinessunit: "Y").reduce({}) do |h, s|
+      @company_long_names_by_orgcode ||= where(isbusinessunit: "Y").where.not(code: 'H000109').reduce({}) do |h, s|
         h[s.code] = s.name
         h
       end
     end
 
     def self.org_code_by_long_name
-      @org_code_by_long_name ||= where(isbusinessunit: "Y").reduce({}) do |h, s|
+      @org_code_by_long_name ||= where(isbusinessunit: "Y").where.not(code: 'H000109').reduce({}) do |h, s|
         h[s.name] = s.code
         h
       end
     end
 
     def self.org_code_by_short_name
-      @org_code_by_short_name ||= where(isbusinessunit: "Y").reduce({}) do |h, s|
+      @org_code_by_short_name ||= where(isbusinessunit: "Y").where.not(code: 'H000109').reduce({}) do |h, s|
         h[s.shortname] = s.code
         h
       end
