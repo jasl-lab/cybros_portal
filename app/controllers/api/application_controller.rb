e4b022255be8297e66221c24faec3191f7559e1d
Eric-Guo <eric.guocz@gmail.com>
2020-03-17 13:55:38 +0800

    Create user also when user is missing.

--- app/controllers/api/application_controller.rb
+++ app/controllers/api/application_controller.rb
@@ -25,10 +25,37 @@ module API
       jti = white_jwts_attrs[:jti]
       aud = white_jwts_attrs[:aud]
       exp = white_jwts_attrs[:exp]
-      user = User.find_by(email: email)
-      if user.present?
-        user.whitelisted_jwts.create(jti: jti, aud: aud, exp: exp)
+
+      user = User.find_or_initialize_by(email: email)
+      user.position_title = params[:position_title]
+      user.clerk_code = params[:clerk_code]
+      user.chinese_name = params[:chinese_name]
+      user.job_level = params[:job_level]
+      user.desk_phone = params[:desk_phone]
+      user.confirmed_at = Time.current
+      if user.new_record?
+        random_password = SecureRandom.hex(4) # like "301bccce"
+        user.password = random_password
+        user.password_confirmation = random_password
       end
+
+      user.save
+
+      params[:departments].each do |d|
+        id = d[:id]
+        department_name = d[:name]
+        dept_code = d[:dept_code]
+        company_name = d[:company_name]
+        company_code = d[:company_code]
+
+        dep = Department.find_or_create_by(id: id) do |department|
+          department.name = department_name
+        end
+        dep.update(company_name: company_name, company_code: company_code, name: department_name, dept_code: dept_code)
+        DepartmentUser.find_or_create_by!(user_id: user.id, department_id: dep.id)
+      end
+
+      user.whitelisted_jwts.create(jti: jti, aud: aud, exp: exp)
       head :ok
     end
   end
