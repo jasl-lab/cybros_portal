e58dfe58213a9845d2f870f9118d436cff9eba43
Eric-Guo <eric.guocz@gmail.com>
2020-03-27 17:07:05 +0800

    Form structure of the 子公司经营情况对比

--- app/controllers/report/subsidiaries_operating_comparisons_controller.rb
+++ app/controllers/report/subsidiaries_operating_comparisons_controller.rb
@@ -6,6 +6,21 @@ class Report::SubsidiariesOperatingComparisonsController < Report::BaseControlle
   before_action :set_breadcrumbs, only: %i[show], if: -> { request.format.html? }
 
   def show
+    @year_options = policy_scope(Bi::YearReportHistory).year_options
+    @year_names = params[:year_names]
+    @month_names = policy_scope(Bi::YearReportHistory).month_names
+    @month_name = params[:month_name]&.strip || @month_names.first
+    @orgs_options = params[:orgs]
+
+
+    @year_names = @year_options if @year_names.blank?
+    data = policy_scope(Bi::YearReportHistory).where(year: @year_names, month: @month_name).order(:year)
+
+    all_company_orgcodes = data.collect(&:orgcode)
+    all_company_short_names = all_company_orgcodes.collect { |c| Bi::OrgShortName.company_short_names_by_orgcode.fetch(c, c) }
+
+    @orgs_options = all_company_orgcodes - ['000103'] if @orgs_options.blank? # hide 天华节能
+    @organization_options = all_company_short_names.zip(all_company_orgcodes)
   end
 
   private
