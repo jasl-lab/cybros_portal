bd64edbff59d3545d0c8157b48491d45aa35f029
Eric-Guo <eric.guocz@gmail.com>
2020-04-07 15:38:18 +0800

    Need not fetch the PK_ORG IS NULL record.

--- app/models/bi/org_short_name.rb
+++ app/models/bi/org_short_name.rb
@@ -5,45 +5,49 @@ module Bi
     self.table_name = "ORG_SHORTNAME"
 
     def self.company_short_names
-      @company_short_names ||= where(isbusinessunit: 'Y').reduce({}) do |h, s|
+      @company_short_names ||= available_unit.reduce({}) do |h, s|
         h[s.name] = s.shortname
         h
       end
     end
 
     def self.company_long_names
-      @company_long_names ||= where(isbusinessunit: 'Y').reduce({}) do |h, s|
+      @company_long_names ||= available_unit.reduce({}) do |h, s|
         h[s.shortname] = s.name
         h
       end
     end
 
     def self.company_short_names_by_orgcode
-      @company_short_names_by_orgcode ||= where(isbusinessunit: 'Y').reduce({}) do |h, s|
+      @company_short_names_by_orgcode ||= available_unit.reduce({}) do |h, s|
         h[s.code] = s.shortname
         h
       end
     end
 
     def self.company_long_names_by_orgcode
-      @company_long_names_by_orgcode ||= where(isbusinessunit: 'Y').reduce({}) do |h, s|
+      @company_long_names_by_orgcode ||= available_unit.reduce({}) do |h, s|
         h[s.code] = s.name
         h
       end
     end
 
     def self.org_code_by_long_name
-      @org_code_by_long_name ||= where(isbusinessunit: 'Y').reduce({}) do |h, s|
+      @org_code_by_long_name ||= available_unit.reduce({}) do |h, s|
         h[s.name] = s.code
         h
       end
     end
 
     def self.org_code_by_short_name
-      @org_code_by_short_name ||= where(isbusinessunit: 'Y').reduce({}) do |h, s|
+      @org_code_by_short_name ||= available_unit.reduce({}) do |h, s|
         h[s.shortname] = s.code
         h
       end
+    end
+
+    def self.available_unit
+      where(isbusinessunit: 'Y').where('PK_ORG IS NOT NULL')
     end
   end
 end
