1c2000e2f9e2095da0c0336132686e1cc90afae4
Eric-Guo <eric.guocz@gmail.com>
2020-04-17 11:24:39 +0800

    Review show detail dialog.

--- app/controllers/company/km_maps_controller.rb
+++ app/controllers/company/km_maps_controller.rb
@@ -45,7 +45,7 @@ class Company::KmMapsController < ApplicationController
 
   def show_model
     @project_info = Edoc2::ProjectInfo.find_by projectitemcode: params[:project_item_code]
-    @sas = Bi::SaContract.where(projectcode: @project_info.projectcode)
+    @project_items = Edoc2::ProjectInfo.where(projectcode: @project_info.projectcode)
   end
 
   def fill_department
