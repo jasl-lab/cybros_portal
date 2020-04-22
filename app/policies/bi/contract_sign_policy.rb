0192f298b05936283d9b59b868d4633105912375
Eric-Guo <eric.guocz@gmail.com>
2020-04-22 14:52:13 +0800

    Another review.

--- app/policies/bi/contract_sign_policy.rb
+++ app/policies/bi/contract_sign_policy.rb
@@ -2,13 +2,17 @@ module Bi
   class ContractSignPolicy < BasePolicy
     class Scope < Scope
       def resolve
-        scope.all
+        if user.admin?
+          scope.all
+        else
+          scope.none
+        end
       end
     end
 
     def show?
       return false unless user.present?
-      user.roles.pluck(:report_view_all).any? || user.admin?
+      user.admin?
     end
   end
 end
