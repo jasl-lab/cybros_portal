f700a69fc07cd677175392015255635c37d96fc1
Eric-Guo <eric.guocz@gmail.com>
2020-04-22 14:45:23 +0800

    Review 集团累计合同额 access.

--- app/policies/bi/contract_sign_policy.rb
+++ app/policies/bi/contract_sign_policy.rb
@@ -8,8 +8,7 @@ module Bi
 
     def show?
       return false unless user.present?
-      user.roles.pluck(:report_viewer).any? || user.roles.pluck(:report_view_all).any? \
-      || user.job_level.to_i >= 11 || user.admin?
+      user.roles.pluck(:report_view_all).any? || user.admin?
     end
   end
 end
