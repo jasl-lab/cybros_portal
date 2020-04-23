80e25e518da3154a366d5ad9008f820786bd704d
Eric-Guo <eric.guocz@gmail.com>
2020-04-23 14:28:31 +0800

    Review the SQL.

--- app/controllers/company/km_maps_controller.rb
+++ app/controllers/company/km_maps_controller.rb
@@ -16,9 +16,7 @@ class Company::KmMapsController < ApplicationController
     @available_departments = Edoc2::ProjectInfo.project_item_dept_name(@company_name)
 
     data = Edoc2::ProjectInfo.where.not(coordinate: nil)
-      .where.not(projectitemcode: %w[TH20024401 TH20024501 TH20047001 TH20024601])
-      .where.not("projectsort IN ('工程', '采购') AND businesstypename in ('室内')")
-      .where.not("projectsort IN ('工程') AND businesstypename in ('景观')")
+      .where("iscontractcode='Y' AND businesstype IN('01', '02', '03', '04') AND NOT (businesstype='03' and projectsort in('工程','采购')) AND NOT (businesstype='04' and projectsort in('工程')) OR (projectbigstage='30' AND isboutiqueproject='是')")
     data = data.where(businesstypename: @biz_category) if @biz_category.present?
     data = data.where(projectsort: @prj_category) if @prj_category.present?
     data = data.where('clientname like ?', "%#{@client}%") if @client.present?
