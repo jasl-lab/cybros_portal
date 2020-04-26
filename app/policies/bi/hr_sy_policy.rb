85fd292bab0588b3bde6c9b69ef17754c4d00c5e
Eric-Guo <eric.guocz@gmail.com>
2020-04-26 15:59:54 +0800

    Review access.

--- app/policies/bi/hr_sy_policy.rb
+++ app/policies/bi/hr_sy_policy.rb
@@ -4,7 +4,7 @@ module Bi
   class HrSyPolicy < Struct.new(:user, :dashboard)
     def show?
       user.present? &&
-        (user.roles.pluck(:hr_report_admin).any? || user.roles.pluck(:hr_report_viewer).any? || user.roles.pluck(:hr_report_writer).any? || user.admin?)
+        (user.roles.pluck(:hr_report_admin).any? || user.admin?)
     end
   end
 end
--- app/policies/bi/rt_group_subsidiary_hr_monthly_policy.rb
+++ app/policies/bi/rt_group_subsidiary_hr_monthly_policy.rb
@@ -4,7 +4,7 @@ module Bi
   class RtGroupSubsidiaryHrMonthlyPolicy < Struct.new(:user, :dashboard)
     def show?
       user.present? &&
-        (user.roles.pluck(:hr_report_admin).any? || user.admin?)
+        (user.roles.pluck(:hr_report_writer).any? || user.roles.pluck(:hr_report_admin).any? || user.admin?)
     end
   end
 end
