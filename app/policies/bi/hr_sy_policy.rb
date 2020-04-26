5de8177ce48f2ee174025ee2c4b6a36b6bf0743d
Eric-Guo <eric.guocz@gmail.com>
2020-04-26 10:20:09 +0800

    Another re-order of the HR reports.

---
+++ app/policies/bi/hr_sy_policy.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+module Bi
+  class HrSyPolicy < Struct.new(:user, :dashboard)
+    def show?
+      user.present? &&
+        (user.roles.pluck(:hr_report_admin).any? || user.roles.pluck(:hr_report_viewer).any? || user.roles.pluck(:hr_report_writer).any? || user.admin?)
+    end
+  end
+end
