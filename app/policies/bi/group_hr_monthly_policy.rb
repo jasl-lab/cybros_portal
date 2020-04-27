ed8664478548f6a815603108f120c44ad4172c21
Eric-Guo <eric.guocz@gmail.com>
2020-04-27 11:37:14 +0800

    Review access again

--- app/policies/bi/group_hr_monthly_policy.rb
+++ app/policies/bi/group_hr_monthly_policy.rb
@@ -4,7 +4,7 @@ module Bi
   class GroupHrMonthlyPolicy < Struct.new(:user, :dashboard)
     def show?
       user.present? &&
-        (user.roles.pluck(:hr_group_rt_reader).any? || user.admin?)
+        (user.roles.pluck(:hr_group_reader).any? || user.admin?)
     end
   end
 end
