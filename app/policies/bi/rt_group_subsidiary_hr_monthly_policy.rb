1f0943f17466f42b0001b52296f5bb4c108ecb46
Eric-Guo <eric.guocz@gmail.com>
2020-05-08 16:45:07 +0800

    Allow 所长、副所长 to see the sub company HR reports

--- app/policies/bi/rt_group_subsidiary_hr_monthly_policy.rb
+++ app/policies/bi/rt_group_subsidiary_hr_monthly_policy.rb
@@ -6,6 +6,7 @@ module Bi
       user.present? &&
         ( user.roles.pluck(:hr_subsidiary_rt_reader).any? ||
           (user.job_level.to_i >= 17 && (user.position_title.include?('董事长') || user.position_title.include?('总经理'))) ||
+          (user.job_level.to_i >= 13 && user.position_title.include?('所长')) ||
           user.admin? )
     end
   end
