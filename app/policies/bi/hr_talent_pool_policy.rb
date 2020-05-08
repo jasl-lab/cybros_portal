7bbc5eac42321868a606eef172afb00139e49597
Eric-Guo <eric.guocz@gmail.com>
2020-05-08 16:42:29 +0800

    Allow 主职兼职中在职的，对应的基准岗位为“总经理”or”董事长“

--- app/policies/bi/hr_talent_pool_policy.rb
+++ app/policies/bi/hr_talent_pool_policy.rb
@@ -4,7 +4,9 @@ module Bi
   class HrTalentPoolPolicy < Struct.new(:user, :dashboard)
     def show?
       user.present? &&
-        (user.roles.pluck(:hr_subsidiary_reader).any? || user.admin?)
+        ( user.roles.pluck(:hr_subsidiary_reader).any? ||
+          (user.job_level.to_i >= 17 && (user.position_title.include?('董事长') || user.position_title.include?('总经理'))) ||
+          user.admin? )
     end
   end
 end
