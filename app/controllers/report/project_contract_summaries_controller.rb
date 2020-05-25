55f6c211673f02630e8ae2f6735ed45b004c19d2
Eric-Guo <eric.guocz@gmail.com>
2020-05-25 16:39:23 +0800

    Finish 4 report entry.

---
+++ app/controllers/report/project_contract_summaries_controller.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+class Report::ProjectContractSummariesController < Report::BaseController
+  before_action :authenticate_user!
+  before_action :set_page_layout_data, if: -> { request.format.html? }
+  before_action :set_breadcrumbs, only: %i[index], if: -> { request.format.html? }
+
+  def show
+    authorize :"Bi::ProjectContractSummary"
+    prepare_meta_tags title: t(".title")
+  end
+
+  protected
+
+  def set_page_layout_data
+    @_sidebar_name = "financial_management"
+  end
+
+  def set_breadcrumbs
+    @_breadcrumbs = [
+    { text: t("layouts.sidebar.application.header"),
+      link: root_path },
+    { text: t("layouts.sidebar.financial_management.header"),
+      link: report_financial_management_path }]
+  end
+end
