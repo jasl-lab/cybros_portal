211cf1224fa35a0602c7e2806c24e4b0a10a37a4
Eric-Guo <eric.guocz@gmail.com>
2020-06-01 10:47:16 +0800

    Using new tables.

---
+++ app/models/bi/work_hours_count_combine.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+module Bi
+  class WorkHoursCountCombine < BiLocalTimeRecord
+    self.table_name = 'WORK_HOURS_COUNT_COMBINE'
+  end
+end
--- app/models/bi/work_hours_count_real.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-module Bi
-  class WorkHoursCountReal < BiLocalTimeRecord
-    self.table_name = 'WORK_HOURS_COUNT_REAL'
-  end
-end
