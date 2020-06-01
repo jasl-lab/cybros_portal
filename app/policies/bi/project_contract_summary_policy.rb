ddfa92780dd2f63e09a5c1878e10afdee4458884
Eric-Guo <eric.guocz@gmail.com>
2020-06-01 13:50:53 +0800

    New people allowed.

--- app/policies/bi/project_contract_summary_policy.rb
+++ app/policies/bi/project_contract_summary_policy.rb
@@ -4,7 +4,7 @@ module Bi
   class ProjectContractSummaryPolicy < Struct.new(:user, :dashboard)
     def show?
       user.present? &&
-        ( user.chinese_name.in?(%w(郭颖杰)) ||
+        ( user.chinese_name.in?(%w(郭颖杰 余慧 陆文娟 周聪睿 蒋磊 吴悠)) ||
           user.admin?)
     end
   end
