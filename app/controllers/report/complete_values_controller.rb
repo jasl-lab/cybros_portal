20090566c8310096c080fb400b11ac992b57708e
Eric-Guo <eric.guocz@gmail.com>
2020-06-07 16:29:49 +0800

    Separate department number table and chart.

--- app/controllers/report/complete_values_controller.rb
+++ app/controllers/report/complete_values_controller.rb
@@ -61,20 +61,27 @@ class Report::CompleteValuesController < Report::BaseController
     @complete_value_year_totals_remain = @complete_value_year_totals.zip(@complete_value_totals).map { |d| d[0] - d[1] }
     @fix_sum_complete_value_year_totals = (@complete_value_year_totals.sum / 100.0).round(1)
 
-    staff_per_orgcode = if @end_of_month.year <= 2020 && @end_of_month.month < 5
+    staff_per_orgcode_by_year = if @end_of_month.year <= 2020 && @end_of_month.month < 5
       Bi::StaffCount.staff_per_orgcode(@end_of_month)
     else
-      Bi::YearAvgStaff.staff_per_dept_code_by_year_and_sum(@end_of_month, @view_orgcode_sum)
+      Bi::YearAvgStaff.staff_per_orgcode_by_date_and_sum(@end_of_month, @view_orgcode_sum)
     end
-    @staff_per_company = staff_per_orgcode.transform_keys { |c| Bi::OrgShortName.company_short_names_by_orgcode.fetch(c, c) }
     @complete_value_totals_per_staff = data.collect do |d|
-      staff_number = staff_per_orgcode.fetch(d.orgcode, Bi::BiLocalTimeRecord::DEFAULT_PEOPLE_NUM)
+      staff_number = staff_per_orgcode_by_year.fetch(d.orgcode, Bi::BiLocalTimeRecord::DEFAULT_PEOPLE_NUM)
       if staff_number.zero?
         0
       else
         (d.sum_total / (staff_number * 10000).to_f).round(0)
       end
     end
+
+    staff_per_orgcode = if @end_of_month.year <= 2020 && @end_of_month.month < 5
+      Bi::StaffCount.staff_per_orgcode(@end_of_month)
+    else
+      Bi::YearAvgStaff.staff_per_orgcode_by_year_and_sum(@end_of_month, @view_orgcode_sum)
+    end
+    @staff_per_company = staff_per_orgcode.transform_keys { |c| Bi::OrgShortName.company_short_names_by_orgcode.fetch(c, c) }
+
     sum_of_complete_value_totals_per_staff = @complete_value_totals_per_staff.sum.to_f
     @fix_avg_complete_value_totals_per_staff = if @complete_value_totals_per_staff.present?
       (sum_of_complete_value_totals_per_staff / @complete_value_totals_per_staff.size).round(0)
