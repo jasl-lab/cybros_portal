b6be9b8362f212a2766090985ed3da06a15cf69d
Eric-Guo <eric.guocz@gmail.com>
2020-06-08 19:49:46 +0800

    Role reviews

--- app/policies/bi/key_customer_detail_policy.rb
+++ app/policies/bi/key_customer_detail_policy.rb
@@ -3,8 +3,11 @@
 module Bi
   class KeyCustomerDetailPolicy < Struct.new(:user, :dashboard)
     def show?
-      user.present? && (user.admin? \
-        || user.roles.pluck(:large_customer_detail_viewser).any?)
+      user.present? && ( user.admin? \
+        || user.roles.pluck(:large_customer_detail_viewser).any? \
+        || ((user.user_company_names & Role::CREATIVE_SECTION).any? && user.position_title.include?('总经理')) \
+        || (user.user_company_short_names.include?('上海天华') && (user.position_title.include?('管理副所长') || user.position_title.include?('所长助理')))
+      )
     end
   end
 end
