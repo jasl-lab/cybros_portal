2d84f41d8df5a8b92a19e36ddeb6d0d579b16dac
Eric-Guo <eric.guocz@gmail.com>
2020-06-11 17:28:33 +0800

    Add back the manual control.

--- app/policies/bi/contract_sign_dept_policy.rb
+++ app/policies/bi/contract_sign_dept_policy.rb
@@ -5,11 +5,15 @@ module Bi
     class Scope < Scope
       def resolve
         return scope.none unless user.present?
-        can_access_org_codes = user.can_access_org_codes + can_access_org_codes
+        can_access_org_codes = user.can_access_org_codes.append(user.user_company_orgcode)
         if user.roles.pluck(:report_view_all).any? || user.admin? || can_access_org_codes == User::ALL_OF_ALL
           scope.all
-        else
+        elsif user.roles.pluck(:report_viewer).any? \
+          || user.roles.pluck(:report_company_detail_viewer).any? \
+          || user.job_level.to_i >= 11
           scope.where(orgcode: can_access_org_codes).or(scope.where(orgcode_sum: can_access_org_codes))
+        else
+          scope.none
         end
       end
     end
