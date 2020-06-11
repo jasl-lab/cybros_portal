47f0c7fe35e0399bd84e2003909a2c6113e2b5e9
Eric-Guo <eric.guocz@gmail.com>
2020-06-11 10:58:40 +0800

    Another user.

--- app/policies/bi/subsidiaries_operating_comparison_policy.rb
+++ app/policies/bi/subsidiaries_operating_comparison_policy.rb
@@ -3,7 +3,7 @@
 module Bi
   class SubsidiariesOperatingComparisonPolicy < Struct.new(:user, :dashboard)
     def show?
-      user.present? && ( user.admin? || %w[蔡钦 聂琳 章静 崔立杰 柳玉进 王旭冉 王俐雯].include?(user.chinese_name) )
+      user.present? && ( user.admin? || %w[蔡钦 聂琳 章静 崔立杰 柳玉进 王旭冉 王俐雯 崔瑶].include?(user.chinese_name) )
     end
   end
 end
