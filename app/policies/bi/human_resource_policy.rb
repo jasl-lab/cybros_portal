2f36864b5f7ae0c6000b9334e4537ecfffa76c9c
Zero_Zeng <1154945484@qq.com>
2020-06-16 16:49:23 +0800

    权限扩展到兼职14级所长和13级管理副所长

--- app/policies/bi/human_resource_policy.rb
+++ app/policies/bi/human_resource_policy.rb
@@ -7,8 +7,18 @@ module Bi
         ( user.roles.pluck(:hr_group_rt_reader).any? ||
           user.roles.pluck(:hr_subsidiary_rt_reader).any? ||
           user.roles.pluck(:hr_report_writer).any? ||
-          (user.job_level.to_i >= 14 && user.position_title.include?('所长')) ||
-          (user.job_level.to_i >= 13 && user.position_title.include?('管理副所长')) ||
+            (user.my_access_codes.any? do |access_code|
+            job_level = access_code[4]
+            title = access_code[3]
+            job_level.to_i >= 14 && title.include?('所长')
+           end
+           ) ||
+          (user.my_access_codes.any? do |access_code|
+            job_level = access_code[4]
+            title = access_code[3]
+            job_level.to_i >= 13 && title.include?('管理副所长')
+           end
+           ) ||
           user.admin?)
     end
   end
