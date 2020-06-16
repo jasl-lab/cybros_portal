c34ccd586a442afd27236b6430f38591edf3d25b
Zero_Zeng <1154945484@qq.com>
2020-06-16 16:20:34 +0800

    权限控到14级所长和13级管理副所长

--- app/policies/bi/human_resource_policy.rb
+++ app/policies/bi/human_resource_policy.rb
@@ -7,7 +7,8 @@ module Bi
         ( user.roles.pluck(:hr_group_rt_reader).any? ||
           user.roles.pluck(:hr_subsidiary_rt_reader).any? ||
           user.roles.pluck(:hr_report_writer).any? ||
-          (user.job_level.to_i >= 13 && user.position_title.include?('所长')) ||
+          (user.job_level.to_i >= 14 && user.position_title.include?('所长')) ||
+          (user.job_level.to_i >= 13 && user.position_title.include?('管理副所长')) ||
           user.admin?)
     end
   end
