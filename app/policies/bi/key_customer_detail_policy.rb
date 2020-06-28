84b118189ae13e5680cd3e21d66d71a2dec673de
Eric-Guo <eric.guocz@gmail.com>
2020-06-28 11:33:42 +0800

    Review rule of key customer detail policy.

--- app/policies/bi/key_customer_detail_policy.rb
+++ app/policies/bi/key_customer_detail_policy.rb
@@ -5,8 +5,8 @@ module Bi
     def show?
       user.present? && ( user.admin? \
         || user.roles.pluck(:large_customer_detail_viewser).any? \
-        || ((user.user_company_names & Role::CREATIVE_SECTION).any? && user.position_title.include?('总经理')) \
-        || (user.user_company_short_names.include?('上海天华') && (user.position_title.include?('管理副所长') || user.position_title.include?('所长助理')))
+        || ((user.user_company_names & Role::CREATIVE_SECTION).any? && user.position_title.include?('总经理') && !user.position_title.include?('总经理助理')) \
+        || (user.user_company_short_names.include?('上海天华') && (user.position_title.include?('管理副所长')))
       )
     end
   end
