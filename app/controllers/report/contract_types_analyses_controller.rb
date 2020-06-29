5f1159b7c4e187e8b2086f878ff7e178973430a5
Eric-Guo <eric.guocz@gmail.com>
2020-06-29 11:10:14 +0800

    Another new chart.

--- app/controllers/report/contract_types_analyses_controller.rb
+++ app/controllers/report/contract_types_analyses_controller.rb
@@ -41,6 +41,14 @@ class Report::ContractTypesAnalysesController < Report::BaseController
     first_10_contract_price_施工图_合同总金额 = contract_price_施工图_合同总金额[0..9]
     @contract_price_施工图_公司 = contract_price_施工图_公司[0..9].append('其他')
     @contract_price_施工图_合同总金额 = first_10_contract_price_施工图_合同总金额.append(total_contract_price_施工图_合同总金额 - first_10_contract_price_施工图_合同总金额.sum)
+
+    sum_scope = Bi::ContractPrice.select('projectstage, projecttype, SUM(realamounttotal) realamounttotal').group(:projectstage, :projecttype)
+    sum_住宅方案 = sum_scope.find { |c| c.projectstage == '前端' && c.projecttype == '土建住宅' }
+    sum_住宅施工图 = sum_scope.find { |c| c.projectstage == '后端' && c.projecttype == '土建住宅' }
+    sum_公建方案 = sum_scope.find { |c| c.projectstage == '前端' && c.projecttype == '土建公建' }
+    sum_公建施工图 = sum_scope.find { |c| c.projectstage == '后端' && c.projecttype == '土建公建' }
+    @contract_price_住宅公建 = %w[住宅方案 住宅施工图 公建方案 公建施工图]
+    @contract_price_住宅公建总金额 = [sum_住宅方案.realamounttotal, sum_住宅施工图.realamounttotal, sum_公建方案.realamounttotal, sum_公建施工图.realamounttotal]
   end
 
   private
--- app/javascript/controllers/contract_types_analysis_controller.js
+++ app/javascript/controllers/contract_types_analysis_controller.js
@@ -2,16 +2,20 @@ import { Controller } from "stimulus"
 
 let contractTypesAnalysisPlanChart;
 let contractTypesAnalysisWorkingDrawingChart;
+let contractTypesResidentialPublicDrawingChart;
 
 export default class extends Controller {
   connect() {
     contractTypesAnalysisPlanChart = echarts.init(document.getElementById('contract-types-analysis-plan-chart'));
     contractTypesAnalysisWorkingDrawingChart = echarts.init(document.getElementById('contract-types-analysis-working-drawing-chart'));
+    contractTypesResidentialPublicDrawingChart = echarts.init(document.getElementById('contract-types-analysis-residential-public-chart'));
 
     const planAxis = JSON.parse(this.data.get("plan_axis"));
     const planAmount = JSON.parse(this.data.get("plan_amount"));
     const workingDrawingAxis = JSON.parse(this.data.get("working_drawing_axis"));
     const workingDrawingAmount = JSON.parse(this.data.get("working_drawing_amount"));
+    const residentialPublicAxis = JSON.parse(this.data.get("residential_public_axis"));
+    const residentialPublicAmount = JSON.parse(this.data.get("residential_public_amount"));
 
     function mapPlan2PieDoughnut(amount, index) {
       return { value: amount, name: planAxis[index] };
@@ -21,8 +25,13 @@ export default class extends Controller {
       return { value: amount, name: workingDrawingAxis[index] };
     }
 
+    function mapResidentialPublic2PieDoughnut(amount, index) {
+      return { value: amount, name: residentialPublicAxis[index] };
+    }
+
     const planData = planAmount.map(mapPlan2PieDoughnut);
     const workingDrawingData = workingDrawingAmount.map(mapWorkingDrawing2PieDoughnut);
+    const residentialPublicData = residentialPublicAmount.map(mapResidentialPublic2PieDoughnut);
 
     const plan_option = {
       tooltip: {
@@ -38,7 +47,7 @@ export default class extends Controller {
           {
               name: '访问来源',
               type: 'pie',
-              radius: ['40%', '70%'],
+              radius: ['30%', '60%'],
               avoidLabelOverlap: true,
               label: {
                   show: true
@@ -46,7 +55,7 @@ export default class extends Controller {
               emphasis: {
                   label: {
                       show: true,
-                      fontSize: '30',
+                      fontSize: '22',
                       fontWeight: 'bold'
                   }
               },
@@ -72,7 +81,7 @@ export default class extends Controller {
           {
               name: '访问来源',
               type: 'pie',
-              radius: ['40%', '70%'],
+              radius: ['30%', '60%'],
               avoidLabelOverlap: true,
               label: {
                   show: true
@@ -80,7 +89,7 @@ export default class extends Controller {
               emphasis: {
                   label: {
                       show: true,
-                      fontSize: '30',
+                      fontSize: '22',
                       fontWeight: 'bold'
                   }
               },
@@ -92,22 +101,59 @@ export default class extends Controller {
       ]
     };
 
+    const residential_public_option = {
+      tooltip: {
+          trigger: 'item',
+          formatter: '{a} <br/>{b}: {c} ({d}%)'
+      },
+      legend: {
+          orient: 'horizontal',
+          data: residentialPublicAxis
+      },
+      series: [
+          {
+              name: '住宅公建比例',
+              type: 'pie',
+              radius: ['30%', '60%'],
+              avoidLabelOverlap: true,
+              label: {
+                  show: true
+              },
+              emphasis: {
+                  label: {
+                      show: true,
+                      fontSize: '22',
+                      fontWeight: 'bold'
+                  }
+              },
+              labelLine: {
+                  show: true
+              },
+              data: residentialPublicData
+          }
+      ]
+    };
+
     contractTypesAnalysisPlanChart.setOption(plan_option, false);
     contractTypesAnalysisWorkingDrawingChart.setOption(working_drawing_option, false);
+    contractTypesResidentialPublicDrawingChart.setOption(residential_public_option, false);
 
     setTimeout(() => {
       contractTypesAnalysisPlanChart.resize();
       contractTypesAnalysisWorkingDrawingChart.resize();
+      contractTypesResidentialPublicDrawingChart.resize();
     }, 200);
   }
 
   layout() {
     contractTypesAnalysisPlanChart.resize();
     contractTypesAnalysisWorkingDrawingChart.resize();
+    contractTypesResidentialPublicDrawingChart.resize();
   }
 
   disconnect() {
     contractTypesAnalysisPlanChart.dispose();
     contractTypesAnalysisWorkingDrawingChart.dispose();
+    contractTypesResidentialPublicDrawingChart.dispose();
   }
 }
