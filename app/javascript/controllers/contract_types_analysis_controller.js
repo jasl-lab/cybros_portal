b0a1ec3abdf1c3113767bc93cf9412c139c2ba87
Zero_Zeng <1154945484@qq.com>
2020-06-30 16:43:26 +0800

    增加2019年住宅公建饼图

--- app/javascript/controllers/contract_types_analysis_controller.js
+++ app/javascript/controllers/contract_types_analysis_controller.js
@@ -3,12 +3,14 @@ import { Controller } from "stimulus"
 let contractTypesAnalysisPlanChart;
 let contractTypesAnalysisWorkingDrawingChart;
 let contractTypesResidentialPublicDrawingChart;
+let contractTypesResidentialPublicDrawingProviousChart;
 
 export default class extends Controller {
   connect() {
     contractTypesAnalysisPlanChart = echarts.init(document.getElementById('contract-types-analysis-plan-chart'));
     contractTypesAnalysisWorkingDrawingChart = echarts.init(document.getElementById('contract-types-analysis-working-drawing-chart'));
     contractTypesResidentialPublicDrawingChart = echarts.init(document.getElementById('contract-types-analysis-residential-public-chart'));
+    contractTypesResidentialPublicDrawingProviousChart = echarts.init(document.getElementById('contract-types-analysis-residential-public-chart-provious'));
 
     const planAxis = JSON.parse(this.data.get("plan_axis"));
     const planAmount = JSON.parse(this.data.get("plan_amount"));
@@ -16,6 +18,8 @@ export default class extends Controller {
     const workingDrawingAmount = JSON.parse(this.data.get("working_drawing_amount"));
     const residentialPublicAxis = JSON.parse(this.data.get("residential_public_axis"));
     const residentialPublicAmount = JSON.parse(this.data.get("residential_public_amount"));
+    const residentialPublicProviousAxis = JSON.parse(this.data.get("residential_public_provious_axis"));
+    const residentialPublicProviousAmount = JSON.parse(this.data.get("residential_public_provious_amount"));
 
     function mapPlan2PieDoughnut(amount, index) {
       return { value: amount, name: planAxis[index] };
@@ -29,9 +33,14 @@ export default class extends Controller {
       return { value: amount, name: residentialPublicAxis[index] };
     }
 
+    function mapResidentialPublicProvious2PieDoughnut(amount, index) {
+      return { value: amount, name: residentialPublicProviousAxis[index] };
+    }
+
     const planData = planAmount.map(mapPlan2PieDoughnut);
     const workingDrawingData = workingDrawingAmount.map(mapWorkingDrawing2PieDoughnut);
     const residentialPublicData = residentialPublicAmount.map(mapResidentialPublic2PieDoughnut);
+    const residentialPublicProviousData = residentialPublicProviousAmount.map(mapResidentialPublicProvious2PieDoughnut);
 
     const plan_option = {
       tooltip: {
@@ -134,14 +143,49 @@ export default class extends Controller {
       ]
     };
 
+    const residential_public_provious_option = {
+      tooltip: {
+          trigger: 'item',
+          formatter: '{a} <br/>{b}: {c} ({d}%)'
+      },
+      legend: {
+          orient: 'horizontal',
+          data: residentialPublicProviousAxis
+      },
+      series: [
+          {
+              name: '住宅公建比例',
+              type: 'pie',
+              radius: ['30%', '60%'],
+              avoidLabelOverlap: true,
+              label: {
+                  show: true
+              },
+              emphasis: {
+                  label: {
+                      show: true,
+                      fontSize: '22',
+                      fontWeight: 'bold'
+                  }
+              },
+              labelLine: {
+                  show: true
+              },
+              data: residentialPublicProviousData
+          }
+      ]
+    };
+
     contractTypesAnalysisPlanChart.setOption(plan_option, false);
     contractTypesAnalysisWorkingDrawingChart.setOption(working_drawing_option, false);
     contractTypesResidentialPublicDrawingChart.setOption(residential_public_option, false);
+    contractTypesResidentialPublicDrawingProviousChart.setOption(residential_public_option, false);
 
     setTimeout(() => {
       contractTypesAnalysisPlanChart.resize();
       contractTypesAnalysisWorkingDrawingChart.resize();
-      contractTypesResidentialPublicDrawingChart.resize();
+      contractTypesResidentialPublicDrawingChart.resize(); 
+      contractTypesResidentialPublicDrawingProviousChart.resize();
     }, 200);
   }
 
@@ -149,11 +193,13 @@ export default class extends Controller {
     contractTypesAnalysisPlanChart.resize();
     contractTypesAnalysisWorkingDrawingChart.resize();
     contractTypesResidentialPublicDrawingChart.resize();
+    contractTypesResidentialPublicDrawingProviousChart.resize();
   }
 
   disconnect() {
     contractTypesAnalysisPlanChart.dispose();
     contractTypesAnalysisWorkingDrawingChart.dispose();
     contractTypesResidentialPublicDrawingChart.dispose();
+    contractTypesResidentialPublicDrawingProviousChart.dispose();
   }
 }
