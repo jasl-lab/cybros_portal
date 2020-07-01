6e4662a44790f0c29a9372eaa157fc0eb7a6f0a9
Zero_Zeng <1154945484@qq.com>
2020-07-01 18:10:01 +0800

    关闭组织架构图权限

--- app/policies/bi/organization_chart_policy.rb
+++ app/policies/bi/organization_chart_policy.rb
@@ -3,9 +3,9 @@
 module Bi
   class OrganizationChartPolicy < Struct.new(:user, :dashboard)
     def show?
-      user.present? && ( user.admin? \
-        || user.roles.pluck(:org_viewer).any?
-      )
+      user.present? && ( user.admin?)
+      #年久失修 不给看 || user.roles.pluck(:org_viewer).any?
+      
     end
   end
 end
