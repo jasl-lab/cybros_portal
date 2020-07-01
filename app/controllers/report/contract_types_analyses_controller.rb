a6fbd24bda1fa5f71b0c16d52254b6dfa32e722e
Eric-Guo <eric.guocz@gmail.com>
2020-07-01 14:19:20 +0800

    Prepare the data.

--- app/controllers/report/contract_types_analyses_controller.rb
+++ app/controllers/report/contract_types_analyses_controller.rb
@@ -45,26 +45,31 @@ class Report::ContractTypesAnalysesController < Report::BaseController
     @contract_price_施工图_公司 = contract_price_施工图_公司[0..9].append('其他')
     @contract_price_施工图_合同总金额 = first_10_contract_price_施工图_合同总金额.append(total_contract_price_施工图_合同总金额 - first_10_contract_price_施工图_合同总金额.sum)
 
-    @contract_price_住宅公建 = %w[住宅方案 住宅施工图 公建方案 公建施工图]
-    @contract_price_住宅公建总金额 = 住宅公建_contract_price(@beginning_of_year, end_of_year)
-
-    @previous_beginning_of_year = Date.ordinal(@year_name.to_i - 1)
-    previous_end_of_year = @previous_beginning_of_year.end_of_year
-    @previous_contract_price_住宅公建总金额 = 住宅公建_contract_price(@previous_beginning_of_year, previous_end_of_year)
+    @years, @years_sum_住宅方案, @years_sum_住宅施工图, @years_sum_公建方案, @years_sum_公建施工图 \
+      = 住宅公建_contract_price(2016, 2020, @orgs_options)
   end
 
   private
 
-    def 住宅公建_contract_price(begin_of_year, end_of_year)
+    def 住宅公建_contract_price(begin_of_year, end_of_year, orgs_options)
       sum_scope = Bi::ContractPrice
-        .select('projectstage, projecttype, SUM(realamounttotal) realamounttotal')
-        .group(:projectstage, :projecttype)
-        .where(filingtime: begin_of_year..end_of_year)
-      sum_住宅方案 = sum_scope.find { |c| c.projectstage == '前端' && c.projecttype == '土建住宅' }
-      sum_住宅施工图 = sum_scope.find { |c| c.projectstage == '后端' && c.projecttype == '土建住宅' }
-      sum_公建方案 = sum_scope.find { |c| c.projectstage == '前端' && c.projecttype == '土建公建' }
-      sum_公建施工图 = sum_scope.find { |c| c.projectstage == '后端' && c.projecttype == '土建公建' }
-      return [sum_住宅方案.realamounttotal, sum_住宅施工图.realamounttotal, sum_公建方案.realamounttotal, sum_公建施工图.realamounttotal]
+        .select('YEAR(filingtime) year_name, projectstage, projecttype, SUM(realamounttotal) realamounttotal')
+        .group('YEAR(filingtime), projectstage, projecttype')
+        .where('YEAR(filingtime) >= ?', begin_of_year)
+        .where('YEAR(filingtime) <= ?', end_of_year)
+        .where(businessltdcode: orgs_options)
+      years_name = (begin_of_year..end_of_year).to_a
+      years_sum_住宅方案 = []
+      years_sum_住宅施工图 = []
+      years_sum_公建方案 = []
+      years_sum_公建施工图 = []
+      years_name.each do |year|
+        years_sum_住宅方案 << sum_scope.find { |c| c.year_name == year && c.projectstage == '前端' && c.projecttype == '土建住宅' }&.realamounttotal
+        years_sum_住宅施工图 << sum_scope.find { |c| c.year_name == year && c.projectstage == '后端' && c.projecttype == '土建住宅' }&.realamounttotal
+        years_sum_公建方案 << sum_scope.find { |c| c.year_name == year && c.projectstage == '前端' && c.projecttype == '土建公建' }&.realamounttotal
+        years_sum_公建施工图 << sum_scope.find { |c| c.year_name == year && c.projectstage == '后端' && c.projecttype == '土建公建' }&.realamounttotal
+      end
+      return [years_name, years_sum_住宅方案, years_sum_住宅施工图, years_sum_公建方案, years_sum_公建施工图]
     end
 
 
--- app/javascript/controllers/contract_types_analysis_controller.js
+++ app/javascript/controllers/contract_types_analysis_controller.js
@@ -2,25 +2,16 @@ import { Controller } from "stimulus"
 
 let contractTypesAnalysisPlanChart;
 let contractTypesAnalysisWorkingDrawingChart;
-let contractTypesResidentialPublicDrawingChart;
-let contractTypesResidentialPublicDrawingProviousChart;
 
 export default class extends Controller {
   connect() {
     contractTypesAnalysisPlanChart = echarts.init(document.getElementById('contract-types-analysis-plan-chart'));
     contractTypesAnalysisWorkingDrawingChart = echarts.init(document.getElementById('contract-types-analysis-working-drawing-chart'));
-    contractTypesResidentialPublicDrawingChart = echarts.init(document.getElementById('contract-types-analysis-residential-public-chart'));
-    contractTypesResidentialPublicDrawingProviousChart = echarts.init(document.getElementById('contract-types-analysis-residential-public-previous-chart'));
 
-    const beginningOfYear = this.data.get("beginning_of_year");
-    const previousBeginningOfYear = this.data.get("previous_beginning_of_year");
     const planAxis = JSON.parse(this.data.get("plan_axis"));
     const planAmount = JSON.parse(this.data.get("plan_amount"));
     const workingDrawingAxis = JSON.parse(this.data.get("working_drawing_axis"));
     const workingDrawingAmount = JSON.parse(this.data.get("working_drawing_amount"));
-    const residentialPublicAxis = JSON.parse(this.data.get("residential_public_axis"));
-    const residentialPublicAmount = JSON.parse(this.data.get("residential_public_amount"));
-    const residentialPublicProviousAmount = JSON.parse(this.data.get("residential_public_provious_amount"));
 
     function mapPlan2PieDoughnut(amount, index) {
       return { value: amount, name: planAxis[index] };
@@ -30,14 +21,8 @@ export default class extends Controller {
       return { value: amount, name: workingDrawingAxis[index] };
     }
 
-    function mapResidentialPublic2PieDoughnut(amount, index) {
-      return { value: amount, name: residentialPublicAxis[index] };
-    }
-
     const planData = planAmount.map(mapPlan2PieDoughnut);
     const workingDrawingData = workingDrawingAmount.map(mapWorkingDrawing2PieDoughnut);
-    const residentialPublicData = residentialPublicAmount.map(mapResidentialPublic2PieDoughnut);
-    const residentialPublicProviousData = residentialPublicProviousAmount.map(mapResidentialPublic2PieDoughnut);
 
     const plan_option = {
       title: {
@@ -115,106 +100,22 @@ export default class extends Controller {
       ]
     };
 
-    const residential_public_option = {
-      title: {
-        text: `${beginningOfYear}土建项目各业务类型合同占比`,
-        left: 'center',
-      },
-      tooltip: {
-          trigger: 'item',
-          formatter: '{a} <br/>{b}: {c} ({d}%)'
-      },
-      legend: {
-        orient: 'horizontal',
-        bottom: 20,
-        data: residentialPublicAxis
-      },
-      series: [
-          {
-              name: '住宅公建比例',
-              type: 'pie',
-              radius: ['30%', '60%'],
-              avoidLabelOverlap: true,
-              label: {
-                  show: true
-              },
-              emphasis: {
-                  label: {
-                      show: true,
-                      fontSize: '22',
-                      fontWeight: 'bold'
-                  }
-              },
-              labelLine: {
-                  show: true
-              },
-              data: residentialPublicData
-          }
-      ]
-    };
-
-    const residential_public_provious_option = {
-      title: {
-        text: `${previousBeginningOfYear}土建项目各业务类型合同占比`,
-        left: 'center',
-      },
-      tooltip: {
-          trigger: 'item',
-          formatter: '{a} <br/>{b}: {c} ({d}%)'
-      },
-      legend: {
-        orient: 'horizontal',
-        bottom: 20,
-        data: residentialPublicAxis
-      },
-      series: [
-          {
-              name: '住宅公建比例',
-              type: 'pie',
-              radius: ['30%', '60%'],
-              avoidLabelOverlap: true,
-              label: {
-                  show: true
-              },
-              emphasis: {
-                  label: {
-                      show: true,
-                      fontSize: '22',
-                      fontWeight: 'bold'
-                  }
-              },
-              labelLine: {
-                  show: true
-              },
-              data: residentialPublicProviousData
-          }
-      ]
-    };
-
     contractTypesAnalysisPlanChart.setOption(plan_option, false);
     contractTypesAnalysisWorkingDrawingChart.setOption(working_drawing_option, false);
-    contractTypesResidentialPublicDrawingChart.setOption(residential_public_option, false);
-    contractTypesResidentialPublicDrawingProviousChart.setOption(residential_public_provious_option, false);
 
     setTimeout(() => {
       contractTypesAnalysisPlanChart.resize();
       contractTypesAnalysisWorkingDrawingChart.resize();
-      contractTypesResidentialPublicDrawingChart.resize();
-      contractTypesResidentialPublicDrawingProviousChart.resize();
     }, 200);
   }
 
   layout() {
     contractTypesAnalysisPlanChart.resize();
     contractTypesAnalysisWorkingDrawingChart.resize();
-    contractTypesResidentialPublicDrawingChart.resize();
-    contractTypesResidentialPublicDrawingProviousChart.resize();
   }
 
   disconnect() {
     contractTypesAnalysisPlanChart.dispose();
     contractTypesAnalysisWorkingDrawingChart.dispose();
-    contractTypesResidentialPublicDrawingChart.dispose();
-    contractTypesResidentialPublicDrawingProviousChart.dispose();
   }
 }
