683e34b9cad7926dbcf078de31772778fb30b08c
Eric-Guo <eric.guocz@gmail.com>
2020-07-02 10:03:59 +0800

    Add charts in category stack

--- app/javascript/controllers/contract_types_analysis_controller.js
+++ app/javascript/controllers/contract_types_analysis_controller.js
@@ -2,16 +2,23 @@ import { Controller } from "stimulus"
 
 let contractTypesAnalysisPlanChart;
 let contractTypesAnalysisWorkingDrawingChart;
+let contractTypesAnalysisYearCategoryStackChart;
 
 export default class extends Controller {
   connect() {
     contractTypesAnalysisPlanChart = echarts.init(document.getElementById('contract-types-analysis-plan-chart'));
     contractTypesAnalysisWorkingDrawingChart = echarts.init(document.getElementById('contract-types-analysis-working-drawing-chart'));
+    contractTypesAnalysisYearCategoryStackChart = echarts.init(document.getElementById('contract-types-analysis-year-category-stack-chart'));
 
     const planAxis = JSON.parse(this.data.get("plan_axis"));
     const planAmount = JSON.parse(this.data.get("plan_amount"));
     const workingDrawingAxis = JSON.parse(this.data.get("working_drawing_axis"));
     const workingDrawingAmount = JSON.parse(this.data.get("working_drawing_amount"));
+    const yearsCategory = JSON.parse(this.data.get("years_category"));
+    const yearsResidentialPlanAmount = JSON.parse(this.data.get("years_residential_plan"));
+    const yearsResidentialConstructionAmount = JSON.parse(this.data.get("years_residential_construction"));
+    const yearsPublicPlanAmount = JSON.parse(this.data.get("years_public_plan"));
+    const yearsPublicConstructionAmount = JSON.parse(this.data.get("years_public_construction"));
 
     function mapPlan2PieDoughnut(amount, index) {
       return { value: amount, name: planAxis[index] };
@@ -100,22 +107,89 @@ export default class extends Controller {
       ]
     };
 
+    const year_category_stack_option = {
+      tooltip: {
+        trigger: 'axis',
+        axisPointer: {
+          type: 'shadow'
+        }
+      },
+      legend: {
+        data: ['住宅方案', '住宅施工图', '公建方案', '公建施工图']
+      },
+      grid: {
+        left: '3%',
+        right: '4%',
+        bottom: '3%',
+        containLabel: true
+      },
+      xAxis: {
+        type: 'value'
+      },
+      yAxis: {
+        type: 'category',
+        data: yearsCategory
+      },
+      series: [{
+          name: '住宅方案',
+          type: 'bar',
+          stack: '总量',
+          label: {
+              show: true,
+              position: 'insideRight'
+          },
+          data: yearsResidentialPlanAmount
+        },{
+          name: '住宅施工图',
+          type: 'bar',
+          stack: '总量',
+          label: {
+              show: true,
+              position: 'insideRight'
+          },
+          data: yearsResidentialConstructionAmount
+        },{
+          name: '公建方案',
+          type: 'bar',
+          stack: '总量',
+          label: {
+              show: true,
+              position: 'insideRight'
+          },
+          data: yearsPublicPlanAmount
+        },{
+          name: '公建施工图',
+          type: 'bar',
+          stack: '总量',
+          label: {
+              show: true,
+              position: 'insideRight'
+          },
+          data: yearsPublicConstructionAmount
+        }
+      ]
+    };
+
     contractTypesAnalysisPlanChart.setOption(plan_option, false);
     contractTypesAnalysisWorkingDrawingChart.setOption(working_drawing_option, false);
+    contractTypesAnalysisYearCategoryStackChart.setOption(year_category_stack_option, false);
 
     setTimeout(() => {
       contractTypesAnalysisPlanChart.resize();
       contractTypesAnalysisWorkingDrawingChart.resize();
+      contractTypesAnalysisYearCategoryStackChart.resize();
     }, 200);
   }
 
   layout() {
     contractTypesAnalysisPlanChart.resize();
     contractTypesAnalysisWorkingDrawingChart.resize();
+    contractTypesAnalysisYearCategoryStackChart.resize();
   }
 
   disconnect() {
     contractTypesAnalysisPlanChart.dispose();
     contractTypesAnalysisWorkingDrawingChart.dispose();
+    contractTypesAnalysisYearCategoryStackChart.dispose();
   }
 }
