898776a0bdaf17ee9d4bcd16c902c71083af45f1
Eric-Guo <eric.guocz@gmail.com>
2020-07-06 17:09:03 +0800

    Still need all_year_names at 合同地域分析

--- app/models/bi/contract_price.rb
+++ app/models/bi/contract_price.rb
@@ -4,6 +4,11 @@ module Bi
   class ContractPrice < BiLocalTimeRecord
     self.table_name = 'CONTRACT_PRICE'
 
+    def self.all_year_names
+      Bi::ContractPrice.order(filingtime: :desc).where('filingtime >= ?', Date.new(2016,1,1))
+        .pluck(:filingtime).collect { |d| d.year }.uniq
+    end
+
     def self.all_month_names
       Bi::ContractPrice.order(filingtime: :desc).where('filingtime >= ?', Date.new(2016,1,1))
         .pluck(:filingtime).collect { |d| d.to_s(:month_and_year) }.uniq
