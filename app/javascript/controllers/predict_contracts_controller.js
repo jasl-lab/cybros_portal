24a40f8d165a4de941c1be06da2dccd360b6b0d6
Eric-Guo <eric.guocz@gmail.com>
2020-07-13 16:41:02 +0800

    Style improvement.

--- app/javascript/controllers/predict_contracts_controller.js
+++ app/javascript/controllers/predict_contracts_controller.js
@@ -6,106 +6,106 @@ export default class extends Controller {
   connect() {
     predictContractChart = echarts.init(document.getElementById('predict-contract-chart'));
 
-var xAxisData = JSON.parse(this.data.get("x_axis"));
-var deptCodes = JSON.parse(this.data.get("dept_codes_as_options"));
-var contractConvert = JSON.parse(this.data.get("contract_convert"));
-var convertRealAmount = JSON.parse(this.data.get("convert_real_amount"));
-var contractConvertTotals = JSON.parse(this.data.get("contract_convert_totals"));
+    const xAxisData = JSON.parse(this.data.get("x_axis"));
+    const deptCodes = JSON.parse(this.data.get("dept_codes_as_options"));
+    const contractConvert = JSON.parse(this.data.get("contract_convert"));
+    const convertRealAmount = JSON.parse(this.data.get("convert_real_amount"));
+    const contractConvertTotals = JSON.parse(this.data.get("contract_convert_totals"));
 
-var option = {
-    legend: {
+    const option = {
+      legend: {
         data: ['跟踪合同额','跟踪合同额（成功率小于80%）','流转中合同额（成功率等于80%）'],
         align: 'left'
-    },
-    tooltip: {
-      trigger: 'axis',
-      axisPointer: {
-        type: 'cross'
-      }
-    },
-    grid: {
-      left: 70,
-      right: 110,
-      top: 50,
-      bottom: 100
-    },
-    toolbox: {
-      feature: {
-        dataView: {},
-        saveAsImage: {
-            pixelRatio: 2
+      },
+      tooltip: {
+        trigger: 'axis',
+        axisPointer: {
+          type: 'cross'
         }
-      }
-    },
-    xAxis: {
-      data: xAxisData,
-      silent: true,
-      axisLabel: {
-        interval: 0,
-        rotate: -40,
-        margin: 24
       },
-      splitLine: {
-          show: false
-      }
-    },
-    yAxis: [{
-      type: 'value',
-      name: '跟踪合同额（万元）',
-      position: 'left',
-      axisLabel: {
-        formatter: '{value}万'
+      grid: {
+        left: 70,
+        right: 110,
+        top: 50,
+        bottom: 100
       },
-      min: 0
-    }],
-    series: [{
-      name: '跟踪合同额（万元）',
-      type: 'bar',
-      barWidth: '30%',
-      barGap: '-100%',
-      data: contractConvertTotals,
-      barWidth: 20,
-      itemStyle: {
-        color: '#DDDDDD'
-      },
-      label: {
-        normal: {
-          show: true,
-          color: '#353535',
-          position: 'top'
+      toolbox: {
+        feature: {
+          dataView: {},
+          saveAsImage: {
+              pixelRatio: 2
+          }
         }
-      }
-    }, {
-      name: '跟踪合同额（成功率小于80%）',
-      type: 'bar',
-      stack: '总量',
-      data: contractConvert,
-      barWidth: 20,
-      itemStyle: {
-        color: '#738496'
       },
-      label: {
-        normal: {
-          show: true,
-          position: 'insideTop'
+      xAxis: {
+        data: xAxisData,
+        silent: true,
+        axisLabel: {
+          interval: 0,
+          rotate: -40,
+          margin: 24
+        },
+        splitLine: {
+            show: false
         }
-      }
-    },{
-      name: '流转中合同额（成功率等于80%）',
-      type: 'bar',
-      stack: '总量',
-      data: convertRealAmount,
-      barWidth: 20,
-      itemStyle: {
-        color: '#334B5C'
       },
-      label: {
-        normal: {
-          show: false
+      yAxis: [{
+        type: 'value',
+        name: '跟踪合同额（万元）',
+        position: 'left',
+        axisLabel: {
+          formatter: '{value}万'
+        },
+        min: 0
+      }],
+      series: [{
+        name: '跟踪合同额（万元）',
+        type: 'bar',
+        barWidth: '30%',
+        barGap: '-100%',
+        data: contractConvertTotals,
+        barWidth: 20,
+        itemStyle: {
+          color: '#DDDDDD'
+        },
+        label: {
+          normal: {
+            show: true,
+            color: '#353535',
+            position: 'top'
+          }
         }
-      }
-    }]
-};
+      },{
+        name: '跟踪合同额（成功率小于80%）',
+        type: 'bar',
+        stack: '总量',
+        data: contractConvert,
+        barWidth: 20,
+        itemStyle: {
+          color: '#738496'
+        },
+        label: {
+          normal: {
+            show: true,
+            position: 'insideTop'
+          }
+        }
+      },{
+        name: '流转中合同额（成功率等于80%）',
+        type: 'bar',
+        stack: '总量',
+        data: convertRealAmount,
+        barWidth: 20,
+        itemStyle: {
+          color: '#334B5C'
+        },
+        label: {
+          normal: {
+            show: false
+          }
+        }
+      }]
+    };
 
     function drill_down_contract_detail(params) {
       if (params.componentType === 'series') {
