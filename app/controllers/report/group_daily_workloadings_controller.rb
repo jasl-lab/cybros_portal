47dcb862b0ca00493da981159513cf14738959e8
Eric-Guo <eric.guocz@gmail.com>
2020-07-13 13:44:51 +0800

    Add JS part of the group_daily_workloading

--- app/controllers/report/group_daily_workloadings_controller.rb
+++ app/controllers/report/group_daily_workloadings_controller.rb
@@ -7,10 +7,53 @@ class Report::GroupDailyWorkloadingsController < Report::BaseController
     authorize Bi::WorkHoursDayCountOrg
     @begin_date = params[:begin_date]&.strip || Time.now.beginning_of_month
     @end_date = params[:end_date]&.strip || Time.now.end_of_day
-    beginning_of_day = Date.parse(@begin_date).beginning_of_day unless @begin_date.is_a?(Time)
-    end_of_day = Date.parse(@end_date).end_of_day unless @end_date.is_a?(Time)
+    beginning_of_day = if @begin_date.is_a?(Time)
+      @begin_date.beginning_of_day
+    else
+      Date.parse(@begin_date).beginning_of_day
+    end
+    end_of_day = if @end_date.is_a?(Time)
+      @end_date.end_of_day
+    else
+      Date.parse(@end_date).end_of_day
+    end
     @view_orgcode_sum = params[:view_orgcode_sum] == "true"
 
+    data = policy_scope(Bi::WorkHoursDayCountOrg)
+      .where(date: beginning_of_day..end_of_day)
+      .where('ORG_ORDER.org_order is not null')
+      .order('ORG_ORDER.org_order DESC')
+
+    data = if @view_orgcode_sum
+      data
+        .select("orgcode_sum orgcode, org_order, SUM(date_real) date_real, SUM(date_need) date_need, SUM(blue_print_real) blue_print_real, SUM(blue_print_need) blue_print_need, SUM(construction_real) construction_real, SUM(construction_need) construction_need")
+        .joins("LEFT JOIN ORG_ORDER on ORG_ORDER.org_code = WORK_HOURS_DAY_COUNT_ORG.orgcode_sum")
+        .group(:orgcode_sum, :org_order)
+    else
+      data
+        .select("orgcode, org_order, SUM(date_real) date_real, SUM(date_need) date_need, SUM(blue_print_real) blue_print_real, SUM(blue_print_need) blue_print_need, SUM(construction_real) construction_real, SUM(construction_need) construction_need")
+        .joins("LEFT JOIN ORG_ORDER on ORG_ORDER.org_code = WORK_HOURS_DAY_COUNT_ORG.orgcode")
+        .group(:orgcode, :org_order)
+    end
+
+    job_data = data.having("SUM(date_need) > 0")
+    blue_print_data = data.having("SUM(blue_print_need)")
+    construction_data = data.having("SUM(construction_need) > 0")
+    @job_company_or_department_names = job_data.collect(&:orgcode).collect { |c| Bi::OrgShortName.company_short_names_by_orgcode.fetch(c, c) }
+    @blue_print_company_or_department_names = blue_print_data.collect(&:orgcode).collect { |c| Bi::OrgShortName.company_short_names_by_orgcode.fetch(c, c) }
+    @construction_company_or_department_names = construction_data.collect(&:orgcode).collect { |c| Bi::OrgShortName.company_short_names_by_orgcode.fetch(c, c) }
+
+    @day_rate = job_data.collect { |d| ((d.date_real / d.date_need.to_f) * 100).round(0) rescue 0 }
+    @day_rate_ref = params[:day_rate_ref] || 95
+
+    @planning_day_rate = blue_print_data.collect { |d| ((d.blue_print_real / d.blue_print_need.to_f) * 100).round(0) rescue 0 }
+    @planning_day_rate_ref = params[:planning_day_rate_ref] || 95
+
+    @building_day_rate = construction_data.collect { |d| ((d.construction_real / d.construction_need.to_f) * 100).round(0) rescue 0 }
+    @building_day_rate_ref = params[:building_day_rate_ref] || 80
+
+    @current_user_companies_short_names = current_user.user_company_short_names +
+      current_user.can_access_org_codes.collect { |c| Bi::OrgShortName.company_short_names_by_orgcode.fetch(c, c) }
   end
 
   private
---
+++ app/javascript/controllers/group_daily_workloadings_controller.js
@@ -0,0 +1,303 @@
+import { Controller } from "stimulus"
+
+let groupDailyWorkloadingsDayChart;
+let groupDailyWorkloadingsPlanningDayChart;
+let groupDailyWorkloadingsBuildingDayChart;
+
+export default class extends Controller {
+  connect() {
+    groupDailyWorkloadingsDayChart = echarts.init(document.getElementById('group-daily-workloadings-day-chart'));
+    groupDailyWorkloadingsPlanningDayChart = echarts.init(document.getElementById('group-daily-workloadings-planning-day-chart'));
+    groupDailyWorkloadingsBuildingDayChart = echarts.init(document.getElementById('group-daily-workloadings-building-day-chart'));
+
+    const xAxisJob = JSON.parse(this.data.get("x_axis_job"));
+    const xAxisBluePrint = JSON.parse(this.data.get("x_axis_blue_print"));
+    const xAxisConstruction = JSON.parse(this.data.get("x_axis_construction"));
+    const currentUserCompaniesShortNames = JSON.parse(this.data.get("current_user_companies_short_names"));
+    const dayRateData = JSON.parse(this.data.get("day_rate"));
+    const dayRateDataRef = this.data.get("day_rate_ref");
+    const planningDayRateData = JSON.parse(this.data.get("planning_day_rate"));
+    const planningDayRateDataRef = this.data.get("planning_day_rate_ref");
+    const buildingDayRateData = JSON.parse(this.data.get("building_day_rate"));
+    const buildingDayRateDataRef = this.data.get("building_day_rate_ref");
+
+    const day_option = {
+        title: {
+          text: '工作填报率'
+        },
+        grid: {
+          left: 50,
+          right: 110,
+          top: 60,
+          bottom: 60
+        },
+        toolbox: {
+          feature: {
+            dataView: {},
+            saveAsImage: {
+              pixelRatio: 2
+            }
+          }
+        },
+        tooltip: {},
+        xAxis: {
+          data: xAxisJob,
+          silent: true,
+          axisLabel: {
+            interval: 0,
+            rotate: -40
+          },
+          splitLine: {
+            show: false
+          }
+        },
+        yAxis: {
+          axisLabel: {
+            show: true,
+            interval: 'auto',
+            formatter: '{value} %'
+          }
+        },
+        series: [{
+          name: '工作填报率',
+          type: 'line',
+          symbol: 'triangle',
+          symbolSize: function(val, params) {
+            if (val < dayRateDataRef) {
+              return 8;
+            } else {
+              return 16;
+            }
+          },
+          data: dayRateData,
+          itemStyle: {
+            color: '#738496'
+          },
+          label: {
+            normal: {
+              show: true,
+              position: 'top',
+              formatter: '{c}%'
+            }
+          },
+          markLine: {
+            label: {
+              formatter: '{c}% 填报率及格线'
+            },
+            lineStyle: {
+              type: 'solid',
+              width: 1
+            },
+            data: [
+              {
+                yAxis: dayRateDataRef
+              }
+            ]
+          }
+        }]
+    };
+
+    const planning_day_option = {
+        title: {
+          text: '方案饱和度'
+        },
+        grid: {
+          left: 50,
+          right: 110,
+          top: 60,
+          bottom: 125
+        },
+        toolbox: {
+          feature: {
+            dataView: {},
+            saveAsImage: {
+                pixelRatio: 2
+            }
+          }
+        },
+        tooltip: {},
+        xAxis: {
+          data: xAxisBluePrint,
+          silent: true,
+          axisLabel: {
+            interval: 0,
+            rotate: -40
+          },
+          splitLine: {
+              show: false
+          }
+        },
+        yAxis: {
+          axisLabel: {
+            show: true,
+            interval: 'auto',
+            formatter: '{value} %'
+          }
+        },
+        series: [{
+          name: '方案饱和度',
+          type: 'line',
+          symbol: 'circle',
+          symbolSize: function(val, params) {
+            if (val < planningDayRateDataRef) {
+              return 8;
+            } else {
+              return 16;
+            }
+          },
+          data: planningDayRateData,
+          itemStyle: {
+            color: '#334B5C'
+          },
+          label: {
+            normal: {
+              show: true,
+              position: 'top',
+              formatter: '{c}%'
+            }
+          },
+          markLine: {
+            label: {
+              formatter: '{c}% 饱和度线'
+            },
+            lineStyle: {
+              type: 'solid',
+              width: 1
+            },
+            data: [
+              {
+                yAxis: planningDayRateDataRef
+              }
+            ]
+          }
+        }]
+    };
+
+    const building_day_option = {
+        title: {
+          text: '施工图饱和度'
+        },
+        grid: {
+          left: 50,
+          right: 110,
+          top: 60,
+          bottom: 125
+        },
+        toolbox: {
+          feature: {
+            dataView: {},
+            saveAsImage: {
+                pixelRatio: 2
+            }
+          }
+        },
+        tooltip: {},
+        xAxis: {
+          data: xAxisConstruction,
+          silent: true,
+          axisLabel: {
+            interval: 0,
+            rotate: -40
+          },
+          splitLine: {
+              show: false
+          }
+        },
+        yAxis: {
+          axisLabel: {
+            show: true,
+            interval: 'auto',
+            formatter: '{value} %'
+          }
+        },
+        series: [{
+          name: '施工图饱和度',
+          type: 'line',
+          symbol: 'square',
+          symbolSize:  function(val, params) {
+            if (val < buildingDayRateDataRef) {
+              return 8;
+            } else {
+              return 16;
+            }
+          },
+          data: buildingDayRateData,
+          itemStyle: {
+            color: '#6AB0B8'
+          },
+          label: {
+            normal: {
+              show: true,
+              position: 'top',
+              formatter: '{c}%'
+            }
+          },
+          markLine: {
+            label: {
+              formatter: '{c}% 饱和度线'
+            },
+            lineStyle: {
+              type: 'solid',
+              width: 1
+            },
+            data: [
+              {
+                yAxis: buildingDayRateDataRef
+              }
+            ]
+          }
+        }]
+    };
+
+    function drill_down_subsidiary(params) {
+      if (params.componentType === 'series') {
+        if (params.seriesType === 'line') {
+          let company_name;
+          switch (params.seriesName) {
+            case '工作填报率':
+              company_name = xAxisJob[params.dataIndex]
+              break;
+            case '方案饱和度':
+              company_name = xAxisBluePrint[params.dataIndex]
+              break;
+            case '施工图饱和度':
+              company_name = xAxisConstruction[params.dataIndex]
+              break;
+          }
+
+          let url = '/report/subsidiary_workloading';
+
+          url += '?company_name=' + encodeURIComponent(company_name) + '&view_deptcode_sum=true';
+
+          if (currentUserCompaniesShortNames.indexOf(company_name) > -1 || currentUserCompaniesShortNames.indexOf('上海天华') > -1) {
+            Turbolinks.visit(url);
+          }
+        }
+      }
+    }
+
+    groupDailyWorkloadingsDayChart.setOption(day_option, false);
+    groupDailyWorkloadingsDayChart.on('click', drill_down_subsidiary);
+    groupDailyWorkloadingsPlanningDayChart.setOption(planning_day_option, false);
+    groupDailyWorkloadingsPlanningDayChart.on('click', drill_down_subsidiary);
+    groupDailyWorkloadingsBuildingDayChart.setOption(building_day_option, false);
+    groupDailyWorkloadingsBuildingDayChart.on('click', drill_down_subsidiary);
+    setTimeout(() => {
+      groupDailyWorkloadingsDayChart.resize();
+      groupDailyWorkloadingsPlanningDayChart.resize();
+      groupDailyWorkloadingsBuildingDayChart.resize();
+    }, 200);
+  }
+
+  layout() {
+    groupDailyWorkloadingsDayChart.resize();
+    groupDailyWorkloadingsPlanningDayChart.resize();
+    groupDailyWorkloadingsBuildingDayChart.resize();
+  }
+
+  disconnect() {
+    groupDailyWorkloadingsDayChart.dispose();
+    groupDailyWorkloadingsPlanningDayChart.dispose();
+    groupDailyWorkloadingsBuildingDayChart.dispose();
+  }
+}
