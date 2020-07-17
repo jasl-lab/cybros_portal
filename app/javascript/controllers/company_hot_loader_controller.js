36d67a386ec076b7dd1957a2b511f673d2cff18d
Eric-Guo <eric.guocz@gmail.com>
2020-07-17 16:22:37 +0800

    Show hot section also.

---
+++ app/javascript/controllers/company_hot_loader_controller.js
@@ -0,0 +1,22 @@
+import { Controller } from "stimulus"
+
+export default class extends Controller {
+  connect() {
+    this.load()
+  }
+
+  load() {
+    $.ajax('https://portal.thape.com.cn/api/v1/hots?page=1', {
+      dataType: 'json'
+    }).done(function(response) {
+      const data = response.data[0];
+      console.log(data);
+
+      $("#company-hots-img").attr("src", data.cover);
+      $("#company-hots-title").html(data.title);
+      $("#company-hots-text").html(data.description);
+      $("#company-hots-link").attr("href", data.url);
+
+    });
+  }
+}
