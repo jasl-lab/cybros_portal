e737945c5fc122fb0b23bee5ed026221ff07c185
Eric-Guo <eric.guocz@gmail.com>
2020-07-21 14:21:16 +0800

    Review logic of the HR control.

--- app/policies/bi/hr_talent_pool_policy.rb
+++ app/policies/bi/hr_talent_pool_policy.rb
@@ -5,7 +5,8 @@ module Bi
     def show?
       user.present? &&
         ( user.roles.pluck(:hr_subsidiary_reader).any? ||
-          (user.job_level.to_i >= 13 && user.position_titles.include?('所长')) ||
+          (user.job_level.to_i >= 13 && user.position_titles.include?('管理副所长')) ||
+          (user.job_level.to_i >= 14 && user.position_titles.include?('所长')) ||
           user.admin? )
     end
   end
