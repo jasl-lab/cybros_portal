9b111583e36632c810575aeae2130dd74a0a7e94
Eric-Guo <eric.guocz@gmail.com>
2020-07-22 14:11:15 +0800

    Remove all hard code.

--- app/policies/bi/complete_value_policy.rb
+++ app/policies/bi/complete_value_policy.rb
@@ -4,7 +4,7 @@ module Bi
   class CompleteValuePolicy < BasePolicy
     class Scope < Scope
       def resolve
-        if user.present? && (user.admin? || %w[000176].include?(user.clerk_code))
+        if user.present? && (user.admin? || user.roles.pluck(:report_view_all).any? )
           scope.all
         elsif user.present? && (user.roles.pluck(:report_viewer).any? || user.job_level.to_i >= 11)
           scope.where(orgcode: user.can_access_org_codes)
@@ -16,7 +16,7 @@ module Bi
 
     def show?
       return false unless user.present?
-      user.admin? || %w[000176].include?(user.clerk_code) \
+      user.admin? \
         || user.roles.pluck(:report_viewer).any? \
         || user.roles.pluck(:report_view_all).any? \
         || user.operation_access_codes.any? { |c| c[0] <= User::ALL_EXCEPT_OTHER_COMPANY_DETAILS }
