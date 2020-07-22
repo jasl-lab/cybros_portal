027177c8334e14a86261597f159ce859fc8d47ac
Eric-Guo <eric.guocz@gmail.com>
2020-07-22 11:07:22 +0800

    Control report view logic using operation_access_codes.

--- app/policies/bi/sub_company_need_receive_policy.rb
+++ app/policies/bi/sub_company_need_receive_policy.rb
@@ -12,8 +12,10 @@ module Bi
 
     def show?
       return false unless user.present?
-      user.roles.pluck(:report_viewer).any? || user.roles.pluck(:report_view_all).any? \
-      || user.job_level.to_i >= 11 || user.admin?
+      user.roles.pluck(:report_viewer).any? \
+      || user.roles.pluck(:report_view_all).any? \
+      || user.operation_access_codes.any? { |c| c[0] <= User::ALL_EXCEPT_OTHER_COMPANY_DETAILS } \
+      || user.admin?
     end
   end
 end
