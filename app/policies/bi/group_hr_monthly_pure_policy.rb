852694404c97fcc3cf163c8afc7bde5239b68b79
Zero_Zeng <1154945484@qq.com>
2020-07-23 15:35:16 +0800

    HR集团实时月报（有撤销）权限修改

--- app/policies/bi/group_hr_monthly_pure_policy.rb
+++ app/policies/bi/group_hr_monthly_pure_policy.rb
@@ -4,7 +4,7 @@ module Bi
   class GroupHrMonthlyPurePolicy < Struct.new(:user, :dashboard)
     def show?
       user.present? &&
-        ( user.admin? )
+        ( user.admin? || user.roles.any? { |r| r.role_name == '人力资源报表管理员' })
     end
   end
 end
