6d2d8fdc191ebf8765dfe16fccc05dde3edcad5c
Eric-Guo <eric.guocz@gmail.com>
2020-07-24 13:51:56 +0800

    Same people access for bi account.

--- app/policies/bi/account_report_policy.rb
+++ app/policies/bi/account_report_policy.rb
@@ -4,7 +4,7 @@ module Bi
   class AccountReportPolicy < Struct.new(:user, :dashboard)
     def show?
       user.present? &&
-        ( user.admin? )
+        ( user.admin? || user.chinese_name.in?(%w(郭颖杰 余慧 陆文娟 周聪睿 蒋磊 吴悠 毕赢 孙涛)) )
     end
   end
 end
