fc5e08495ff101003d60f19972c46b045264896b
Eric-Guo <eric.guocz@gmail.com>
2020-07-29 16:16:28 +0800

    Review logic for 臧晓磊

--- app/policies/bi/sub_company_real_receive_policy.rb
+++ app/policies/bi/sub_company_real_receive_policy.rb
@@ -6,7 +6,8 @@ module Bi
       def resolve
         if user.present? && (user.roles.pluck(:report_view_all).any? || user.admin?)
           scope.all
-        elsif user.present? && (user.roles.pluck(:report_viewer).any? || user.operation_access_codes.any? { |c| c[0] <= User::MY_COMPANY_EXCEPT_OTHER_DEPTS })
+        elsif user.present? && (user.roles.pluck(:report_viewer).any? \
+          || user.operation_access_codes.any? { |c| c[0] <= User::MY_COMPANY_EXCEPT_OTHER_DEPTS })
           allow_orgcodes = user.can_access_org_codes
           scope.where(orgcode: allow_orgcodes).or(scope.where(orgcode_sum: allow_orgcodes))
         else
@@ -19,7 +20,8 @@ module Bi
           || user.operation_access_codes.any? { |c| c[0] <= User::ALL_EXCEPT_OTHER_COMPANY_DETAILS } \
           || user.admin?)
           scope.all
-        elsif user.present? && (user.roles.pluck(:report_viewer).any?)
+        elsif user.present? && (user.roles.pluck(:report_viewer).any? \
+          || user.operation_access_codes.any? { |c| c[0] <= User::MY_COMPANY_EXCEPT_OTHER_DEPTS })
           allow_orgcodes = user.can_access_org_codes
           scope.where(orgcode: allow_orgcodes).or(scope.where(orgcode_sum: allow_orgcodes))
         else
@@ -32,7 +34,7 @@ module Bi
       return false unless user.present?
       user.roles.pluck(:report_viewer).any? \
       || user.roles.pluck(:report_view_all).any? \
-      || user.operation_access_codes.any? { |c| c[0] <= User::ALL_EXCEPT_OTHER_COMPANY_DETAILS } \
+      || user.operation_access_codes.any? { |c| c[0] <= User::MY_COMPANY_EXCEPT_OTHER_DEPTS } \
       || user.admin?
     end
   end
