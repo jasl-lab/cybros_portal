450ec18ec994260325fb7722e819c99c0320429a
Eric-Guo <eric.guocz@gmail.com>
2020-07-29 15:52:29 +0800

    Add missing scope.

--- app/policies/bi/sub_company_need_receive_policy.rb
+++ app/policies/bi/sub_company_need_receive_policy.rb
@@ -2,8 +2,13 @@ module Bi
   class SubCompanyNeedReceivePolicy < BasePolicy
     class Scope < Scope
       def resolve
-        if user.present? && (user.roles.pluck(:report_viewer).any? || user.roles.pluck(:report_view_all).any? || user.admin?)
+        if user.present? && (user.roles.pluck(:report_viewer).any? \
+          || user.roles.pluck(:report_view_all).any? \
+          || user.admin?)
           scope.all
+        elsif user.present? && (user.operation_access_codes.any? { |c| c[0] <= User::MY_COMPANY_EXCEPT_OTHER_DEPTS })
+          allow_orgcodes = user.can_access_org_codes
+          scope.where(orgcode: allow_orgcodes).or(scope.where(orgcode_sum: allow_orgcodes))
         else
           scope.none
         end
