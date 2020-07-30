cde352398211b39252bfab3fd47283845262959b
Eric-Guo <eric.guocz@gmail.com>
2020-07-30 10:26:21 +0800

    All see all in 工作完成情况

--- app/policies/bi/complete_value_policy.rb
+++ app/policies/bi/complete_value_policy.rb
@@ -4,9 +4,12 @@ module Bi
   class CompleteValuePolicy < BasePolicy
     class Scope < Scope
       def resolve
-        if user.present? && (user.admin? || user.roles.pluck(:report_view_all).any? )
+        if user.present? && (user.admin? \
+          || user.roles.pluck(:report_view_all).any? \
+          || user.operation_access_codes.any? { |c| c[0] <= User::ALL_EXCEPT_OTHER_COMPANY_DETAILS })
           scope.all
-        elsif user.present? && (user.roles.pluck(:report_viewer).any? || user.operation_access_codes.any? { |c| c[0] <= User::MY_COMPANY_EXCEPT_OTHER_DEPTS })
+        elsif user.present? && (user.roles.pluck(:report_viewer).any? \
+          || user.operation_access_codes.any? { |c| c[0] <= User::MY_COMPANY_EXCEPT_OTHER_DEPTS })
           scope.where(orgcode: user.can_access_org_codes)
         else
           scope.none
