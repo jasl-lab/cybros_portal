02a0419cd752f35d44d643898f5c100a6df8f068
Eric-Guo <eric.guocz@gmail.com>
2020-07-30 10:12:55 +0800

    Allow see all for ALL_EXCEPT_OTHER_COMPANY_DETAILS

--- app/policies/bi/work_hours_count_org_policy.rb
+++ app/policies/bi/work_hours_count_org_policy.rb
@@ -2,9 +2,12 @@ module Bi
   class WorkHoursCountOrgPolicy < BasePolicy
     class Scope < Scope
       def resolve
-        if user.present? && (user.roles.pluck(:report_view_all).any? || user.admin?)
+        if user.present? && (user.roles.pluck(:report_view_all).any? \
+          || user.operation_access_codes.any? { |c| c[0] <= User::ALL_EXCEPT_OTHER_COMPANY_DETAILS } \
+          || user.admin?)
           scope.all
-        elsif user.present? && (user.roles.pluck(:report_viewer).any? || user.operation_access_codes.any? { |c| c[0] <= User::MY_COMPANY_EXCEPT_OTHER_DEPTS })
+        elsif user.present? && (user.roles.pluck(:report_viewer).any? \
+          || user.operation_access_codes.any? { |c| c[0] <= User::MY_COMPANY_EXCEPT_OTHER_DEPTS })
           can_access_org_codes = user.can_access_org_codes.append(user.user_company_orgcode)
           scope.where(orgcode: can_access_org_codes)
         else
