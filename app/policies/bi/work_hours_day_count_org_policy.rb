922a8d78c7a1f592f7d8eb8f3c71109672e1ef81
Eric-Guo <eric.guocz@gmail.com>
2020-07-30 10:16:52 +0800

    Allow see all for 每日填报率及饱和度

--- app/policies/bi/work_hours_day_count_org_policy.rb
+++ app/policies/bi/work_hours_day_count_org_policy.rb
@@ -2,9 +2,12 @@ module Bi
   class WorkHoursDayCountOrgPolicy < BasePolicy
     class Scope < Scope
       def resolve
-        if user.present? && (user.roles.pluck(:report_view_all).any? || user.admin?)
+        if user.present? && (user.roles.pluck(:report_view_all).any? \
+          || user.operation_access_codes.any? { |c| c[0] <= User::ALL_EXCEPT_OTHER_COMPANY_DETAILS } \
+          || user.admin?)
           scope.all
-        elsif user.present? && (user.roles.pluck(:report_viewer).any? || user.operation_access_codes.any? { |c| c[0] <= User::MY_COMPANY_EXCEPT_OTHER_DEPTS })
+        elsif user.present? && (user.roles.pluck(:report_viewer).any? \
+          || user.operation_access_codes.any? { |c| c[0] <= User::MY_COMPANY_EXCEPT_OTHER_DEPTS })
           scope.where(orgcode: user.can_access_org_codes)
         else
           scope.none
