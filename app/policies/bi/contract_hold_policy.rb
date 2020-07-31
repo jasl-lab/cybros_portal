535bebf25ae9f362f7b8c06c8d6c98cdbccb8e74
Eric-Guo <eric.guocz@gmail.com>
2020-07-31 15:42:42 +0800

    Fix 所级业务保有量 access for code 3.

--- app/policies/bi/contract_hold_policy.rb
+++ app/policies/bi/contract_hold_policy.rb
@@ -31,7 +31,7 @@ module Bi
       return false unless user.present?
       user.roles.pluck(:report_viewer).any? \
       || user.roles.pluck(:report_view_all).any? \
-      || user.operation_access_codes.any? { |c| c[0] <= User::ALL_EXCEPT_OTHER_COMPANY_DETAILS } \
+      || user.operation_access_codes.any? { |c| c[0] <= User::MY_COMPANY_EXCEPT_OTHER_DEPTS } \
       || user.admin?
     end
   end
