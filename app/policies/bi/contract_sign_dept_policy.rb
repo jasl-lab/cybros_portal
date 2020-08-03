348ed60863832004c7943e34b18bf378c0fc2648
Eric-Guo <eric.guocz@gmail.com>
2020-08-03 11:11:27 +0800

    Fix bug.

--- app/policies/bi/contract_sign_dept_policy.rb
+++ app/policies/bi/contract_sign_dept_policy.rb
@@ -14,7 +14,7 @@ module Bi
           || user.operation_access_codes.any? { |c| c[0] <= User::MY_COMPANY_ALL_DETAILS }
           scope.where(orgcode: can_access_org_codes).or(scope.where(orgcode_sum: can_access_org_codes))
         elsif user.operation_access_codes.any? { |c| c[0] <= User::MY_COMPANY_EXCEPT_OTHER_DEPTS }
-          scope.where(orgcode: can_access_org_codes, deptcode: can_access_dept_codes).or(scope.where(orgcode_sum: can_access_org_codes, deptcode_sum: can_access_dept_codes))
+          scope.where(orgcode: can_access_org_codes, deptcode: user.can_access_dept_codes).or(scope.where(orgcode_sum: can_access_org_codes, deptcode_sum: user.can_access_dept_codes))
         else
           scope.none
         end
