1ff1cdfa9cdbfe6a7fd7388d1b673a547e61954e
Eric-Guo <eric.guocz@gmail.com>
2020-08-03 10:50:24 +0800

    More dept code control

--- app/policies/bi/contract_sign_detail_date_policy.rb
+++ app/policies/bi/contract_sign_detail_date_policy.rb
@@ -13,7 +13,7 @@ module Bi
           scope.where(orgcode: allow_orgcodes).or(scope.where(orgcode_sum: allow_orgcodes))
         elsif user.present? && (user.operation_access_codes.any? { |c| c[0] <= User::MY_COMPANY_EXCEPT_OTHER_DEPTS })
           allow_orgcodes = user.can_access_org_codes
-          scope.where(orgcode: allow_orgcodes).or(scope.where(orgcode_sum: allow_orgcodes))
+          scope.where(orgcode: allow_orgcodes, deptcode: user.can_access_dept_codes).or(scope.where(orgcode_sum: allow_orgcodes, deptcode_sum: user.can_access_dept_codes))
         else
           scope.none
         end
