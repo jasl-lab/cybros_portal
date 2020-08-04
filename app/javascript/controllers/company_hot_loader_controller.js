8cba9090b7181efd08d1a7d5d5df14248613be22
Eric-Guo <eric.guocz@gmail.com>
2020-08-04 16:04:49 +0800

    URL update.

--- app/javascript/controllers/company_hot_loader_controller.js
+++ app/javascript/controllers/company_hot_loader_controller.js
@@ -6,7 +6,7 @@ export default class extends Controller {
   }
 
   load() {
-    $.ajax('https://portal.thape.com.cn/api/v1/hots?page=1', {
+    $.ajax('https://portal.thape.com.cn/api/v1/hots?h=true&page=1', {
       dataType: 'json'
     }).done(function(response) {
       const data = response.data[0];
