45a0292668ce5e357708d1148ba6457f87159855
Eric-Guo <eric.guocz@gmail.com>
2020-08-06 13:49:21 +0800

    Show 可发放餐补次数 in 人员工作填报率及饱和度分析

--- app/controllers/report/subsidiary_people_workloadings_controller.rb
+++ app/controllers/report/subsidiary_people_workloadings_controller.rb
@@ -52,6 +52,36 @@ class Report::SubsidiaryPeopleWorkloadingsController < Report::BaseController
       data.where(deptcode: @selected_dept_code)
     end
 
+    lunch_count = policy_scope(Bi::WorkHoursCountCombine)
+      .select('userid, type1')
+      .where('type1 > 0')
+      .where(date: beginning_of_day..end_of_day, orgcode: @selected_company_code)
+
+    @lunch_work_count = lunch_count.where(iswork: 1).reduce({}) do |h, l|
+      t = l.type1 >= 10 ? 1 : 0
+      if h[l.userid].present?
+        h[l.userid] = h[l.userid] + t
+      else
+        h[l.userid] = t
+      end
+      h
+    end
+    @lunch_non_work_count = lunch_count.where(iswork: 0).reduce({}) do |h, l|
+      t = if l.type1 >= 8
+        2
+      elsif l.type1 >= 4 && l.type1 < 8
+        1
+      else
+        0
+      end
+      if h[l.userid].present?
+        h[l.userid] = h[l.userid] + t
+      else
+        h[l.userid] = t
+      end
+      h
+    end
+
     fill_rate_numerator = policy_scope(Bi::WorkHoursCountCombine)
       .select('userid, COUNT(*) realhours_count')
       .where(date: beginning_of_day..end_of_day, orgcode: @selected_company_code)
