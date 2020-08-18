afc17d616230bc524b8e64fddfe6116a21ee134b
Zero_Zeng <1154945484@qq.com>
2020-08-18 15:39:19 +0800

    add HR monthly pure report

---
+++ app/controllers/report/group_subsidiary_hr_monthly_pures_controller.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+class Report::GroupSubsidiaryHrMonthlyPuresController < Report::BaseController
+  before_action :authenticate_user!
+  before_action :set_page_layout_data, if: -> { request.format.html? }
+  before_action :set_breadcrumbs, only: %i[index], if: -> { request.format.html? }
+  before_action :prepare_encrypt_uid
+
+  def show
+    authorize :"Bi::GroupSubsidiaryHrMonthlyPure"
+    prepare_meta_tags title: t(".title")
+    @redirect_url = "view/report?viewlet=HR/HR_REPORT_MONTH_JT_3_HR.cpt&ref_t=design&ref_c=7c332fd4-ca53-4731-8200-9147e584be33"
+    @hide_app_footer = true
+    @hide_main_header_wrapper = true
+    @hide_scroll = true
+    render 'shared/report_show'
+  end
+
+  protected
+
+  def set_page_layout_data
+    @_sidebar_name = "human_resource"
+  end
+
+  def set_breadcrumbs
+    @_breadcrumbs = [
+    { text: t("layouts.sidebar.application.header"),
+      link: root_path },
+    { text: t("layouts.sidebar.human_resource.header"),
+      link: report_human_resource_path }]
+  end
+end
---
+++ app/policies/bi/group_subsidiary_hr_monthly_pure_policy.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+module Bi
+  class GroupSubsidiaryHrMonthlyPurePolicy < Struct.new(:user, :dashboard)
+    def show?
+      user.present? &&
+        ( user.roles.pluck(:hr_subsidiary_reader).any? || user.admin? )
+    end
+  end
+end
