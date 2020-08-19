a0e2516888fa9792034cceae1f21cbbfff9a5d20
Eric-Guo <eric.guocz@gmail.com>
2020-08-19 14:57:22 +0800

    Fix number problem in reports.

--- app/controllers/report/contract_types_analyses_controller.rb
+++ app/controllers/report/contract_types_analyses_controller.rb
@@ -36,7 +36,7 @@ class Report::ContractTypesAnalysesController < Report::BaseController
     @contract_price_方案_公司 = contract_price_方案_公司[0..9].append('其他')
     first_10_contract_price_方案_合同总金额 = contract_price_方案_合同总金额[0..9]
     @contract_price_方案_合同总金额 = first_10_contract_price_方案_合同总金额.append(total_contract_price_方案_合同总金额 - first_10_contract_price_方案_合同总金额.sum)
-    @contract_price_方案_合同总金额 = @contract_price_方案_合同总金额.map { |d| (d / 10000_00.0).round(0) }
+    @contract_price_方案_合同总金额 = @contract_price_方案_合同总金额.map { |d| (d / 10000_00.0).round(1) }
 
 
     contract_price_施工图_公司 = contract_price_施工图.collect { |c| Bi::OrgShortName.company_short_names_by_orgcode.fetch(c.businessltdcode, c.businessltdcode) }
@@ -45,7 +45,7 @@ class Report::ContractTypesAnalysesController < Report::BaseController
     first_10_contract_price_施工图_合同总金额 = contract_price_施工图_合同总金额[0..9]
     @contract_price_施工图_公司 = contract_price_施工图_公司[0..9].append('其他')
     @contract_price_施工图_合同总金额 = first_10_contract_price_施工图_合同总金额.append(total_contract_price_施工图_合同总金额 - first_10_contract_price_施工图_合同总金额.sum)
-    @contract_price_施工图_合同总金额 = @contract_price_施工图_合同总金额.map { |d| (d/10000_00.0).round(0) }
+    @contract_price_施工图_合同总金额 = @contract_price_施工图_合同总金额.map { |d| (d / 10000_00.0).round(1) }
 
     @years_category, @years_sum_住宅方案, @years_sum_住宅施工图, @years_sum_公建方案, @years_sum_公建施工图 \
       = 住宅公建_contract_price(Time.now.year-2, Time.now.year, @orgs_options)
@@ -66,15 +66,30 @@ class Report::ContractTypesAnalysesController < Report::BaseController
       years_sum_公建方案 = []
       years_sum_公建施工图 = []
       years_name.each do |year|
-        years_sum_住宅方案 << sum_scope.find { |c| c.year_name == year && (c.projectstage == '前端' || c.projectstage == '全过程') && c.projecttype == '土建住宅' }&.frontpart
-        years_sum_住宅施工图 << sum_scope.find { |c| c.year_name == year && (c.projectstage == '后端' || c.projectstage == '全过程') && c.projecttype == '土建住宅' }&.rearpart
-        years_sum_公建方案 << sum_scope.find { |c| c.year_name == year && (c.projectstage == '前端' || c.projectstage == '全过程') && c.projecttype == '土建公建' }&.frontpart
-        years_sum_公建施工图 << sum_scope.find { |c| c.year_name == year && (c.projectstage == '后端' || c.projectstage == '全过程') && c.projecttype == '土建公建' }&.rearpart
+        years_sum_住宅方案_前端 = sum_scope.find { |c| c.year_name == year && c.projectstage == '前端' && c.projecttype == '土建住宅' }&.frontpart
+        years_sum_住宅方案_全过程 = sum_scope.find { |c| c.year_name == year && c.projectstage == '全过程' && c.projecttype == '土建住宅' }&.frontpart
+        years_sum_住宅方案 << years_sum_住宅方案_前端.to_f + years_sum_住宅方案_全过程.to_f
+        # Rails.logger.debug "years_sum_住宅方案: #{year}, #{years_sum_住宅方案}"
+
+        years_sum_住宅施工图_后端 = sum_scope.find { |c| c.year_name == year && c.projectstage == '后端' && c.projecttype == '土建住宅' }&.rearpart
+        years_sum_住宅施工图_全过程 = sum_scope.find { |c| c.year_name == year && c.projectstage == '全过程' && c.projecttype == '土建住宅' }&.rearpart
+        years_sum_住宅施工图 << years_sum_住宅施工图_后端.to_f + years_sum_住宅施工图_全过程.to_f
+        # Rails.logger.debug "years_sum_住宅施工图: #{year}, #{years_sum_住宅施工图}"
+
+        years_sum_公建方案_前端 = sum_scope.find { |c| c.year_name == year && c.projectstage == '前端' && c.projecttype == '土建公建' }&.frontpart
+        years_sum_公建方案_全过程 = sum_scope.find { |c| c.year_name == year && c.projectstage == '全过程' && c.projecttype == '土建公建' }&.frontpart
+        years_sum_公建方案 << years_sum_公建方案_前端.to_f + years_sum_公建方案_全过程.to_f
+        # Rails.logger.debug "years_sum_公建方案: #{year}, #{years_sum_公建方案}"
+
+        years_sum_公建施工图_后端 = sum_scope.find { |c| c.year_name == year && c.projectstage == '后端' && c.projecttype == '土建公建' }&.rearpart
+        years_sum_公建施工图_全过程 = sum_scope.find { |c| c.year_name == year && c.projectstage == '全过程' && c.projecttype == '土建公建' }&.rearpart
+        years_sum_公建施工图 << years_sum_公建施工图_后端.to_f + years_sum_公建施工图_全过程.to_f
+        # Rails.logger.debug "years_sum_公建施工图: #{year}, #{years_sum_公建施工图}"
       end
-      years_sum_住宅方案 = years_sum_住宅方案.map { |d| (d/10000_00.0).round(0) }
-      years_sum_住宅施工图 = years_sum_住宅施工图.map { |d| (d/10000_00.0).round(0) }
-      years_sum_公建方案 = years_sum_公建方案.map { |d| (d/10000_00.0).round(0) }
-      years_sum_公建施工图 = years_sum_公建施工图.map { |d| (d/10000_00.0).round(0) }
+      years_sum_住宅方案 = years_sum_住宅方案.map { |d| (d/10000_00.0).round(1) }
+      years_sum_住宅施工图 = years_sum_住宅施工图.map { |d| (d/10000_00.0).round(1) }
+      years_sum_公建方案 = years_sum_公建方案.map { |d| (d/10000_00.0).round(1) }
+      years_sum_公建施工图 = years_sum_公建施工图.map { |d| (d/10000_00.0).round(1) }
       return [years_name, years_sum_住宅方案, years_sum_住宅施工图, years_sum_公建方案, years_sum_公建施工图]
     end
 
