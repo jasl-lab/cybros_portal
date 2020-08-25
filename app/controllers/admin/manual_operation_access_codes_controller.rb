b436c3009a3f548b5f583810d7b5e24040bac565
Eric-Guo <eric.guocz@gmail.com>
2020-08-25 14:56:40 +0800

    Allow remove at manual operation access cods.

--- app/controllers/admin/manual_operation_access_codes_controller.rb
+++ app/controllers/admin/manual_operation_access_codes_controller.rb
@@ -1,8 +1,6 @@
 # frozen_string_literal: true
 
 class Admin::ManualOperationAccessCodesController < Admin::ApplicationController
-  before_action :set_user, only: %i[create destroy]
-
   def index
     @code_1 = ManualOperationAccessCode.where(code: User::ALL_OF_ALL)
     @code_2 = ManualOperationAccessCode.where(code: User::ALL_EXCEPT_OTHER_COMPANY_DETAILS)
@@ -12,21 +10,29 @@ class Admin::ManualOperationAccessCodesController < Admin::ApplicationController
   end
 
   def create
-    @user.manual_operation_access_codes.create(manual_operation_access_code_params)
-    redirect_to admin_user_path(id: @user.id), notice: '市场运营的权限访问码已新建。'
+    user = User.find(params[:user_id])
+    user.manual_operation_access_codes.create(manual_operation_access_code_params)
+    redirect_to admin_user_path(id: user.id), notice: '市场运营的权限访问码已新建。'
   end
 
   def destroy
-    moac = @user.manual_operation_access_codes.find_by!(id: params[:id])
+    user = if params[:user_id].present?
+      User.find(params[:user_id])
+    else
+      ManualOperationAccessCode.find(params[:id]).user
+    end
+
+    moac = user.manual_operation_access_codes.find_by!(id: params[:id])
     moac.destroy
-    redirect_to admin_user_path(id: @user.id), notice: '市场运营的权限访问码已删除。'
+
+    if params[:user_id].present?
+      redirect_to admin_user_path(id: user.id), notice: '市场运营的权限访问码已删除。'
+    else
+      redirect_to admin_manual_operation_access_codes_path, notice: '市场运营的权限访问码已删除。'
+    end
   end
 
   private
-
-    def set_user
-      @user = User.find(params[:user_id])
-    end
 
     def manual_operation_access_code_params
       params.require(:manual_operation_access_code).permit(:code, :org_code, :dept_code, :title, :job_level)
