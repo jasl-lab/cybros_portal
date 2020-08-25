f4a296f5dae65fda89abf81119d4a2004cacdbbf
Eric-Guo <eric.guocz@gmail.com>
2020-08-25 10:23:01 +0800

    Limit the scope.

--- app/policies/bi/work_hours_count_combine_policy.rb
+++ app/policies/bi/work_hours_count_combine_policy.rb
@@ -5,9 +5,10 @@ module Bi
         if user.present? && (user.roles.pluck(:report_view_all).any? || user.admin?)
           scope.all
         elsif user.present? && (user.roles.pluck(:report_viewer).any? \
-          || user.operation_access_codes.any? { |c| c[0] <= User::MY_COMPANY_EXCEPT_OTHER_DEPTS })
-          can_access_org_codes = user.can_access_org_codes.append(user.user_company_orgcode)
-          scope.where(orgcode: can_access_org_codes)
+          || user.operation_access_codes.any? { |c| c[0] <= User::MY_COMPANY_ALL_DETAILS })
+          scope.where(orgcode: user.can_access_org_codes).or(scope.where(orgcode_sum: user.can_access_org_codes))
+        elsif user.present? && (user.operation_access_codes.any? { |c| c[0] <= User::MY_COMPANY_EXCEPT_OTHER_DEPTS })
+          scope.where(orgcode: user.can_access_org_codes, deptcode: user.can_access_dept_codes).or(scope.where(orgcode_sum: user.can_access_org_codes, deptcode_sum: user.can_access_dept_codes))
         else
           scope.none
         end
