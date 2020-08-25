a8821577eef3febc87ba03674e7814d1b791d909
Eric-Guo <eric.guocz@gmail.com>
2020-08-25 10:13:04 +0800

    No need hard code name.

--- app/policies/bi/work_hours_day_count_dept_policy.rb
+++ app/policies/bi/work_hours_day_count_dept_policy.rb
@@ -2,7 +2,8 @@ module Bi
   class WorkHoursDayCountDeptPolicy < BasePolicy
     class Scope < Scope
       def resolve
-        if user.present? && (user.roles.pluck(:report_view_all).any? || user.admin? || user.chinese_name.in?(%w(李俭 亢梦婕)))
+        if user.present? && (user.roles.pluck(:report_view_all).any? \
+          || user.admin?)
           scope.all
         elsif user.present? && (user.roles.pluck(:report_viewer).any? \
           || user.operation_access_codes.any? { |c| c[0] <= User::MY_COMPANY_EXCEPT_OTHER_DEPTS })
