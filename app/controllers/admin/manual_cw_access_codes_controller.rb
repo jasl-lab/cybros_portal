d8ec13f739d4934701399009e36aa21318d13161
Eric-Guo <eric.guocz@gmail.com>
2020-08-27 14:34:36 +0800

    Allow modify the manual_cw_access_codes

---
+++ app/controllers/admin/manual_cw_access_codes_controller.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+class Admin::ManualCwAccessCodesController < Admin::ApplicationController
+  before_action :set_user, only: %i[create destroy]
+
+  def create
+    @user.manual_cw_access_codes.create(manual_cw_access_code_params)
+    redirect_to admin_user_path(id: @user.id), notice: '财务的自定义角色权限已新建。'
+  end
+
+  def destroy
+    mcac = @user.manual_cw_access_codes.find_by!(id: params[:id])
+    mcac.destroy
+    redirect_to admin_user_path(id: @user.id), notice: '财务的自定义角色权限已删除。'
+  end
+
+  private
+
+    def set_user
+      @user = User.find(params[:user_id])
+    end
+
+    def manual_cw_access_code_params
+      params.require(:manual_cw_access_code).permit(:cw_rolename, :org_code, :dept_code)
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -28,9 +28,11 @@ Rails.application.routes.draw do
     resources :users, except: %i[destroy] do
       resources :manual_operation_access_codes, only: %i[destroy create]
       resources :manual_hr_access_codes, only: %i[destroy create]
+      resources :manual_cw_access_codes, only: %i[destroy create]
       collection do
         get :operation_org_code_change
         get :hr_org_code_change
+        get :cw_org_code_change
       end
       member do
         patch :lock
