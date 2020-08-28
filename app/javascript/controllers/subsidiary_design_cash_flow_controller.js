687071424beac41369e300400a2f5bc5da5c0360
Eric-Guo <eric.guocz@gmail.com>
2020-08-28 14:26:26 +0800

    Add overall cashflow.

--- app/javascript/controllers/subsidiary_design_cash_flow_controller.js
+++ app/javascript/controllers/subsidiary_design_cash_flow_controller.js
@@ -1,9 +1,11 @@
 import { Controller } from "stimulus"
 
+let companyDesignCashFlowChart;
 let subsidiaryDesignCashFlowChart;
 
 export default class extends Controller {
   connect() {
+    companyDesignCashFlowChart = echarts.init(document.getElementById('company-design-cash-flow-chart'));
     subsidiaryDesignCashFlowChart = echarts.init(document.getElementById('subsidiary-design-cash-flow-chart'));
 
     const orgCheckdate = JSON.parse(this.data.get("org_checkdate"));
@@ -22,7 +24,41 @@ export default class extends Controller {
 
     const seriesData = deptOpeningMoney.map(buildSeries);
 
-    const options = {
+    const company_options = {
+      tooltip: {
+        trigger: 'axis'
+      },
+      grid: {
+        left: '3%',
+        right: '3%',
+        bottom: '3%',
+        containLabel: true
+      },
+      toolbox: {
+        feature: {
+          dataView: {},
+          saveAsImage: {}
+        }
+      },
+      xAxis: {
+        type: 'category',
+        boundaryGap: false,
+        data: orgCheckdate,
+        axisLabel: {
+          formatter: '{value}月'
+        },
+      },
+      yAxis: {
+        type: 'value',
+        name: '万元',
+      },
+      series: {
+        type: 'line',
+        data: orgOpeningMoney
+      }
+    };
+
+    const subsidiary_options = {
       tooltip: {
         trigger: 'axis'
       },
@@ -57,18 +93,22 @@ export default class extends Controller {
       series: seriesData
     };
 
-    subsidiaryDesignCashFlowChart.setOption(options, false);
+    companyDesignCashFlowChart.setOption(company_options, false);
+    subsidiaryDesignCashFlowChart.setOption(subsidiary_options, false);
 
     setTimeout(() => {
+      companyDesignCashFlowChart.resize();
       subsidiaryDesignCashFlowChart.resize();
     }, 200);
   }
 
   layout() {
+    companyDesignCashFlowChart.resize();
     subsidiaryDesignCashFlowChart.resize();
   }
 
   disconnect() {
+    companyDesignCashFlowChart.dispose();
     subsidiaryDesignCashFlowChart.dispose();
   }
 }
