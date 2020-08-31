5858d2158d7b77ad5ff51197ae9e008eeb62786d
Eric-Guo <eric.guocz@gmail.com>
2020-08-31 14:02:50 +0800

    Review 所级管理报表 - 合同管理表（商务）

--- app/policies/bi/contract_information_form_policy.rb
+++ app/policies/bi/contract_information_form_policy.rb
@@ -4,8 +4,9 @@ module Bi
   class ContractInformationFormPolicy < Struct.new(:user, :dashboard)
     def show?
       user.present? &&
-        ( user.chinese_name.in?(%w(郭颖杰 余慧 陆文娟 周聪睿 蒋磊 吴悠 毕赢 孙涛 陈一恺)) ||
-          user.admin?)
+        (user.chinese_name.in?(%w(郭颖杰 余慧 陆文娟 周聪睿 蒋磊 吴悠 毕赢 孙涛 陈一恺)) ||
+         user.roles.pluck(:role_name).any? { |r| r.in?(%w[CW_财务分析部管理员 CW_所级管理者1 CW_所级管理者2]) }
+         user.admin?)
     end
   end
 end
--- app/policies/bi/dept_homepage_policy.rb
+++ app/policies/bi/dept_homepage_policy.rb
@@ -5,7 +5,7 @@ module Bi
     def show?
       user.present? &&
         (user.chinese_name.in?(%w(郭颖杰 余慧 陆文娟 周聪睿 蒋磊 吴悠 毕赢 孙涛 陈一恺)) ||
-         user.roles.pluck(:role_name).any? { |r| r.in?(%w[HR_IT和人力管理员 HR_集团高管 HR_人力资源报表HR经理 HR_集团人力相关人员 HR_所级管理者 HR_子公司总经理、董事长]) }
+         user.roles.pluck(:role_name).any? { |r| r.in?(%w[CW_财务分析部管理员 CW_集团高管 CW_子公司高管1 CW_子公司高管2 CW_所级管理者1 CW_所级管理者2]) }
          user.admin?)
     end
   end
