5d910c8dfb8ddd6e2b3c2b00ee6e8a41e77e6d94
Eric-Guo <eric.guocz@gmail.com>
2020-08-31 09:14:13 +0800

    Using HR Role using defined in DB.

--- app/policies/bi/human_resource_policy.rb
+++ app/policies/bi/human_resource_policy.rb
@@ -8,6 +8,7 @@ module Bi
           user.roles.pluck(:hr_subsidiary_rt_reader).any? ||
           user.roles.pluck(:hr_report_writer).any? ||
           user.roles.pluck(:role_name).any? { |r| r == 'HR_所级管理者' } ||
+          user.roles.pluck(:role_name).any? { |r| r == 'HR_子公司总经理、董事长' } ||
           user.admin?)
     end
   end
