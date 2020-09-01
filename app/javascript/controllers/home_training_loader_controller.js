b9fa689cc4bb71d84aa14337fa5488a4cb95dd9f
Eric-Guo <eric.guocz@gmail.com>
2020-09-01 17:11:47 +0800

    Loading training course also.

---
+++ app/javascript/controllers/home_training_loader_controller.js
@@ -0,0 +1,22 @@
+import { Controller } from "stimulus"
+
+export default class extends Controller {
+  connect() {
+    this.load()
+  }
+
+  load() {
+    $.ajax('/api/v1/courses?h=true', {
+      dataType: 'json'
+    }).done(function(response) {
+      const template = {'<>':'div','class':'col-sm-12 col-md-6 col-lg-4 col-xl-3',
+       'html':'<div class="card"><img src="${cover}" class="card-img-top"><div class="card-body"><p class="card-text">${title}</p><a href="${url}" target="_blank" class="card-link">查看</a></div></div>'};
+
+      const data = response.data;
+
+      const news_html = json2html.transform(data,template);
+
+      $("#home-training-container").html(news_html);
+    });
+  }
+}
