6e535d1116235fca18b001b8a330197386adaf3b
Eric-Guo <eric.guocz@gmail.com>
2020-09-04 16:23:25 +0800

    More logic fillings.

--- app/controllers/report/hrdw_stfreinstate_bi_saves_controller.rb
+++ app/controllers/report/hrdw_stfreinstate_bi_saves_controller.rb
@@ -6,7 +6,10 @@ class Report::HrdwStfreinstateBiSavesController < Report::BaseController
   before_action :set_breadcrumbs, only: %i[index], if: -> { request.format.html? }
 
   def show
-    prepare_meta_tags title: t(".title")
+    prepare_meta_tags title: t('.title')
+    @archive_month = Time.now.day > 3 ? Time.now : Time.now - 1.month
+    @stfreinstatebimonth_exist = Hrdw::StfreinstateBiMonth.where(pyear: @archive_month.year, pmonth: @archive_month.month).exists?
+    @stfturnoverbimonth_exist = Hrdw::StfturnoverBiMonth.where(pyear: @archive_month.year, pmonth: @archive_month.month).exists?
   end
 
   def stfreinstate_archive
