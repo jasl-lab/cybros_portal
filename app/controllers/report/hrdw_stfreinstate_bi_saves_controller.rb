a66ddb312749e23d459db74f4552598f62275cdb
Eric-Guo <eric.guocz@gmail.com>
2020-09-04 16:50:11 +0800

    Finish archive function.

--- app/controllers/report/hrdw_stfreinstate_bi_saves_controller.rb
+++ app/controllers/report/hrdw_stfreinstate_bi_saves_controller.rb
@@ -13,9 +13,27 @@ class Report::HrdwStfreinstateBiSavesController < Report::BaseController
   end
 
   def stfreinstate_archive
+    @stfreinstatebimonth_exist = Hrdw::StfreinstateBiMonth.where(pyear: params[:year], pmonth: params[:month]).exists?
+    if @stfreinstatebimonth_exist
+      Hrdw::StfreinstateBiMonth.where(pyear: params[:year], pmonth: params[:month]).delete_all
+    end
+    Hrdw::StfreinstateBiMonth.connection.execute("
+      INSERT INTO HRDW_STFREINSTATE_BI_MONTH(name, clerkcode, begindate, enddate, endflag, lastflag, orgcode_sum, orgname_sum, orgcode, orgname, deptcode_sum, deptname_sum, deptcode, deptname, glbdef1, postname, zjname, trnstypename, pocname, trnsevent, gxname, ischange, comment, profession, stname, pyear, pmonth)
+      SELECT name, clerkcode, begindate, enddate, endflag, lastflag, orgcode_sum, orgname_sum, orgcode, orgname, deptcode_sum, deptname_sum, deptcode, deptname, glbdef1, postname, zjname, trnstypename, pocname, trnsevent, gxname, ischange, comment, profession, stname, #{params[:year].to_i}, #{params[:month].to_i}
+      FROM HRDW_STFREINSTATE_BI")
+    redirect_to report_hrdw_stfreinstate_bi_save_path, notice: 'HRDW_STFREINSTATE_BI_MONTH 归档完成'
   end
 
   def stfturnover_archive
+    @stfturnoverbimonth_exist = Hrdw::StfturnoverBiMonth.where(pyear: params[:year], pmonth: params[:month]).exists?
+    if @stfturnoverbimonth_exist
+      Hrdw::StfturnoverBiMonth.where(pyear: params[:year], pmonth: params[:month]).delete_all
+    end
+    Hrdw::StfturnoverBiMonth.connection.execute("
+      INSERT INTO HRDW_STFTURNOVER_BI_MONTH(name, clerkcode, effectivedate, employeetype, trnstypename, orgcode_sum_old, orgname_sum_old, orgcode_old, orgname_old, deptcode_sum_old, deptname_sum_old, deptcode_old, deptname_old, postname_old, zjname_old, orgcode_sum_new, orgname_sum_new, orgcode_new, orgname_new, deptcode_sum_new, deptname_sum_new, deptcode_new, deptname_new, postname_new, zjname_new, pyear, pmonth)
+      SELECT name, clerkcode, effectivedate, employeetype, trnstypename, orgcode_sum_old, orgname_sum_old, orgcode_old, orgname_old, deptcode_sum_old, deptname_sum_old, deptcode_old, deptname_old, postname_old, zjname_old, orgcode_sum_new, orgname_sum_new, orgcode_new, orgname_new, deptcode_sum_new, deptname_sum_new, deptcode_new, deptname_new, postname_new, zjname_new, #{params[:year].to_i}, #{params[:month].to_i}
+      FROM HRDW_STFTURNOVER_BI")
+    redirect_to report_hrdw_stfreinstate_bi_save_path, notice: 'HRDW_STFTURNOVER_BI_MONTH 归档完成'
   end
 
 
