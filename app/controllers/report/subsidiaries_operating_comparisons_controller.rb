aa99516b6eaa2ccfe4bed12c23e7ae08a30c5856
Eric-Guo <eric.guocz@gmail.com>
2020-09-08 12:32:46 +0800

    Make org code order standard code for 子公司经营情况对比

--- app/controllers/report/subsidiaries_operating_comparisons_controller.rb
+++ app/controllers/report/subsidiaries_operating_comparisons_controller.rb
@@ -16,7 +16,11 @@ class Report::SubsidiariesOperatingComparisonsController < Report::BaseControlle
     @year_names = @year_options - [2017, 2016] if @year_names.blank?
     data = policy_scope(Bi::YearReportHistory).where(year: @year_names, month: 1..@month_name.to_i)
 
-    all_company_orgcodes = data.pluck(:orgcode).uniq - ['000103', '000149', '000150', '000130', '00012801','000119','H000109','H000123']
+    all_company_orgcodes = data
+      .joins('LEFT JOIN ORG_ORDER on ORG_ORDER.org_code = YEAR_REPORT_HISTORY.orgcode')
+      .where('ORG_ORDER.org_order is not null')
+      .order('ORG_ORDER.org_order DESC')
+      .pluck(:orgcode).uniq - ['000103', '000149', '000150', '000130', '00012801','000119','H000109','H000123']
     all_company_short_names = all_company_orgcodes.collect { |c| Bi::OrgShortName.company_short_names_by_orgcode.fetch(c, c) }
 
     @orgs_options = all_company_orgcodes if @orgs_options.blank? # hide 天华节能
