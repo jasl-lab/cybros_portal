99522c1c5ca4662bdd6896a8208a88537e568bff
Zero_Zeng <1154945484@qq.com>
2020-09-09 10:36:13 +0800

    HR报表权限调整

--- app/policies/bi/hr_sy_policy.rb
+++ app/policies/bi/hr_sy_policy.rb
@@ -4,9 +4,14 @@ module Bi
   class HrSyPolicy < Struct.new(:user, :dashboard)
     def show?
       user.present? &&
+<<<<<<< Updated upstream
         (user.roles.pluck(:hr_group_reader).any? ||
          user.roles.pluck(:role_name).any? { |r| r.in?(%w[HR_集团人力相关人员 HR_子公司总经理、董事长 HR_集团高管 HR_人力资源报表责任人 HR_人力资源报表HR经理 HR_IT和人力管理员]) } ||
          user.admin?)
+=======
+        ( user.roles.pluck(:role_name).any? { |r| r.in?['HR_集团人力相关人员','HR_子公司总经理、董事长','HR_集团高管','HR_人力资源报表责任人','HR_人力资源报表HR经理','HR_IT和人力管理员']} ||
+          user.admin? )
+>>>>>>> Stashed changes
     end
   end
 end
