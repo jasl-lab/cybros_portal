a908ede97b63a04a37256bc8cabeedc349683687
Eric-Guo <eric.guocz@gmail.com>
2020-09-10 15:57:51 +0800

    Another review of the access control.

--- app/policies/bi/dept_money_flow_policy.rb
+++ app/policies/bi/dept_money_flow_policy.rb
@@ -13,8 +13,10 @@ module Bi
     end
 
     def show?
-      return false unless user.present?
-      user.admin?
+      user.present? &&
+        (user.chinese_name.in?(%w(郭颖杰 余慧 陆文娟 周聪睿 蒋磊 吴悠 毕赢 孙涛 陈一恺)) ||
+         # user.roles.pluck(:role_name).any? { |r| r.in?(%w[CW_财务分析部管理员 CW_所级管理者1]) } ||
+         user.admin?)
     end
   end
 end
