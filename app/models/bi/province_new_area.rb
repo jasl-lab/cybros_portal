17ef125a4d10225a2692c7faf64c288765e5b8d9
Eric-Guo <eric.guocz@gmail.com>
2020-09-11 10:31:04 +0800

    Only Date.new(2018, 2, 1) valid.

--- app/models/bi/province_new_area.rb
+++ app/models/bi/province_new_area.rb
@@ -5,7 +5,7 @@ module Bi
     self.table_name = 'PROVINCE_NEW_AREA'
 
     def self.all_month_names
-      all.order(date: :desc).pluck(:date).collect { |d| d.to_s(:month_and_year) }.uniq
+      all.order(date: :desc).where('date >= ?', Date.new(2018, 2, 1)).pluck(:date).collect { |d| d.to_s(:month_and_year) }.uniq
     end
   end
 end
