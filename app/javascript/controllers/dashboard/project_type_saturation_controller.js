9f4e005c45d0cc17e2592af1d2e8aec14b916a8b
Eric-Guo <eric.guocz@gmail.com>
2020-09-17 10:04:58 +0800

    Finish the 3 bar charts.

--- app/javascript/controllers/dashboard/project_type_saturation_controller.js
+++ app/javascript/controllers/dashboard/project_type_saturation_controller.js
@@ -5,8 +5,103 @@ let projectTypeSaturationDashboard;
 export default class extends Controller {
   connect() {
     projectTypeSaturationDashboard = echarts.init(document.getElementById('project-type-saturation-dashboard'));
+    // https://gallery.echartsjs.com/editor.html?c=xAqxk9ZhVv
 
-    console.log('projectTypeSaturationDashboard');
+
+    const data = [
+    {
+      name: '方案饱和度',
+      subtext: '参考值: 95%',
+      value: 92
+    },{
+      name: '施工图饱和度',
+      subtext: '参考值: 80%',
+      value: 81
+    },{
+      name: '非建筑饱和度',
+      subtext: '参考值: 95%',
+      value: 96
+    }];
+
+    const titleArr= [], seriesArr=[];
+    const colors=[['#389af4', '#dfeaff'],['#ff8c37', '#ffdcc3'],['#ffc257', '#ffedcc'], ['#fd6f97', '#fed4e0'],['#a181fc', '#e3d9fe']]
+    data.forEach(function(item, index){
+        titleArr.push(
+            {
+                text:item.name,
+                subtext: item.subtext,
+                left: index * 32 + 17 +'%',
+                top: '70%',
+                textAlign: 'center',
+                textStyle: {
+                    fontWeight: 'normal',
+                    fontSize: '14',
+                    color: colors[index][0],
+                    textAlign: 'center',
+                },
+            }
+        );
+        seriesArr.push(
+            {
+                name: item.name,
+                type: 'pie',
+                clockWise: false,
+                radius: [30, 40],
+                itemStyle:  {
+                    normal: {
+                        color: colors[index][0],
+                        shadowColor: colors[index][0],
+                        shadowBlur: 0,
+                        label: {
+                            show: false
+                        },
+                        labelLine: {
+                            show: false
+                        },
+                    }
+                },
+                hoverAnimation: false,
+                center: [index * 32 + 17 +'%', '50%'],
+                data: [{
+                    value: item.value,
+                    label: {
+                        normal: {
+                            formatter: function(params){
+                                return params.value+'%';
+                            },
+                            position: 'center',
+                            show: true,
+                            textStyle: {
+                                fontSize: '16',
+                                fontWeight: 'bold',
+                                color: colors[index][0]
+                            }
+                        }
+                    },
+                }, {
+                    value: 100-item.value,
+                    name: 'invisible',
+                    itemStyle: {
+                        normal: {
+                            color: colors[index][1]
+                        },
+                        emphasis: {
+                            color: colors[index][1]
+                        }
+                    }
+                }]
+            }
+        )
+    });
+
+
+    const option = {
+        backgroundColor: "#fff",
+        title:titleArr,
+        series: seriesArr
+    }
+
+    projectTypeSaturationDashboard.setOption(option, false);
 
     setTimeout(() => {
       projectTypeSaturationDashboard.resize();
