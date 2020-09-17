0080c58b6f32d66d1703b93c4809564376783fe6
Zero_Zeng <1154945484@qq.com>
2020-09-17 23:48:09 +0800

    change hr_year_dismiss policy

--- app/policies/bi/hr_year_dismiss_policy.rb
+++ app/policies/bi/hr_year_dismiss_policy.rb
@@ -4,7 +4,7 @@ module Bi
   class HrYearDismissPolicy < Struct.new(:user, :dashboard)
     def show?
       user.present? &&
-        (user.roles.pluck(:role_name).any? { |r| r.in?(%w[HR_IT和人力管理员]) } ||
+        (user.roles.pluck(:role_name).any? { |r| r.in?(%w[HR_IT和人力管理员 HR_集团高管 HR_集团人力相关人员]) } ||
          user.admin?)
     end
   end
