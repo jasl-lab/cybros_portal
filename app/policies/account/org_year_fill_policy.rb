70a8b827ba52b8a4ba04b42f9a7b8fc1defff807
Zero_Zeng <1154945484@qq.com>
2020-09-21 13:46:28 +0800

    change account report policy

--- app/policies/account/org_year_fill_policy.rb
+++ app/policies/account/org_year_fill_policy.rb
@@ -4,7 +4,8 @@ module Account
   class OrgYearFillPolicy < Struct.new(:user, :dashboard)
     def show?
       user.present? &&
-        (user.admin? || user.chinese_name.in?(%w(郭颖杰 余慧 陆文娟 周聪睿 蒋磊 吴悠 毕赢 孙涛 陈一恺)))
+        (user.admin? || user.roles.pluck(:role_name).any? { |r| r.in?(%w[CW_财务分析部管理员 CW_子公司高管1 CW_子公司高管2 CW_填报人]) } || 
+        	user.chinese_name.in?(%w(郭颖杰 余慧 陆文娟 周聪睿 蒋磊 吴悠 毕赢 孙涛 陈一恺)))
     end
   end
 end
