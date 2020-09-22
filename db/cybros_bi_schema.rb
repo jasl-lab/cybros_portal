44ddcd21a3128c1a9775bd21f8d31f7485388462
Eric-Guo <eric.guocz@gmail.com>
2020-09-22 14:39:12 +0800

    Run it.

--- db/cybros_bi_schema.rb
+++ db/cybros_bi_schema.rb
@@ -1488,11 +1488,25 @@ ActiveRecord::Schema.define(version: 2020_09_21_074807) do
     t.datetime "realcontractdate"
   end
 
-  create_table "cybros_dashbaords", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin", force: :cascade do |t|
-    t.integer "current_employee_hc"
-    t.integer "average_employee_hc"
-    t.integer "this_month_foreign_recruits"
-    t.integer "this_month_departures"
+  create_table "cybros_dashboards", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin", comment: "管理驾驶舱", force: :cascade do |t|
+    t.date "fill_at", comment: "数据填写日期"
+    t.integer "current_employee_hc", comment: "现有人数"
+    t.integer "average_employee_hc", comment: "平均人数"
+    t.integer "this_month_foreign_recruits", comment: "本月外招人数"
+    t.integer "this_month_departures", comment: "本月离职人数"
+    t.integer "accumulative_foreign_recruits", comment: "累计外招人数"
+    t.integer "accumulative_departures", comment: "累计离职人数"
+    t.integer "estimated_onboard_in_month", comment: "预计当月入职人数"
+    t.integer "turnover_rate", comment: "离职率"
+    t.decimal "contract_amounts", precision: 10, comment: "生产合同额"
+    t.decimal "predict_contracts", precision: 10, comment: "跟踪合同额"
+    t.decimal "contract_amount_of_the_month", precision: 10, comment: "当月生产合同额"
+    t.decimal "contract_amount_per_employee", precision: 10, comment: "全员人均生产合同额"
+    t.decimal "contract_amount_per_worker", precision: 10, comment: "一线人均生产合同额"
+    t.decimal "predict_contracts_per_employee", precision: 10, comment: "全员人均跟踪合同额"
+    t.decimal "predict_contracts_per_worker", precision: 10, comment: "全员人均跟踪合同额"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "v_gcz_projectitem", id: false, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin", force: :cascade do |t|
