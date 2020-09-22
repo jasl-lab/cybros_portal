09a5d77ae848acebd4f94d31ad0f30b2ed6c68b9
Eric-Guo <eric.guocz@gmail.com>
2020-09-22 17:30:05 +0800

    Fix merge errors.

--- app/policies/account/org_year_fill_policy.rb
+++ app/policies/account/org_year_fill_policy.rb
@@ -4,14 +4,9 @@ module Account
   class OrgYearFillPolicy < Struct.new(:user, :dashboard)
     def show?
       user.present? &&
-<<<<<<< HEAD
         (user.admin? ||
          user.roles.pluck(:role_name).any? { |r| r.in?(%w[CW_财务分析部管理员 CW_子公司高管1 CW_子公司高管2 CW_填报人]) } ||
          user.chinese_name.in?(%w(郭颖杰 余慧 陆文娟 周聪睿 蒋磊 吴悠 毕赢 孙涛 陈一恺)))
-=======
-        (user.admin? || user.roles.pluck(:role_name).any? { |r| r.in?(%w[CW_财务分析部管理员 CW_填报人]) } || 
-        	user.chinese_name.in?(%w(郭颖杰 余慧 陆文娟 周聪睿 蒋磊 吴悠 毕赢 孙涛 陈一恺)))
->>>>>>> df2e057f... change account fill report policy
     end
   end
 end
