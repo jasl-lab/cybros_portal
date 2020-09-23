43807df548f8bdd7c675d8871f034eb5d1d340d5
Zero_Zeng <1154945484@qq.com>
2020-09-23 10:13:26 +0800

    change HR report policy

--- app/policies/bi/group_subsidiary_hr_monthly_policy.rb
+++ app/policies/bi/group_subsidiary_hr_monthly_policy.rb
@@ -4,7 +4,7 @@ module Bi
   class GroupSubsidiaryHrMonthlyPolicy < Struct.new(:user, :dashboard)
     def show?
       user.present? &&
-		  (user.roles.pluck(:role_name).any? { |r| r.in?(%w[HR_集团人力相关人员 HR_子公司总经理、董事长 HR_集团高管 HR_人力资源报表HR经理 HR_IT和人力管理员 HR_所级管理者]) } ||
+		  (user.roles.pluck(:role_name).any? { |r| r.in?(%w[HR_集团人力相关人员 HR_子公司总经理、董事长 HR_人力资源报表责任人 HR_集团高管 HR_人力资源报表HR经理 HR_IT和人力管理员 HR_所级管理者 HR_填报人]) } ||
        user.admin?)
     end
   end
