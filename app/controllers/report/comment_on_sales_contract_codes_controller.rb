f459447e77899ffeaa1af55f0ac171655b6f1bf2
Eric-Guo <eric.guocz@gmail.com>
2020-09-27 10:28:50 +0800

    Disable button after success.

--- app/controllers/report/comment_on_sales_contract_codes_controller.rb
+++ app/controllers/report/comment_on_sales_contract_codes_controller.rb
@@ -3,8 +3,8 @@
 class Report::CommentOnSalesContractCodesController < ApplicationController
   def create
     coc_params = comment_on_sales_contract_code_params
-    coc = Bi::CommentOnSalesContractCode.find_or_initialize_by(sales_contract_code: coc_params[:sales_contract_code], record_month: coc_params[:record_month])
-    coc.update(coc_params)
+    @coc = Bi::CommentOnSalesContractCode.find_or_initialize_by(sales_contract_code: coc_params[:sales_contract_code], record_month: coc_params[:record_month])
+    @coc.update(coc_params)
   end
 
   private
