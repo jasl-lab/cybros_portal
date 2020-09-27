1d22ef61663c89fb7d222042fa28baee75608aea
Eric-Guo <eric.guocz@gmail.com>
2020-09-27 14:41:21 +0800

    Fix select bug.

--- app/models/bi/sub_company_need_receive_sign_detail.rb
+++ app/models/bi/sub_company_need_receive_sign_detail.rb
@@ -5,13 +5,13 @@ module Bi
     self.table_name = 'SUB_COMPANY_NEED_RECEIVE_SIGN_DETAIL'
 
     def self.all_month_names
-      @all_month_names ||= order(date: :desc).distinct.pluck(:date).collect { |d| d.to_s(:month_and_year) }
+      @all_month_names ||= order(date: :desc).distinct.pluck(:date).collect { |d| d.to_s(:month_and_year) }.uniq
     end
 
     def self.all_org_long_names(end_of_date)
       where(date: end_of_date).select(:orgname)
         .where.not(orgname: '上海天华嘉易建筑设计有限公司')
-        .distinct.pluck(:orgname)
+        .distinct.pluck(:orgname).uniq
     end
   end
 end
