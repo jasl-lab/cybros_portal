9f491e9df0a575123ec1594f06e6a535cda02834
Eric-Guo <eric.guocz@gmail.com>
2020-09-28 11:30:22 +0800

    Add form to search the cost_split/human_resources

--- app/controllers/cost_split/human_resources_controller.rb
+++ app/controllers/cost_split/human_resources_controller.rb
@@ -3,6 +3,10 @@
 class CostSplit::HumanResourcesController < CostSplit::BaseController
   def index
     prepare_meta_tags title: t('.title')
+    @all_company_names = []
+    @company_name = nil
+    @dept_options = []
+    @depts = []
   end
 
   protected
