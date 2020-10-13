1f1a15ec190442449446f4eee74598591df1792d
Eric-Guo <eric.guocz@gmail.com>
2020-10-13 14:09:29 +0800

    Check if group_rate.to_i + shanghai_area.to_i + shanghai_hq.to_i == 100

--- app/models/user_split_cost_setting.rb
+++ app/models/user_split_cost_setting.rb
@@ -1,3 +1,13 @@
 class UserSplitCostSetting < ApplicationRecord
   belongs_to :user
+
+  validate :split_rate_reach_100
+
+  def split_rate_reach_100
+    return if group_rate.to_i + shanghai_area.to_i + shanghai_hq.to_i == 100
+
+    errors.add(:group_rate, I18n.t('cost_split.human_resources.user_split_cost_setting_form.errors.empty_text'))
+    errors.add(:shanghai_area, I18n.t('cost_split.human_resources.user_split_cost_setting_form.errors.empty_text'))
+    errors.add(:shanghai_hq, I18n.t('cost_split.human_resources.user_split_cost_setting_form.errors.rate_not_reach_to_100'))
+  end
 end
