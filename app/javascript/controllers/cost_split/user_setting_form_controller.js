35ece205441d284e8e1cd7d87fd0d46a5024de86
Eric-Guo <eric.guocz@gmail.com>
2020-10-13 11:14:30 +0800

    No need stimulus controller.

--- app/javascript/controllers/cost_split/user_setting_form_controller.js
+++
@@ -1,9 +0,0 @@
-import { Controller } from "stimulus"
-
-export default class extends Controller {
-  static targets = [ "form" ]
-
-  submit_form(event) {
-    console.log("Hello submit_form");
-  }
-}
