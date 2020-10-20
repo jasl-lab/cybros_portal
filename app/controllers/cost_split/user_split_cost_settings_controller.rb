e97c7a69c25240601d5593f5a3909b516a7bcc99
Eric-Guo <eric.guocz@gmail.com>
2020-10-20 16:05:17 +0800

    Allow update the company for shanghai_area

--- app/controllers/cost_split/user_split_cost_settings_controller.rb
+++ app/controllers/cost_split/user_split_cost_settings_controller.rb
@@ -30,7 +30,7 @@ class CostSplit::UserSplitCostSettingsController < CostSplit::BaseController
       params.fetch(:user_split_cost_setting, {})
         .permit(:user_id, :org_code, :dept_code, :position_title,
           :group_rate, :shanghai_area, :shanghai_hq, :group_rate_base, :shanghai_area_base, :shanghai_hq_base, 
-          :version, user_split_cost_group_rate_companies_codes: [])
+          :version, user_split_cost_group_rate_companies_codes: [], user_split_cost_shanghai_area_rate_companies_codes: [])
     end
 
     def set_user_and_split_cost_setting
