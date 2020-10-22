2bfd2a28393c30c5ec9ad6d43e0530580e2be953
Eric-Guo <eric.guocz@gmail.com>
2020-10-22 09:07:50 +0800

    Allow create the SplitCostItem

--- app/controllers/cost_split/split_cost_items_controller.rb
+++ app/controllers/cost_split/split_cost_items_controller.rb
@@ -4,6 +4,12 @@ class CostSplit::SplitCostItemsController < CostSplit::BaseController
   def index
     prepare_meta_tags title: t('.title')
     @split_cost_items = SplitCost::SplitCostItem.all
+    @new_split_cost_item = SplitCost::SplitCostItem.new
+  end
+
+  def create
+    SplitCost::SplitCostItem.create(split_cost_item_params)
+    redirect_to cost_split_split_cost_items_path, notice: t('.success')
   end
 
   protected
@@ -16,5 +22,12 @@ class CostSplit::SplitCostItemsController < CostSplit::BaseController
         link: cost_split_root_path },
       { text: t('layouts.sidebar.cost_split.split_cost_items'),
         link: cost_split_split_cost_items_path }]
+    end
+
+  private
+
+    def split_cost_item_params
+      params.fetch(:split_cost_split_cost_item, {})
+        .permit(:split_cost_item_no, :split_cost_item_name, :split_cost_item_category)
     end
 end
--- app/models/split_cost/split_cost_item.rb
+++ app/models/split_cost/split_cost_item.rb
@@ -2,5 +2,6 @@
 
 module SplitCost
   class SplitCostItem < ApplicationRecord
+    validates :split_cost_item_no, :split_cost_item_name, :split_cost_item_category, presence: true
   end
 end
