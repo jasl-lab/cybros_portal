53fa9824f192e0f07c0e3d17f96bf9ff2a13ad84
Eric-Guo <eric.guocz@gmail.com>
2020-10-22 11:15:02 +0800

    New SplitCostItem CATEGORY

--- app/models/split_cost/split_cost_item.rb
+++ app/models/split_cost/split_cost_item.rb
@@ -2,6 +2,7 @@
 
 module SplitCost
   class SplitCostItem < ApplicationRecord
+    CATEGORY = %w[固定资产 无形资产 业务性支出预算]
     validates :split_cost_item_no, :split_cost_item_name, :split_cost_item_category, presence: true
   end
 end
