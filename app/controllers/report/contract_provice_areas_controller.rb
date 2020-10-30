bdcb7be78c6dcc09f47d667fae3eb35017f7d1b6
Eric-Guo <eric.guocz@gmail.com>
2020-10-30 12:32:06 +0800

    null is 0.

--- app/controllers/report/contract_provice_areas_controller.rb
+++ app/controllers/report/contract_provice_areas_controller.rb
@@ -63,44 +63,44 @@ class Report::ContractProviceAreasController < Report::BaseController
     end
 
     def province_new_area(sum_scope)
-      sum_台湾 = sum_scope.find { |c| c.province == '台湾省' }&.new_area
-      sum_河北 = sum_scope.find { |c| c.province == '河北省' }&.new_area
-      sum_山西 = sum_scope.find { |c| c.province == '山西省' }&.new_area
-      sum_内蒙古 = sum_scope.find { |c| c.province == '内蒙古自治区' }&.new_area
-      sum_辽宁 = sum_scope.find { |c| c.province == '辽宁省' }&.new_area
-      sum_吉林 = sum_scope.find { |c| c.province == '吉林省' }&.new_area
-      sum_黑龙江 = sum_scope.find { |c| c.province == '黑龙江省' }&.new_area
+      sum_台湾 = sum_scope.find { |c| c.province == '台湾省' }&.new_area || 0
+      sum_河北 = sum_scope.find { |c| c.province == '河北省' }&.new_area || 0
+      sum_山西 = sum_scope.find { |c| c.province == '山西省' }&.new_area || 0
+      sum_内蒙古 = sum_scope.find { |c| c.province == '内蒙古自治区' }&.new_area || 0
+      sum_辽宁 = sum_scope.find { |c| c.province == '辽宁省' }&.new_area || 0
+      sum_吉林 = sum_scope.find { |c| c.province == '吉林省' }&.new_area || 0
+      sum_黑龙江 = sum_scope.find { |c| c.province == '黑龙江省' }&.new_area || 0
 
-      sum_江苏 = sum_scope.find { |c| c.province == '江苏省' }&.new_area
-      sum_浙江 = sum_scope.find { |c| c.province == '浙江省' }&.new_area
-      sum_安徽 = sum_scope.find { |c| c.province == '安徽省' }&.new_area
-      sum_福建 = sum_scope.find { |c| c.province == '福建省' }&.new_area
-      sum_江西 = sum_scope.find { |c| c.province == '江西省' }&.new_area
-      sum_山东 = sum_scope.find { |c| c.province == '山东省' }&.new_area
-      sum_河南 = sum_scope.find { |c| c.province == '河南省' }&.new_area
+      sum_江苏 = sum_scope.find { |c| c.province == '江苏省' }&.new_area || 0
+      sum_浙江 = sum_scope.find { |c| c.province == '浙江省' }&.new_area || 0
+      sum_安徽 = sum_scope.find { |c| c.province == '安徽省' }&.new_area || 0
+      sum_福建 = sum_scope.find { |c| c.province == '福建省' }&.new_area || 0
+      sum_江西 = sum_scope.find { |c| c.province == '江西省' }&.new_area || 0
+      sum_山东 = sum_scope.find { |c| c.province == '山东省' }&.new_area || 0
+      sum_河南 = sum_scope.find { |c| c.province == '河南省' }&.new_area || 0
 
-      sum_湖北 = sum_scope.find { |c| c.province == '湖北省' }&.new_area
-      sum_湖南 = sum_scope.find { |c| c.province == '湖南省' }&.new_area
-      sum_广东 = sum_scope.find { |c| c.province == '广东省' }&.new_area
-      sum_广西 = sum_scope.find { |c| c.province == '广西壮族自治区' }&.new_area
-      sum_海南 = sum_scope.find { |c| c.province == '海南省' }&.new_area
-      sum_四川 = sum_scope.find { |c| c.province == '四川省' }&.new_area
-      sum_贵州 = sum_scope.find { |c| c.province == '贵州省' }&.new_area
+      sum_湖北 = sum_scope.find { |c| c.province == '湖北省' }&.new_area || 0
+      sum_湖南 = sum_scope.find { |c| c.province == '湖南省' }&.new_area || 0
+      sum_广东 = sum_scope.find { |c| c.province == '广东省' }&.new_area || 0
+      sum_广西 = sum_scope.find { |c| c.province == '广西壮族自治区' }&.new_area || 0
+      sum_海南 = sum_scope.find { |c| c.province == '海南省' }&.new_area || 0
+      sum_四川 = sum_scope.find { |c| c.province == '四川省' }&.new_area || 0
+      sum_贵州 = sum_scope.find { |c| c.province == '贵州省' }&.new_area || 0
 
-      sum_云南 = sum_scope.find { |c| c.province == '云南省' }&.new_area
-      sum_西藏 = sum_scope.find { |c| c.province == '西藏自治区' }&.new_area
-      sum_陕西 = sum_scope.find { |c| c.province == '陕西省' }&.new_area
-      sum_甘肃 = sum_scope.find { |c| c.province == '甘肃省' }&.new_area
-      sum_青海 = sum_scope.find { |c| c.province == '青海省' }&.new_area
-      sum_宁夏 = sum_scope.find { |c| c.province == '宁夏回族自治区' }&.new_area
-      sum_新疆 = sum_scope.find { |c| c.province == '新疆维吾尔自治区' }&.new_area
+      sum_云南 = sum_scope.find { |c| c.province == '云南省' }&.new_area || 0
+      sum_西藏 = sum_scope.find { |c| c.province == '西藏自治区' }&.new_area || 0
+      sum_陕西 = sum_scope.find { |c| c.province == '陕西省' }&.new_area || 0
+      sum_甘肃 = sum_scope.find { |c| c.province == '甘肃省' }&.new_area || 0
+      sum_青海 = sum_scope.find { |c| c.province == '青海省' }&.new_area || 0
+      sum_宁夏 = sum_scope.find { |c| c.province == '宁夏回族自治区' }&.new_area || 0
+      sum_新疆 = sum_scope.find { |c| c.province == '新疆维吾尔自治区' }&.new_area || 0
 
-      sum_北京 = sum_scope.find { |c| c.province == '北京市' }&.new_area
-      sum_天津 = sum_scope.find { |c| c.province == '天津市' }&.new_area
-      sum_上海 = sum_scope.find { |c| c.province == '上海市' }&.new_area
-      sum_重庆 = sum_scope.find { |c| c.province == '重庆市' }&.new_area
-      sum_香港 = sum_scope.find { |c| c.province == '香港特别行政区' }&.new_area
-      sum_澳门 = sum_scope.find { |c| c.province == '澳门特别行政区' }&.new_area
+      sum_北京 = sum_scope.find { |c| c.province == '北京市' }&.new_area || 0
+      sum_天津 = sum_scope.find { |c| c.province == '天津市' }&.new_area || 0
+      sum_上海 = sum_scope.find { |c| c.province == '上海市' }&.new_area || 0
+      sum_重庆 = sum_scope.find { |c| c.province == '重庆市' }&.new_area || 0
+      sum_香港 = sum_scope.find { |c| c.province == '香港特别行政区' }&.new_area || 0
+      sum_澳门 = sum_scope.find { |c| c.province == '澳门特别行政区' }&.new_area || 0
 
       # There is no place to drawing c.province == '其他' and '海外'
 
@@ -113,44 +113,44 @@ class Report::ContractProviceAreasController < Report::BaseController
     end
 
     def province_area_rates(area_rates)
-      sum_台湾 = area_rates.find { |c| c[:province] == '台湾省' }&.fetch(:new_area_rate, nil)
-      sum_河北 = area_rates.find { |c| c[:province] == '河北省' }&.fetch(:new_area_rate, nil)
-      sum_山西 = area_rates.find { |c| c[:province] == '山西省' }&.fetch(:new_area_rate, nil)
-      sum_内蒙古 = area_rates.find { |c| c[:province] == '内蒙古自治区' }&.fetch(:new_area_rate, nil)
-      sum_辽宁 = area_rates.find { |c| c[:province] == '辽宁省' }&.fetch(:new_area_rate, nil)
-      sum_吉林 = area_rates.find { |c| c[:province] == '吉林省' }&.fetch(:new_area_rate, nil)
-      sum_黑龙江 = area_rates.find { |c| c[:province] == '黑龙江省' }&.fetch(:new_area_rate, nil)
+      sum_台湾 = area_rates.find { |c| c[:province] == '台湾省' }&.fetch(:new_area_rate, nil) || 0
+      sum_河北 = area_rates.find { |c| c[:province] == '河北省' }&.fetch(:new_area_rate, nil) || 0
+      sum_山西 = area_rates.find { |c| c[:province] == '山西省' }&.fetch(:new_area_rate, nil) || 0
+      sum_内蒙古 = area_rates.find { |c| c[:province] == '内蒙古自治区' }&.fetch(:new_area_rate, nil) || 0
+      sum_辽宁 = area_rates.find { |c| c[:province] == '辽宁省' }&.fetch(:new_area_rate, nil) || 0
+      sum_吉林 = area_rates.find { |c| c[:province] == '吉林省' }&.fetch(:new_area_rate, nil) || 0
+      sum_黑龙江 = area_rates.find { |c| c[:province] == '黑龙江省' }&.fetch(:new_area_rate, nil) || 0
 
-      sum_江苏 = area_rates.find { |c| c[:province] == '江苏省' }&.fetch(:new_area_rate, nil)
-      sum_浙江 = area_rates.find { |c| c[:province] == '浙江省' }&.fetch(:new_area_rate, nil)
-      sum_安徽 = area_rates.find { |c| c[:province] == '安徽省' }&.fetch(:new_area_rate, nil)
-      sum_福建 = area_rates.find { |c| c[:province] == '福建省' }&.fetch(:new_area_rate, nil)
-      sum_江西 = area_rates.find { |c| c[:province] == '江西省' }&.fetch(:new_area_rate, nil)
-      sum_山东 = area_rates.find { |c| c[:province] == '山东省' }&.fetch(:new_area_rate, nil)
-      sum_河南 = area_rates.find { |c| c[:province] == '河南省' }&.fetch(:new_area_rate, nil)
+      sum_江苏 = area_rates.find { |c| c[:province] == '江苏省' }&.fetch(:new_area_rate, nil) || 0
+      sum_浙江 = area_rates.find { |c| c[:province] == '浙江省' }&.fetch(:new_area_rate, nil) || 0
+      sum_安徽 = area_rates.find { |c| c[:province] == '安徽省' }&.fetch(:new_area_rate, nil) || 0
+      sum_福建 = area_rates.find { |c| c[:province] == '福建省' }&.fetch(:new_area_rate, nil) || 0
+      sum_江西 = area_rates.find { |c| c[:province] == '江西省' }&.fetch(:new_area_rate, nil) || 0
+      sum_山东 = area_rates.find { |c| c[:province] == '山东省' }&.fetch(:new_area_rate, nil) || 0
+      sum_河南 = area_rates.find { |c| c[:province] == '河南省' }&.fetch(:new_area_rate, nil) || 0
 
-      sum_湖北 = area_rates.find { |c| c[:province] == '湖北省' }&.fetch(:new_area_rate, nil)
-      sum_湖南 = area_rates.find { |c| c[:province] == '湖南省' }&.fetch(:new_area_rate, nil)
-      sum_广东 = area_rates.find { |c| c[:province] == '广东省' }&.fetch(:new_area_rate, nil)
-      sum_广西 = area_rates.find { |c| c[:province] == '广西壮族自治区' }&.fetch(:new_area_rate, nil)
-      sum_海南 = area_rates.find { |c| c[:province] == '海南省' }&.fetch(:new_area_rate, nil)
-      sum_四川 = area_rates.find { |c| c[:province] == '四川省' }&.fetch(:new_area_rate, nil)
-      sum_贵州 = area_rates.find { |c| c[:province] == '贵州省' }&.fetch(:new_area_rate, nil)
+      sum_湖北 = area_rates.find { |c| c[:province] == '湖北省' }&.fetch(:new_area_rate, nil) || 0
+      sum_湖南 = area_rates.find { |c| c[:province] == '湖南省' }&.fetch(:new_area_rate, nil) || 0
+      sum_广东 = area_rates.find { |c| c[:province] == '广东省' }&.fetch(:new_area_rate, nil) || 0
+      sum_广西 = area_rates.find { |c| c[:province] == '广西壮族自治区' }&.fetch(:new_area_rate, nil) || 0
+      sum_海南 = area_rates.find { |c| c[:province] == '海南省' }&.fetch(:new_area_rate, nil) || 0
+      sum_四川 = area_rates.find { |c| c[:province] == '四川省' }&.fetch(:new_area_rate, nil) || 0
+      sum_贵州 = area_rates.find { |c| c[:province] == '贵州省' }&.fetch(:new_area_rate, nil) || 0
 
-      sum_云南 = area_rates.find { |c| c[:province] == '云南省' }&.fetch(:new_area_rate, nil)
-      sum_西藏 = area_rates.find { |c| c[:province] == '西藏自治区' }&.fetch(:new_area_rate, nil)
-      sum_陕西 = area_rates.find { |c| c[:province] == '陕西省' }&.fetch(:new_area_rate, nil)
-      sum_甘肃 = area_rates.find { |c| c[:province] == '甘肃省' }&.fetch(:new_area_rate, nil)
-      sum_青海 = area_rates.find { |c| c[:province] == '青海省' }&.fetch(:new_area_rate, nil)
-      sum_宁夏 = area_rates.find { |c| c[:province] == '宁夏回族自治区' }&.fetch(:new_area_rate, nil)
-      sum_新疆 = area_rates.find { |c| c[:province] == '新疆维吾尔自治区' }&.fetch(:new_area_rate, nil)
+      sum_云南 = area_rates.find { |c| c[:province] == '云南省' }&.fetch(:new_area_rate, nil) || 0
+      sum_西藏 = area_rates.find { |c| c[:province] == '西藏自治区' }&.fetch(:new_area_rate, nil) || 0
+      sum_陕西 = area_rates.find { |c| c[:province] == '陕西省' }&.fetch(:new_area_rate, nil) || 0
+      sum_甘肃 = area_rates.find { |c| c[:province] == '甘肃省' }&.fetch(:new_area_rate, nil) || 0
+      sum_青海 = area_rates.find { |c| c[:province] == '青海省' }&.fetch(:new_area_rate, nil) || 0
+      sum_宁夏 = area_rates.find { |c| c[:province] == '宁夏回族自治区' }&.fetch(:new_area_rate, nil) || 0
+      sum_新疆 = area_rates.find { |c| c[:province] == '新疆维吾尔自治区' }&.fetch(:new_area_rate, nil) || 0
 
-      sum_北京 = area_rates.find { |c| c[:province] == '北京市' }&.fetch(:new_area_rate, nil)
-      sum_天津 = area_rates.find { |c| c[:province] == '天津市' }&.fetch(:new_area_rate, nil)
-      sum_上海 = area_rates.find { |c| c[:province] == '上海市' }&.fetch(:new_area_rate, nil)
-      sum_重庆 = area_rates.find { |c| c[:province] == '重庆市' }&.fetch(:new_area_rate, nil)
-      sum_香港 = area_rates.find { |c| c[:province] == '香港特别行政区' }&.fetch(:new_area_rate, nil)
-      sum_澳门 = area_rates.find { |c| c[:province] == '澳门特别行政区' }&.fetch(:new_area_rate, nil)
+      sum_北京 = area_rates.find { |c| c[:province] == '北京市' }&.fetch(:new_area_rate, nil) || 0
+      sum_天津 = area_rates.find { |c| c[:province] == '天津市' }&.fetch(:new_area_rate, nil) || 0
+      sum_上海 = area_rates.find { |c| c[:province] == '上海市' }&.fetch(:new_area_rate, nil) || 0
+      sum_重庆 = area_rates.find { |c| c[:province] == '重庆市' }&.fetch(:new_area_rate, nil) || 0
+      sum_香港 = area_rates.find { |c| c[:province] == '香港特别行政区' }&.fetch(:new_area_rate, nil) || 0
+      sum_澳门 = area_rates.find { |c| c[:province] == '澳门特别行政区' }&.fetch(:new_area_rate, nil) || 0
 
       # There is no place to drawing c[:province] == '其他' and '海外'
 
