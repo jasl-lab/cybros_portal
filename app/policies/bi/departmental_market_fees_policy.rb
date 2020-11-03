7104204f9463f5a8e5f55d2d4ca2937e9ad27d1f
Zero_Zeng <1154945484@qq.com>
2020-11-03 10:04:45 +0800

    add admission for 陈玲

--- app/policies/bi/departmental_market_fees_policy.rb
+++ app/policies/bi/departmental_market_fees_policy.rb
@@ -6,7 +6,7 @@ module Bi
       return false unless user.present?
 
 
-      user.chinese_name.in?(%w(郭颖杰 余慧 陆文娟 周聪睿 蒋磊 吴悠 毕赢 孙涛 陈一恺 高洋)) ||
+      user.chinese_name.in?(%w(郭颖杰 余慧 陆文娟 周聪睿 蒋磊 吴悠 毕赢 孙涛 陈一恺 陈玲)) ||
         user.roles.pluck(:role_name).any? { |r| r.in?(%w[CW_财务分析部管理员 CW_所级管理者1]) } ||
         user.admin?
     end
--- app/policies/bi/dept_homepage_policy.rb
+++ app/policies/bi/dept_homepage_policy.rb
@@ -5,7 +5,7 @@ module Bi
     def show?
       return false unless user.present?
 
-      user.chinese_name.in?(%w(郭颖杰 余慧 陆文娟 周聪睿 蒋磊 吴悠 毕赢 孙涛 陈一恺 高洋)) ||
+      user.chinese_name.in?(%w(郭颖杰 余慧 陆文娟 周聪睿 蒋磊 吴悠 毕赢 孙涛 陈一恺 陈玲)) ||
         user.roles.pluck(:role_name).any? { |r| r.in?(%w[CW_财务分析部管理员 CW_集团高管 CW_子公司高管1 CW_子公司高管2 CW_所级管理者1 CW_所级管理者2]) } ||
         user.admin?
     end
