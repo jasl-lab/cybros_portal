98259eea0e4a3f0aee8bbc54033069dbec958367
Eric-Guo <eric.guocz@gmail.com>
2020-11-03 09:19:30 +0800

    Another fix for company home page.

--- app/javascript/controllers/company_hot_loader_controller.js
+++ app/javascript/controllers/company_hot_loader_controller.js
@@ -12,7 +12,7 @@ export default class extends Controller {
       const data = response.data[0];
       console.log(data);
 
-      $("#company-hots-img").attr("src", data.cover);
+      $("#company-hots-img").attr("src", `https://portal.thape.com.cn${data.cover}`);
       $("#company-hots-title").html(data.title);
       $("#company-hots-text").html(data.description);
       $("#company-hots-link").attr("href", data.url);
