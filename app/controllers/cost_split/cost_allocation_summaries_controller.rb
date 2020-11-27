5aedd68b33165da46c40a09f1939422dee040ad6
Eric-Guo <eric.guocz@gmail.com>
2020-11-27 16:35:28 +0800

    Should be able to select month name.

--- app/controllers/cost_split/cost_allocation_summaries_controller.rb
+++ app/controllers/cost_split/cost_allocation_summaries_controller.rb
@@ -3,5 +3,7 @@
 class CostSplit::CostAllocationSummariesController < CostSplit::BaseController
   def show
     prepare_meta_tags title: t('.title')
+    @all_month_names = policy_scope(SplitCost::UserSplitCostDetail).all_month_names
+    @month_name = params[:month_name]&.strip || @all_month_names.first
   end
 end
--- app/models/split_cost/user_split_cost_detail.rb
+++ app/models/split_cost/user_split_cost_detail.rb
@@ -3,5 +3,9 @@
 module SplitCost
   class UserSplitCostDetail < ApplicationRecord
     belongs_to :user
+
+    def self.all_month_names
+      order(month: :desc).distinct.pluck(:month).collect { |d| d.to_s(:month_and_year) }.uniq
+    end
   end
 end
