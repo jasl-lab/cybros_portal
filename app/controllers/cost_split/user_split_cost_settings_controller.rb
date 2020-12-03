b5fc6a7cf052b4b9247147319612b166b8a4b9b2
Eric-Guo <eric.guocz@gmail.com>
2020-12-03 13:50:36 +0800

    Add links for pop-up the new and edit forms.

--- app/controllers/cost_split/user_split_cost_settings_controller.rb
+++ app/controllers/cost_split/user_split_cost_settings_controller.rb
@@ -1,7 +1,10 @@
 # frozen_string_literal: true
 
 class CostSplit::UserSplitCostSettingsController < CostSplit::BaseController
-  before_action :set_user_and_split_cost_setting, except: :create
+  before_action :set_user_and_split_cost_setting, except: %i[create new]
+
+  def new
+  end
 
   def create
     @scs = SplitCost::UserSplitCostSetting.new(scs_params)
@@ -11,17 +14,11 @@ class CostSplit::UserSplitCostSettingsController < CostSplit::BaseController
     @scs.save
   end
 
+  def edit
+  end
+
   def update
     @scs.update(scs_params)
-  end
-
-  def confirm
-    @scs.update(confirmed: true)
-  end
-
-  def version_up
-    @scs.update(end_date: Date.today)
-    redirect_to cost_split_human_resources_path, notice: t('.success')
   end
 
   private
@@ -29,10 +26,10 @@ class CostSplit::UserSplitCostSettingsController < CostSplit::BaseController
     def scs_params
       params.fetch(:split_cost_user_split_cost_setting, {})
         .permit(:user_id, :org_code, :dept_code, :position_title,
-          :group_rate, :shanghai_area, :shanghai_hq, 
-          :group_rate_base, :shanghai_area_base, :shanghai_hq_base, 
+          :group_rate, :shanghai_area, :shanghai_hq,
+          :group_rate_base, :shanghai_area_base, :shanghai_hq_base,
           :version,
-          user_split_cost_group_rate_companies_codes: [], 
+          user_split_cost_group_rate_companies_codes: [],
           user_split_cost_shanghai_area_rate_companies_codes: [],
           user_split_cost_shanghai_hq_rate_companies_codes: [])
     end
