84ba8b35a7634f2f2d843242aa936c0ec5fc854d
Eric-Guo <eric.guocz@gmail.com>
2020-12-07 09:42:42 +0800

    Fetch rest of the month if there is non-record for the same sales_contract_code

--- app/datatables/subsidiary_need_receive_sign_detail_datatable.rb
+++ app/datatables/subsidiary_need_receive_sign_detail_datatable.rb
@@ -34,26 +34,16 @@ class SubsidiaryNeedReceiveSignDetailDatatable < ApplicationDatatable
   end
 
   def data
-    comment_end_of_date = @end_of_date.end_of_month
-    comment_previous_end_of_date = (@end_of_date - 1.month).end_of_month
     sales_contract_codes = records.collect(&:salescontractcode)
-    coc_records = Bi::CommentOnSalesContractCode.where(sales_contract_code: sales_contract_codes, record_month: comment_end_of_date)
-    coc_previous_records = Bi::CommentOnSalesContractCode.where(sales_contract_code: sales_contract_codes, record_month: comment_previous_end_of_date)
-    coc_histories = Bi::CommentOnSalesContractCode.where(sales_contract_code: sales_contract_codes)
+    coc_histories = Bi::CommentOnSalesContractCode.order(record_month: :desc).where(sales_contract_code: sales_contract_codes)
     records.map do |r|
-      coc = coc_records.find { |c| c.sales_contract_code == r.salescontractcode && c.record_month == comment_end_of_date }
-      prev_coc = coc_previous_records.find { |c| c.sales_contract_code == r.salescontractcode && c.record_month == comment_previous_end_of_date }
-      coc = if coc.present?
-        coc
-      elsif prev_coc.present?
-        prev_coc.record_month = comment_end_of_date
-        prev_coc
+      coc_history = coc_histories.find_all { |c| c.sales_contract_code == r.salescontractcode }
+      coc = if coc_history.present?
+        coc_history.first
       else
-        Bi::CommentOnSalesContractCode.new(sales_contract_code: r.salescontractcode, record_month: comment_end_of_date)
+        Bi::CommentOnSalesContractCode.new(sales_contract_code: r.salescontractcode, record_month: @end_of_date.end_of_month)
       end
-      coc_history = coc_histories.find_all { |c| c.sales_contract_code == r.salescontractcode }.collect do |c|
-        "#{c.record_month}: #{sanitize c.comment}"
-      end
+      display_coc_history = coc_history.collect { |c| "#{c.record_month}: #{sanitize c.comment}" }
       { org_dept_name: "#{Bi::OrgShortName.company_short_names.fetch(r.orgname, r.orgname)}<br />#{r.deptname}".html_safe,
         business_director_name: r.businessdirectorname,
         first_party_name_and_comment: "#{r.firstpartyname}<br /><i>#{coc.comment}</i>".html_safe,
@@ -63,9 +53,9 @@ class SubsidiaryNeedReceiveSignDetailDatatable < ApplicationDatatable
         contract_time: r.contracttime.to_date,
         acc_need_receive: tag.div((r.accneedreceive.to_f / 10000.0)&.round(0), class: 'text-center'),
         sign_receive: tag.div((r.sign_receive.to_f / 10000.0)&.round(0), class: 'text-center'),
-        over_amount: tag.div((r.overamount.to_f / 10000.0)&.round(0), class: "text-center"),
+        over_amount: tag.div((r.overamount.to_f / 10000.0)&.round(0), class: 'text-center'),
         comment_on_sales_contract_code:
-          render(partial: 'report/subsidiary_need_receive_sign_details/comment', locals: { coc: coc, coc_history: coc_history }),
+          render(partial: 'report/subsidiary_need_receive_sign_details/comment', locals: { coc: coc, coc_history: display_coc_history }),
         admin_action: if @show_hide
                         link_to(un_hide_icon, un_hide_report_subsidiary_need_receive_sign_detail_path(sales_contract_code: r.salescontractcode), method: :patch)
                       else
