f38cc02531b7a5d017ea6a294023858b95f4ce1e
Eric-Guo <eric.guocz@gmail.com>
2021-01-06 08:01:25 +0800

    upgrade capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid for current version and patch releases of Capistrano
-lock '~> 3.14.0'
+lock '~> 3.15.0'
 
 set :application, 'cybros'
 set :repo_url, 'git@e.coding.net:sew/cybros_portal.git'
