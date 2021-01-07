30820618ecefd8efa4e1d38859d73eb394a6a877
Eric-Guo <eric.guocz@gmail.com>
2021-01-07 11:35:18 +0800

    Code style improvement.

--- app/models/bi/sh_refresh_rate.rb
+++ app/models/bi/sh_refresh_rate.rb
@@ -9,17 +9,17 @@ module Bi
     end
 
     def self.available_company_names(target_date)
-      orgcodes = where(date: target_date).where("SH_REFRESH_RATE.orgcode IS NOT NULL")
-        .joins("LEFT JOIN ORG_ORDER on ORG_ORDER.org_code = SH_REFRESH_RATE.orgcode")
+      where(date: target_date).where('SH_REFRESH_RATE.orgcode IS NOT NULL')
+        .joins('LEFT JOIN ORG_ORDER on ORG_ORDER.org_code = SH_REFRESH_RATE.orgcode')
         .where('ORG_ORDER.org_order is not null')
         .where("ORG_ORDER.org_type = '创意板块'")
-        .order("ORG_ORDER.org_order DESC")
+        .order('ORG_ORDER.org_order DESC')
         .pluck(:orgcode).uniq
         .collect { |c| [Bi::OrgShortName.company_short_names_by_orgcode.fetch(c, c), c] }
     end
 
     def self.person_count_by_department(target_date)
-      deps = where(date: target_date).where("SH_REFRESH_RATE.orgcode IS NOT NULL")
+      deps = where(date: target_date).where('SH_REFRESH_RATE.orgcode IS NOT NULL')
         .select('deptcode, count(work_no) work_no')
         .group(:deptcode).order(deptcode: :asc)
       @_person_count_by_department = deps.reduce({}) do |h, s|
@@ -31,7 +31,7 @@ module Bi
     def self.person_by_department_in_sh(target_date, orgcode)
       lad = where(date: target_date).where("是否显示 = '1'")
         .joins("LEFT JOIN ORG_REPORT_DEPT_ORDER on ORG_REPORT_DEPT_ORDER.编号 = SH_REFRESH_RATE.deptcode and ORG_REPORT_DEPT_ORDER.开始时间 <= '#{target_date.to_date}' and (ORG_REPORT_DEPT_ORDER.结束时间 >= '#{target_date.to_date}' or ORG_REPORT_DEPT_ORDER.结束时间 is null)")
-        .order(Arel.sql("ORG_REPORT_DEPT_ORDER.部门排名, SH_REFRESH_RATE.deptcode"))
+        .order(Arel.sql('ORG_REPORT_DEPT_ORDER.部门排名, SH_REFRESH_RATE.deptcode'))
       lad = lad.where(orgcode: orgcode)
       h = {}
       department_code_in_order = lad.collect(&:deptcode).uniq
