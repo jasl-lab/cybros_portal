20593ea75c35164bf557bece54ed4b3802b2a6b1
Zero_Zeng <1154945484@qq.com>
2021-01-19 16:33:23 +0800

    change hr report policy

--- app/policies/bi/group_subsidiary_hr_monthly_pure_policy.rb
+++ app/policies/bi/group_subsidiary_hr_monthly_pure_policy.rb
@@ -5,7 +5,7 @@ module Bi
     def show?
       return false unless user.present?
 
-      user.roles.pluck(:role_name).any? { |r| r.in?(%w[HR_IT和人力管理员]) } ||
+      user.roles.pluck(:role_name).any? { |r| r.in?(%w[HR_IT和人力管理员 HR_人力资源报表HR经理 HR_填报人]) } ||
         user.admin?
     end
   end
