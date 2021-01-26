296100f53aca70e0b21d36c5c2a118363d09735b
Eric-Guo <eric.guocz@gmail.com>
2021-01-26 10:45:05 +0800

    structure of the set_baseline_job_types

--- app/controllers/cost_split/set_baseline_job_types_controller.rb
+++ app/controllers/cost_split/set_baseline_job_types_controller.rb
@@ -3,5 +3,7 @@
 class CostSplit::SetBaselineJobTypesController < CostSplit::BaseController
   def index
     prepare_meta_tags title: t('.title')
+    @user_salary_classifications = SplitCost::UserSalaryClassification.all.order(:code)
+    @user_job_types = SplitCost::UserJobType.all.order(:code)
   end
 end
