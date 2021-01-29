cf21cd175d65a9f10239b892e5d8694b0e05d3db
Eric-Guo <eric.guocz@gmail.com>
2021-01-29 14:21:53 +0800

    Review set_part_time_person_costs

--- app/controllers/cost_split/set_part_time_person_costs_controller.rb
+++ app/controllers/cost_split/set_part_time_person_costs_controller.rb
@@ -1,10 +1,9 @@
 # frozen_string_literal: true
 
 class CostSplit::SetPartTimePersonCostsController < CostSplit::BaseController
-    before_action :set_monthly_salary_split_rule, except: %i[index]
-  
-    def index
-      prepare_meta_tags title: t('.title')
-    end
+  before_action :set_monthly_salary_split_rule, except: %i[index]
+
+  def index
+    prepare_meta_tags title: t('.title')
   end
-  
+end
