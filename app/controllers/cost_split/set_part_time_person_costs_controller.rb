b959b6cbcd893522cb55d9e1f62aa214602ae4ee
Eric-Guo <eric.guocz@gmail.com>
2021-02-03 13:48:14 +0800

    Table to show positions on users.

--- app/controllers/cost_split/set_part_time_person_costs_controller.rb
+++ app/controllers/cost_split/set_part_time_person_costs_controller.rb
@@ -5,16 +5,31 @@ class CostSplit::SetPartTimePersonCostsController < CostSplit::BaseController
 
   def index
     prepare_meta_tags title: t('.title')
-    @ncworkno = params[:ncworkno] || current_user.clerk_code
+    @ncworkno = params[:ncworkno] || User.first.clerk_code
     
     @all_month_names = policy_scope(SplitCost::UserMonthlyPartTimeSplitRate).all_month_names
     @month_name = params[:month_name]&.strip || @all_month_names.first
     beginning_of_month = Date.parse(@month_name).beginning_of_month
+
+    @users = User.joins(:position_users)
+      .where(position_users: { main_position: false }).distinct
+
+    if @ncworkno.present?
+      @users.where(clerk_code: @ncworkno)
+    end
+
+    @mpts_rates = if @users.first != User.first
+      policy_scope(SplitCost::UserMonthlyPartTimeSplitRate).where(user: @users.first)
+    else
+      policy_scope(SplitCost::UserMonthlyPartTimeSplitRate)
+    end.where(month: beginning_of_month)
+
+    @users = @users.page(params[:page]).per(params[:per_page])
   end
 
   def part_time_people
     @users = User.joins(:position_users)
-      .where(position_users: { main_position: false})
-      .where('chinese_name LIKE ?', "%#{params[:q]}%").limit(7)
+      .where(position_users: { main_position: false })
+      .where('chinese_name LIKE ?', "%#{params[:q]}%").distinct.limit(7)
   end
 end
