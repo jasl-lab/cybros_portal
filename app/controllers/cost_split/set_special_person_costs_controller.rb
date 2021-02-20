7e41ab99b7f1ae34c5d3d346296935a7921763e5
Eric-Guo <eric.guocz@gmail.com>
2021-02-20 09:57:01 +0800

    Part of the set_special_person_costs page.

--- app/controllers/cost_split/set_special_person_costs_controller.rb
+++ app/controllers/cost_split/set_special_person_costs_controller.rb
@@ -5,5 +5,19 @@ class CostSplit::SetSpecialPersonCostsController < CostSplit::BaseController
 
   def index
     prepare_meta_tags title: t('.title')
+    @ncworkno = params[:ncworkno] || User.first.clerk_code
+    @all_month_names = policy_scope(SplitCost::UserMonthlyPartTimeSpecialJobType).all_month_names
+    @month_name = params[:month_name]&.strip || @all_month_names.first
+    beginning_of_month = Date.parse(@month_name).beginning_of_month
+
+    target_user = if @ncworkno.present?
+      User.find_by(clerk_code: @ncworkno)
+    end
+
+    @mpts_job_types = if target_user != User.first
+      policy_scope(SplitCost::UserMonthlyPartTimeSpecialJobType).where(user: target_user)
+    else
+      policy_scope(SplitCost::UserMonthlyPartTimeSpecialJobType)
+    end.where(month: beginning_of_month)
   end
 end
