e80fc4706d79c90d6070bdd1bc29ab6b4d48e82d
Eric-Guo <eric.guocz@gmail.com>
2021-02-20 14:07:05 +0800

    Also no need.

--- app/models/wechat_user.rb
+++ app/models/wechat_user.rb
@@ -2,8 +2,4 @@
 
 class WechatUser < ActiveRecord::Base
   devise :database_authenticatable, :jwt_authenticatable, jwt_revocation_strategy: Devise::JWT::RevocationStrategies::Null
-
-  def self.find_for_jwt_authentication(sub)
-    find_by(id: sub)
-  end
 end
