18b1129a30cf029227ecd77e11366ec1abb5dbda
Zero_Zeng <1154945484@qq.com>
2021-02-22 17:12:02 +0800

    Change operation summary report show type

--- app/controllers/account/operation_summary_depts_controller.rb
+++ app/controllers/account/operation_summary_depts_controller.rb
@@ -9,7 +9,7 @@ class Account::OperationSummaryDeptsController < Account::BaseController
   def show
     authorize :"Account::OperationSummaryDept"
     prepare_meta_tags title: t('.title')
-    @redirect_url = 'view/report?op=view&viewlet=FR/Finance/SummaryOfOperation_Shanghai.cpt&ref_t=design'
+    @redirect_url = 'view/report?op=write&viewlet=FR/Finance/SummaryOfOperation_Shanghai.cpt&ref_t=design'
     @hide_app_footer = true
     @hide_main_header_wrapper = true
     @hide_scroll = true
