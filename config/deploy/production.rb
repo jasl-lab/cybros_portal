1cc57dc737c179008219d7e3ddf0993911c8ff62
Eric-Guo <eric.guocz@gmail.com>
2021-02-24 07:58:22 +0800

    Required by apistrano3-puma v5.0.3
    https://github.com/seuros/capistrano-puma/pull/307/files

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,6 +3,7 @@
 set :nginx_use_ssl, true
 set :branch, :thape
 set :puma_service_unit_name, :puma_prod
+set :puma_systemctl_user, :system
 set :sidekiq_service_unit_name, 'sidekiq_prod'
 set :sidekiq_service_unit_user, :system
 
