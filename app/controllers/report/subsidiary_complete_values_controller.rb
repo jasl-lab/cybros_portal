510ecbf15ba777f4ff286d2bf8fb61d6c37766de
Eric-Guo <eric.guocz@gmail.com>
2021-02-25 10:55:41 +0800

    No use logic.

--- app/controllers/report/subsidiary_complete_values_controller.rb
+++ app/controllers/report/subsidiary_complete_values_controller.rb
@@ -70,14 +70,8 @@ class Report::SubsidiaryCompleteValuesController < Report::BaseController
 
     @complete_value_year_totals = @complete_value_totals.collect { |d| (d / (@end_of_month.month / 12.0)).round(0) }
     complete_value_year_totals_deptcode_sum = complete_value_totals_deptcode_sum.collect { |d| (d / (@end_of_month.month / 12.0)).round(0) }
-    @fix_sum_complete_value_year_totals = if orgcode == '000101' && @end_of_month.month == 12
-      sum_total_record = policy_scope(Bi::CompleteValueDept, :group_resolve).where(orgcode: orgcode)
-        .where(month: @end_of_month.beginning_of_year..@end_of_month).where(date: last_available_date)
-        .select('SUM(IFNULL(total,0)) sum_total').first
-      (sum_total_record.sum_total / 10000.0).round(1)
-    else
-      complete_value_year_totals_deptcode_sum.sum.round(1)
-    end
+
+    @fix_sum_complete_value_year_totals = complete_value_year_totals_deptcode_sum.sum.round(1)
     @complete_value_year_remains = @complete_value_year_totals.zip(@complete_value_totals).map { |d| d[0] - d[1] }
 
     worker_per_dept_code_by_year = if orgcode == '000101' && @end_of_month.year <= 2020 && @end_of_month.month < 5
