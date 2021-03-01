3b514a4700fc75b4f4a0bac7b0543885e3492e76
Eric-Guo <eric.guocz@gmail.com>
2021-03-01 10:37:16 +0800

    gem and yarn upgrade

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 # config valid for current version and patch releases of Capistrano
-lock '~> 3.15.0'
+lock '~> 3.16.0'
 
 set :application, 'cybros'
 set :repo_url, 'git@e.coding.net:sew/cybros_portal.git'
