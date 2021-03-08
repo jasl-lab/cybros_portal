143de90fcc59015f6e52f8f895912ba5efb410e8
Eric-Guo <eric.guocz@gmail.com>
2021-03-08 13:07:29 +0800

    Allow CW_上海所级管理者 to see the reports.

--- app/policies/bi/departmental_market_fees_policy.rb
+++ app/policies/bi/departmental_market_fees_policy.rb
@@ -7,7 +7,7 @@ module Bi
 
 
       user.chinese_name.in?(%w(郭颖杰 余慧 陆文娟 周聪睿 蒋磊 吴悠 毕赢 孙涛 陈一恺 陈玲)) ||
-        user.roles.pluck(:role_name).any? { |r| r.in?(%w[CW_财务分析部管理员]) } ||
+        user.roles.pluck(:role_name).any? { |r| r.in?(%w[CW_财务分析部管理员 CW_上海所级管理者]) } ||
         user.admin?
     end
   end
