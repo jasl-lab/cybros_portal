bf142f650009029b0b9d0ad01df816c3ca89db62
qinjiapeng <759848795@qq.com>
2021-03-08 18:18:47 +0800

    新增项目地图搜索项: 规模范围、业务类型、项目类型、服务类型，修改搜索项配置

--- app/controllers/api/project_maps_controller.rb
+++ app/controllers/api/project_maps_controller.rb
@@ -4,36 +4,60 @@ module API
   class ProjectMapsController < WechatMiniBaseController
     before_action :authenticate_wechat_user!
     before_action :make_sure_auth
-
-    def show
-      city = params[:city].presence || '所有'
-      province = params[:province].presence || '所有'
-      client = params[:client].presence
-
-      trace_state = if params[:trace_state].presence.blank?
-        %w[跟踪中 跟踪成功 跟踪失败]
-      else
+    before_action only: [:show, :list] do
+      is_commercial = policy(Bi::NewMapInfo).show?
+      province = params[:province].presence && params[:province].strip
+      city = params[:city].presence && params[:city].strip
+      client = params[:client].presence && params[:client].strip
+      trace_state = params[:trace_state].presence && params[:trace_state].strip
+      trace_state = if is_commercial && trace_state.blank?
+        all_tracestates
+      elsif is_commercial && trace_state.present?
         params[:trace_state]
+      else
+        '跟踪成功'
       end
-      year = params[:year].presence || '所有'
-      project_type = params[:project_type].presence
-      keywords = params[:keywords].presence
+      year = params[:year].presence && params[:year].strip
+      default_business_type = all_business_types.collect{|item| item[:value]}.uniq
+      default_project_type = all_business_types.collect{|item| item[:project_types]}.flatten.collect{|item| item[:value]}.uniq
+      default_service_stage = all_business_types.collect{|item| item[:project_types]}.flatten.collect{|item| item[:service_stages]}.flatten.uniq
+      business_type = params[:business_type].presence && params[:business_type].strip || default_business_type.join('|')
+      project_type = params[:project_type].presence && params[:project_type].strip || default_project_type.join('|')
+      service_stage = params[:service_stage].presence && params[:service_stage].strip || default_service_stage.join('|')
+      company = params[:company].presence && params[:company].strip
+      department = params[:department].presence && params[:department].strip
+      scales = params[:scales].presence && params[:scales].strip
+      keywords = params[:keywords].presence && params[:keywords].strip
 
       map_infos = Bi::NewMapInfo.where.not(coordinate: nil).includes(:project_items)
-      map_infos = map_infos.where(tracestate: trace_state)
-      map_infos = map_infos.where('YEAR(CREATEDDATE) = ?', year) unless year == '所有'
-      map_infos = map_infos.where('company LIKE ?', "%#{city}%") unless city == '所有'
-      map_infos = map_infos.where('province LIKE ?', "%#{province}%") unless province == '所有'
-      map_infos = map_infos.where('projecttype LIKE ?', "%#{project_type}%") if project_type.present?
-      map_infos = map_infos.where('developercompanyname LIKE ?', "%#{client}%") if client.present?
       map_infos = map_infos.where('instr(coordinate, ?) > 0', ',')
+      if province.present?
+        map_infos = map_infos.where('province LIKE ?', "%#{province}%")
+        map_infos = map_infos.where('company LIKE ?', "%#{city}%") if city.present?
+      end
+      map_infos = map_infos.where('projecttype REGEXP ?', "(^(#{business_type})-(#{project_type})-(#{service_stage}))|(,(#{business_type})-(#{project_type})-(#{service_stage}))")
+      map_infos = map_infos.where('developercompanyname LIKE ?', "%#{client}%") if client.present?
+      if scales.present? && scales.match(/^\d+(\.\d*)?,\d+(\.\d*)?$/)
+        scales = scales.split(',')
+        map_infos = map_infos.where(scalearea: scales[0].to_f..scales[1].to_f)
+      end
+      if company.present?
+        map_infos = map_infos.where('maindeptname REGEXP ?', "#{company}-?#{department || '.+'}")
+      end
+      map_infos = map_infos.where(tracestate: trace_state)
+      if is_commercial
+        map_infos = map_infos.where('YEAR(CREATEDDATE) = ?', year) if is_commercial && year.present?
+      end
       if keywords.present?
         map_infos = map_infos
           .where('marketinfoname LIKE ? OR projectframename LIKE ? OR ID LIKE ?',
             "%#{keywords}%", "%#{keywords}%", "%#{keywords}%")
       end
+      @map_infos = map_infos
+    end
 
-      @valid_map_points = map_infos.collect do |m|
+    def show
+      @valid_map_points = @map_infos.collect do |m|
         lat = m.coordinate.split(',')[1].to_f
         if lat >= 85.051128 || lat <= -85.051128
           Rails.logger.error "coordinate lat error: #{m.id} #{m.marketinfoname} #{m.coordinate}"
@@ -58,33 +82,7 @@ module API
     end
 
     def list
-      city = params[:city].presence || '所有'
-      province = params[:province].presence || '所有'
-      client = params[:client].presence
-
-      trace_state = if params[:trace_state].presence.blank?
-        %w[跟踪中 跟踪成功 跟踪失败]
-      else
-        params[:trace_state]
-      end
-      year = params[:year].presence || '所有'
-      project_type = params[:project_type].presence
-      keywords = params[:keywords].presence
-
-      map_infos = policy_scope(Bi::NewMapInfo).where.not(coordinate: nil).includes(:project_items)
-      map_infos = map_infos.where(tracestate: trace_state)
-      map_infos = map_infos.where('YEAR(CREATEDDATE) = ?', year) unless year == '所有'
-      map_infos = map_infos.where('company LIKE ?', "%#{city}%") unless city == '所有'
-      map_infos = map_infos.where('province LIKE ?', "%#{province}%") unless province == '所有'
-      map_infos = map_infos.where('projecttype LIKE ?', "%#{project_type}%") if project_type.present?
-      map_infos = map_infos.where('developercompanyname LIKE ?', "%#{client}%") if client.present?
-      map_infos = map_infos.where('instr(coordinate, ?) > 0', ',')
-      if keywords.present?
-        map_infos = map_infos
-          .where('marketinfoname LIKE ? OR projectframename LIKE ? OR ID LIKE ?',
-            "%#{keywords}%", "%#{keywords}%", "%#{keywords}%")
-      end
-      @list = map_infos.page(params[:page]).per(params[:per_page]).collect do |m|
+      @list = @map_infos.page(params[:page]).per(params[:per_page]).collect do |m|
         lat = m.coordinate.split(',')[1].to_f
         if lat >= 85.051128 || lat <= -85.051128
           Rails.logger.error "coordinate lat error: #{m.id} #{m.marketinfoname} #{m.coordinate}"
@@ -107,16 +105,19 @@ module API
           business_type_deptnames: business_type_deptnames }
       end
 
-      @total = map_infos.count
+      @total = @map_infos.count
     end
 
     def query_config
-      @tracestates = Bi::NewMapInfo.all_tracestates
-      @tracestates = @tracestates.map { |item| item }
-      @tracestates.shift
-      @createddate_years = Bi::NewMapInfo.all_createddate_year
-      @createddate_years = @createddate_years.map { |item| item }
-      @createddate_years.shift
+      if policy(Bi::NewMapInfo).show?
+        @tracestates = all_tracestates
+        @createddate_years = Bi::NewMapInfo.all_createddate_year
+        @createddate_years = @createddate_years.map { |item| item }
+        @createddate_years.shift
+      end
+      @business_types = all_business_types
+      @project_processes = all_project_processes
+      @companies = all_companies
     end
 
     def project
@@ -134,5 +135,103 @@ module API
     def project_contract
       @sc = Bi::SaContract.find_by salescontractid: params[:id]
     end
+
+    private
+      def all_business_types
+        [
+          {
+            value: '土建',
+            project_types: [
+              {
+                value: '公建',
+                service_stages: ['方案', '施工图', '咨询', '标准化'],
+              },
+              {
+                value: '住宅',
+                service_stages: ['方案', '施工图', '咨询', '标准化'],
+              },
+              {
+                value: '工业',
+                service_stages: ['方案', '施工图', '咨询', '标准化'],
+              },
+            ]
+          },
+          {
+            value: '规划',
+            project_types: [
+              {
+                value: '法定规划',
+                service_stages: ['方案'],
+              },
+              {
+                value: '非法定规划',
+                service_stages: ['方案'],
+              },
+            ]
+          },
+          {
+            value: '室内',
+            project_types: [
+              {
+                value: '住宅公区',
+                service_stages: ['方案', '施工图', '咨询'],
+              },
+              {
+                value: '商办公区',
+                service_stages: ['方案', '施工图', '咨询'],
+              },
+              {
+                value: '酒店',
+                service_stages: ['方案', '施工图', '咨询'],
+              },
+              {
+                value: '精装修',
+                service_stages: ['方案', '施工图', '咨询'],
+              },
+              {
+                value: '租赁办公',
+                service_stages: ['方案', '咨询'],
+              },
+              {
+                value: '租赁商业',
+                service_stages: ['方案', '咨询'],
+              },
+            ]
+          },
+          {
+            value: '景观',
+            project_types: [
+              {
+                value: '住宅',
+                service_stages: ['方案', '咨询'],
+              },
+              {
+                value: '公建',
+                service_stages: ['方案', '咨询'],
+              },
+              {
+                value: '市政',
+                service_stages: ['方案', '咨询'],
+              },
+            ]
+          },
+        ]
+      end
+
+      def all_project_processes
+        ['土建-前端-方案报批通过、竣工验收', '土建-后端-审图通过、竣工验收']
+      end
+      def all_companies
+        companies = Bi::OrgReportDeptOrder.where('是否显示 = 1').order(组织: :asc, 显示顺序: :asc).all
+        companies.collect do |company|
+          {
+            company: company.组织,
+            department: company.部门,
+          }
+        end
+      end
+      def all_tracestates
+        %w[跟踪中 跟踪成功 仅投标拿地协议 跟踪失败]
+      end
   end
 end
--- app/views/api/project_maps/query_config.json.jbuilder
+++ app/views/api/project_maps/query_config.json.jbuilder
@@ -1,4 +1,16 @@
 # frozen_string_literal: true
-
-json.traceStates @tracestates
-json.years @createddate_years
+if @tracestates.present?
+  json.traceStates @tracestates
+end
+if @createddate_years.present?
+  json.years @createddate_years
+end
+json.businessTypes @business_types do |business_type|
+  json.value business_type[:value]
+  json.projectTypes business_type[:project_types] do |project_type|
+    json.value project_type[:value]
+    json.serviceStages project_type[:service_stages]
+  end
+end
+json.projectProcesses @project_processes
+json.companies @companies
