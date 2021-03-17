85044c54c21e0f9ed23d9a676ac9dd388f258df8
Eric-Guo <eric.guocz@gmail.com>
2021-03-17 15:33:09 +0800

    Review all 财务分析-所级管理表 again.

--- app/policies/bi/org_market_fees_policy.rb
+++ app/policies/bi/org_market_fees_policy.rb
@@ -5,9 +5,9 @@ module Bi
     def show?
       return false unless user.present?
 
-      user.chinese_name.in?(%w[章静 聂琳]) ||
-        user.roles.pluck(:role_name).any? { |r| r.in?(%w[CW_财务分析部管理员]) } ||
-        user.admin?
+      user.chinese_name.in?(%w[章静 聂琳]) || \
+        user.admin? || \
+        user.roles.pluck(:role_name).any? { |r| r.in?(%w[CW_财务分析部管理员]) }
     end
   end
 end
