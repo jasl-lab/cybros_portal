b63475954d9f390441f6fc4bf8396c13979fa37f
Eric-Guo <eric.guocz@gmail.com>
2021-03-23 14:39:35 +0800

    Add missing policy files.

---
+++ app/policies/capital/summary_fund_sum_policy.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+module Capital
+  class SummaryFundSumPolicy < Struct.new(:user, :dashboard)
+    def show?
+      return false unless user.present?
+
+      user.admin? || \
+        user.roles.pluck(:role_name).any? { |r| r.in?(%w[CW_财务管理员]) }
+    end
+  end
+end
