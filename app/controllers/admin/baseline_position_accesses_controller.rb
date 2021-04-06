728653be935a337f878f128aa06f8f719e04d05b
Eric-Guo <eric.guocz@gmail.com>
2021-04-06 17:15:22 +0800

    Allow modify the baseline_position_accesses in index pages.

--- app/controllers/admin/baseline_position_accesses_controller.rb
+++ app/controllers/admin/baseline_position_accesses_controller.rb
@@ -1,8 +1,31 @@
 # frozen_string_literal: true
 
 class Admin::BaselinePositionAccessesController < Admin::ApplicationController
+  before_action :set_baseline_position_access, except: %i[index]
+
   def index
     prepare_meta_tags title: t('.title')
     @baseline_position_accesses = BaselinePositionAccess.all
   end
+
+  def edit
+  end
+
+  def show
+    render :update
+  end
+
+  def update
+    @baseline_position_access.update(baseline_position_access_params)
+  end
+
+  private
+
+    def set_baseline_position_access
+      @baseline_position_access = BaselinePositionAccess.find(params[:id])
+    end
+
+    def baseline_position_access_params
+      params.fetch(:baseline_position_access, {}).permit(:contract_map_access)
+    end
 end
