d4db173be3c3bf50e499cf6a801649dc6a12d7bf
Eric-Guo <eric.guocz@gmail.com>
2021-04-06 16:40:11 +0800

    Display the access in index pages.

--- app/controllers/admin/baseline_position_accesses_controller.rb
+++ app/controllers/admin/baseline_position_accesses_controller.rb
@@ -3,5 +3,6 @@
 class Admin::BaselinePositionAccessesController < Admin::ApplicationController
   def index
     prepare_meta_tags title: t('.title')
+    @baseline_position_accesses = BaselinePositionAccess.all
   end
 end
