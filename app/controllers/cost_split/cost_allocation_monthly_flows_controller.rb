25a5671c3dd985d564a984da2604f76c6d0b4e44
Eric-Guo <eric.guocz@gmail.com>
2021-04-09 11:28:02 +0800

    Enable the month selects.

--- app/controllers/cost_split/cost_allocation_monthly_flows_controller.rb
+++ app/controllers/cost_split/cost_allocation_monthly_flows_controller.rb
@@ -8,5 +8,11 @@ class CostSplit::CostAllocationMonthlyFlowsController < CostSplit::BaseControlle
     @all_month_names = policy_scope(SplitCost::UserSplitCostDetail).all_month_names
     @month_name = params[:month_name]&.strip || @all_month_names.first
     beginning_of_month = Date.parse(@month_name).beginning_of_month
+
+    @split_cost_item_details = SplitCost::SplitCostItemDetail
+      .where(month: beginning_of_month)
+      .order(:to_split_company_code)
+      .group(:to_split_company_code)
+      .select('to_split_company_code, SUM(IFNULL(group_cost,0)) group_cost, SUM(IFNULL(shanghai_area_cost,0)) shanghai_area_cost, SUM(IFNULL(shanghai_hq_cost,0)) shanghai_hq_cost')
   end
 end
