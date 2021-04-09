2f1b1f505a81cf3fb3f0ab4f460baccb35f398d9
Eric-Guo <eric.guocz@gmail.com>
2021-04-09 16:42:22 +0800

    Add relationshop for positions and baseline position access.

--- app/models/baseline_position_access.rb
+++ app/models/baseline_position_access.rb
@@ -2,4 +2,5 @@
 
 class BaselinePositionAccess < ApplicationRecord
   enum contract_map_access: %i[no_access project_detail_with_download view_project_details view_project_only]
+  has_many :positions, foreign_key: :b_postcode, primary_key: :b_postcode
 end
