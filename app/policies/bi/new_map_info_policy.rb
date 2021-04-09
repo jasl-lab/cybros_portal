eb3f08add4cb2b11ec7b7f3731de92c670119dba
qinjiapeng <759848795@qq.com>
2021-04-09 17:39:06 +0800

    增加项目地图基准岗位权限配置，取消根据职位判断下载权限

--- app/policies/bi/new_map_info_policy.rb
+++ app/policies/bi/new_map_info_policy.rb
@@ -2,17 +2,14 @@
 
 module Bi
   class NewMapInfoPolicy < BasePolicy
-    ALLOW_SHOW_TITLES = %w[商务经理 管理副所长 所长助理 所长 市场总监 总经理助理 副总经理 总经理 运营分析经理].freeze
-    ALLOW_DOWNLOAD_HQ_TITLES = %w[管理副所长 所长助理 所长 市场总监 总经理助理 副总经理 总经理 运营分析经理].freeze
-    ALLOW_DOWNLOAD_SUBSIDIARY_TITLES = %w[总经理助理 副总经理 总经理].freeze
 
     class Scope < Scope
       def resolve
         return scope.none unless user.present?
 
         if user.admin? || \
-           ALLOW_SHOW_TITLES.any? { |title| user.position_title.include?(title) } || \
-           user.roles.pluck(:project_map_viewer).any?
+            user.positions.any? { |position| position.baseline_position_access.contract_map_access.in?(%w[project_detail_with_download view_project_details]) } || \
+            user.roles.pluck(:project_map_viewer).any?
           scope.all
         else
           scope.none
@@ -24,7 +21,7 @@ module Bi
       return false unless user.present?
 
       user.admin? || \
-        ALLOW_SHOW_TITLES.any? { |title| user.position_title.include?(title) } || \
+        user.positions.any? { |position| position.baseline_position_access.contract_map_access.in?(%w[project_detail_with_download view_project_details]) } || \
         user.roles.pluck(:project_map_viewer).any?
     end
 
@@ -32,14 +29,16 @@ module Bi
       return false unless user.present?
 
       user.admin? || \
-        ALLOW_SHOW_TITLES.any? { |title| user.position_title.include?(title) } || \
+        user.positions.any? { |position| position.baseline_position_access.contract_map_access.in?(%w[project_detail_with_download view_project_details]) } || \
         user.roles.pluck(:project_map_viewer).any?
     end
 
     def allow_download?
-      user.admin? || (user.in_tianhua_hq? && ALLOW_DOWNLOAD_HQ_TITLES.any? { |title| user.position_title.include?(title) }) \
-        || (!user.in_tianhua_hq? && ALLOW_DOWNLOAD_SUBSIDIARY_TITLES.any? { |title| user.position_title.include?(title) }) \
-        || user.roles.pluck(:project_map_contract_download).any?
+      return false unless user.present?
+
+      user.admin? || \
+        user.positions.any? { |position| position.baseline_position_access.contract_map_access.in?(%w[project_detail_with_download]) } || \
+        user.roles.pluck(:project_map_contract_download).any?
     end
   end
 end
