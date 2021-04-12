56a99b1cd320a620b8f72460b878e03a8fb1b10d
qinjiapeng <759848795@qq.com>
2021-04-12 15:46:43 +0800

    修改roles users_auto

--- app/controllers/admin/roles_controller.rb
+++ app/controllers/admin/roles_controller.rb
@@ -16,12 +16,11 @@ class Admin::RolesController < Admin::ApplicationController
     @users = @role.users.includes(:departments)
     @users_auto = case @role.id
                   when 6  # 查看项目地图并允许下载合同
-                    User.joins(:departments).where(departments: { company_name: '上海天华建筑设计有限公司' })
-                      .where(position_title: Bi::NewMapInfoPolicy::ALLOW_DOWNLOAD_HQ_TITLES)
-                        .or(User.joins(:departments).where.not(departments: { company_name: '上海天华建筑设计有限公司' })
-                      .where(position_title: Bi::NewMapInfoPolicy::ALLOW_DOWNLOAD_SUBSIDIARY_TITLES))
-                  when 30 # 查看项目地图
-                    User.where(position_title: Bi::NewMapInfoPolicy::ALLOW_SHOW_TITLES)
+                    position_ids = Position.joins(:baseline_position_access).where(baseline_position_access: { contract_map_access: 1 }).pluck(:id)
+                    User.joins(:position_users).where(position_users: { position_id: position_ids }).distinct
+                  when 30 # 查看项目地图与合同信息
+                    position_ids = Position.joins(:baseline_position_access).where(baseline_position_access: { contract_map_access: [1, 2] }).pluck(:id)
+                    User.joins(:position_users).where(position_users: { position_id: position_ids }).distinct
                   else
                     []
     end
