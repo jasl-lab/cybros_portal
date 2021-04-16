b538ce1d0b73ed99558456fc1978d0c4a967c32f
Eric-Guo <eric.guocz@gmail.com>
2021-04-16 09:19:16 +0800

    Show the real content.

--- app/controllers/admin/received_sms_messages_controller.rb
+++ app/controllers/admin/received_sms_messages_controller.rb
@@ -4,6 +4,7 @@ module Admin
   class ReceivedSmsMessagesController < ApplicationController
     def index
       prepare_meta_tags title: t('.title')
+      @received_sms_messages = Company::ReceivedSmsMessage.all
     end
   end
 end
