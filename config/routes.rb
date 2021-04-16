bbe617bc2c19152f93a59ae1a97f14672d5b6d24
Eric-Guo <eric.guocz@gmail.com>
2021-04-16 09:00:23 +0800

    Move admin routes to separate files.

--- config/routes.rb
+++ config/routes.rb
@@ -41,44 +41,9 @@ Rails.application.routes.draw do
     end
   end
 
-  namespace :admin do
-    root to: 'home#index'
-
-    resources :users, except: %i[destroy] do
-      resources :manual_operation_access_codes, only: %i[destroy create]
-      resources :manual_hr_access_codes, only: %i[destroy create]
-      resources :manual_cw_access_codes, only: %i[destroy create]
-      resources :manual_pts_access_codes, only: %i[destroy create]
-      resources :manual_pt_special_access_codes, only: %i[destroy create]
-      collection do
-        get :operation_org_code_change
-        get :hr_org_code_change
-        get :cw_org_code_change
-      end
-      member do
-        patch :lock
-        patch :unlock
-        patch :resend_confirmation_mail
-        patch :resend_invitation_mail
-        patch :login_as
-      end
-    end
-
-    resources :roles, only: %i[index show update] do
-      member do
-        delete :user
-      end
-    end
-
-    resources :manual_operation_access_codes, only: %i[index destroy]
-    resources :baseline_position_accesses, only: %i[index edit update show]
-    resource :bi_dept_structures, only: %i[show]
-    resource :bi_view_histories, only: %i[show]
-  end
-
-  draw :routes_company
-
-  draw :routes_report
+  draw :admin
+  draw :company
+  draw :report
 
   namespace :person do
     root to: 'home#index'
---
+++ config/routes/admin.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+
+namespace :admin do
+  root to: 'home#index'
+
+  resources :users, except: %i[destroy] do
+    resources :manual_operation_access_codes, only: %i[destroy create]
+    resources :manual_hr_access_codes, only: %i[destroy create]
+    resources :manual_cw_access_codes, only: %i[destroy create]
+    resources :manual_pts_access_codes, only: %i[destroy create]
+    resources :manual_pt_special_access_codes, only: %i[destroy create]
+    collection do
+      get :operation_org_code_change
+      get :hr_org_code_change
+      get :cw_org_code_change
+    end
+    member do
+      patch :lock
+      patch :unlock
+      patch :resend_confirmation_mail
+      patch :resend_invitation_mail
+      patch :login_as
+    end
+  end
+
+  resources :roles, only: %i[index show update] do
+    member do
+      delete :user
+    end
+  end
+
+  resources :manual_operation_access_codes, only: %i[index destroy]
+  resources :baseline_position_accesses, only: %i[index edit update show]
+  resource :bi_dept_structures, only: %i[show]
+  resource :bi_view_histories, only: %i[show]
+end
