a8075956c4157b48124ab6f5e3d743f85c673ec1
Eric-Guo <eric.guocz@gmail.com>
2021-04-26 10:14:37 +0800

    Structure of the sms_phone_configurations pages.

---
+++ app/controllers/company/sms_phone_configurations_controller.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+class Company::SmsPhoneConfigurationsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_page_layout_data, if: -> { request.format.html? }
+
+  def index
+    prepare_meta_tags title: t('.title')
+  end
+
+  protected
+
+    def set_page_layout_data
+      @_sidebar_name = 'company'
+    end
+end
