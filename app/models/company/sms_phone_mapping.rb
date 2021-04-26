fdcb8b99a77133553a68c15b255e3de85aef9d66
Eric-Guo <eric.guocz@gmail.com>
2021-04-26 16:03:48 +0800

    Make it valid.

--- app/models/company/sms_phone_mapping.rb
+++ app/models/company/sms_phone_mapping.rb
@@ -2,6 +2,8 @@
 
 module Company
   class SmsPhoneMapping < ApplicationRecord
+    validates_presence_of :receive_id
+    validates_uniqueness_of :receive_id
     belongs_to :user, optional: true
   end
 end
