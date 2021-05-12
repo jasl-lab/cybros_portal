f35ea82a58b0cdd28bc0c17e75f21d37da5dd88f
Eric-Guo <eric.guocz@gmail.com>
2021-05-12 15:21:32 +0800

    Revert "Go new productions"
    This reverts commit 13f6867e8c0831949d2e7098f4f05fd46abeafd0.

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -2,9 +2,9 @@
 
 set :nginx_use_ssl, true
 set :branch, :thape
-set :puma_service_unit_name, :puma_bi_prod
+set :puma_service_unit_name, :puma_prod
 set :puma_systemctl_user, :system
-set :sidekiq_service_unit_name, 'sidekiq_bi_prod'
+set :sidekiq_service_unit_name, 'sidekiq_prod'
 set :sidekiq_service_unit_user, :system
 
-server 'thape_vendor', user: 'cybros_bi', roles: %w{app db web}
+server 'thape_cybros', user: 'deployer', roles: %w{app db web}
