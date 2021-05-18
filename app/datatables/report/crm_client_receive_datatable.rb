db4f80feb7309c745b62a6405ee08bea713c260b
Eric-Guo <eric.guocz@gmail.com>
2021-05-18 17:33:16 +0800

    Don't relay on sum_receive_gt1_years, calculated using SUM.

--- app/datatables/report/crm_client_receive_datatable.rb
+++ app/datatables/report/crm_client_receive_datatable.rb
@@ -12,11 +12,11 @@ module Report
     def view_columns
       @view_columns ||= {
         crmthrank: { source: 'client_rank', searchable: false, orderable: true },
-        crmshort: { source: 'Bi::CrmClientReceive.crmshort', cond: :like, searchable: true, orderable: true },
+        crmshort: { source: 'Bi::CrmClientReceive.crmshort', searchable: false, orderable: true },
         cricrank: { source: 'Bi::CrmClientReceive.cricrank', searchable: false, orderable: true },
         clientproperty: { source: 'Bi::CrmClientReceive.clientproperty', cond: :string_eq, searchable: true, orderable: true },
-        sum_receive: { source: 'sum_receive', cond: :string_eq, searchable: true, orderable: true },
-        sum_receive_gt1_years: { source: 'sum_receive_gt1_years', cond: :like, searchable: true, orderable: true },
+        sum_receive: { source: 'sum_receive', searchable: false, orderable: true },
+        sum_receive_gt1_years: { source: nil, searchable: false, orderable: true },
 
         aging_amount_lt3_months: { source: 'aging_amount_lt3_months', orderable: true },
         aging_amount_4to12_months: { source: 'aging_amount_4to12_months', orderable: true },
@@ -33,7 +33,7 @@ module Report
           cricrank: r.cricrank,
           clientproperty: r.clientproperty,
           sum_receive: (r.sum_receive.to_f / 10000.0).round(0),
-          sum_receive_gt1_years: (r.sum_receive_gt1_years.to_f / 10000.0).round(0),
+          sum_receive_gt1_years: "#{((r.aging_amount_gt1_years.to_f/r.sum_receive.to_f)*100.0).round(0)} %",
 
           aging_amount_lt3_months: (r.aging_amount_lt3_months.to_f / 10000.0).round(0),
           aging_amount_4to12_months: (r.aging_amount_4to12_months.to_f / 10000.0).round(0),
@@ -48,7 +48,7 @@ module Report
       rr = @crm_client_receives
       rr = rr.where(orgcode_sum: @org_codes) if @org_codes.present?
       rr = rr.where('crmshort LIKE ?', "%#{@client_name}%") if @client_name.present?
-      rr.select('crmshort, cricrank, clientproperty, DENSE_RANK() OVER (ORDER BY SUM(sum_receive) DESC) client_rank, SUM(sum_receive) sum_receive, SUM(sum_receive_gt1_years) sum_receive_gt1_years, SUM(aging_amount_lt3_months) aging_amount_lt3_months, SUM(aging_amount_4to12_months) aging_amount_4to12_months, SUM(aging_amount_gt1_years) aging_amount_gt1_years, SUM(sign_receive) sign_receive, SUM(unsign_receive_gt1_years) unsign_receive_gt1_years, SUM(unsign_receive) unsign_receive')
+      rr.select('crmshort, cricrank, clientproperty, DENSE_RANK() OVER (ORDER BY SUM(sum_receive) DESC) client_rank, SUM(sum_receive) sum_receive, SUM(aging_amount_lt3_months) aging_amount_lt3_months, SUM(aging_amount_4to12_months) aging_amount_4to12_months, SUM(aging_amount_gt1_years) aging_amount_gt1_years, SUM(sign_receive) sign_receive, SUM(unsign_receive_gt1_years) unsign_receive_gt1_years, SUM(unsign_receive) unsign_receive')
         .group('crmshort, cricrank, clientproperty')
     end
   end
