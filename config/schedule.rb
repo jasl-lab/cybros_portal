ac4b39d79b5880f2ef1b2fd5d6674a95345d0d2c
Eric-Guo <eric.guocz@gmail.com>
2021-05-21 10:13:35 +0800

    Review crontab.

--- config/schedule.rb
+++ config/schedule.rb
@@ -5,29 +5,22 @@
 # It's helpful, but not entirely necessary to understand cron before proceeding.
 # http://en.wikipedia.org/wiki/Cron
 
-# Example:
-#
-# set :output, "/path/to/my/cron_log.log"
-#
-# every 2.hours do
-#   command "/usr/bin/some_great_command"
-#   runner "MyModel.some_method"
-#   rake "some:great:rake:task"
-# end
-#
-every 1.day, at: ['8:00', '10:00', '12:00', '14:00', '17:00'] do
+set :output, '/var/www/cybros_bi/shared/log/cron_log.log'
+
+# Time is UTC
+every 1.day, at: ['0:00', '2:00', '4:00', '6:00', '9:00'] do
   rake 'sync_yingjianke:sync_overrun_users'
 end
 
-every 1.day, at: '5:55' do
+every 1.day, at: '21:55' do
   command 'RAILS_ENV=production; cd /var/www/cybros/current/ && bundle exec pumactl -S /var/www/cybros/shared/tmp/pids/puma.state -F /var/www/cybros/shared/puma.rb restart'
 end
 
-every 1.day, at: ['0:49', '10:02', '12:02', '14:02', '17:02'] do
+every 1.day, at: ['16:49', '2:02', '4:02', '6:02', '9:02'] do
   rake 'role:all'
 end
 
-every 1.day, at: '12:50am' do
+every 1.day, at: '4:50' do
   rake 'sync_nc:all'
 end
 
