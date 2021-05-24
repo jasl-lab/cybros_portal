989f08b0e32d6a721ba3e6070cbe31741d79ab0f
Zero_Zeng <1154945484@qq.com>
2021-05-24 14:18:31 +0800

    add 曾思哲 to mergereport test permission

--- app/policies/account/mergereport_account_fill_policy.rb
+++ app/policies/account/mergereport_account_fill_policy.rb
@@ -5,7 +5,7 @@ module Account
     def show?
       user.present? &&
         (user.admin? || user.roles.pluck(:role_name).any? { |r| r.in?(%w[CW_财务分析部管理员]) } ||
-          user.chinese_name.in?(%w(郭颖杰 陆文娟 毕赢 王艺 郑贤来 李玉鹏 王旭煜)))
+          user.chinese_name.in?(%w(郭颖杰 陆文娟 毕赢 王艺 郑贤来 李玉鹏 王旭煜 曾思哲)))
     end
   end
 end
