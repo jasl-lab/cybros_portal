fc9b76b3957e027c232d6a399ec0f4649e336188
Eric-Guo <eric.guocz@gmail.com>
2021-05-25 17:02:10 +0800

    Support multi receiveIds

--- app/controllers/api/received_sms_messages_controller.rb
+++ app/controllers/api/received_sms_messages_controller.rb
@@ -18,10 +18,11 @@ module API
 
     def show
       return render json: { is_success: 403, error_message: '此 API 仅允许内网调用。' }, status: :forbidden unless request.remote_ip.start_with?('172.') || request.remote_ip.start_with?('10.') || request.remote_ip == '127.0.0.1'
-      receive_id = params[:receiveId]
-      sms_phone_mapping = Company::SmsPhoneMapping.find_by(receive_id: receive_id)
+      receive_ids = params[:receiveIds]&.split(',')
+
+      sms_phone_mapping = Company::SmsPhoneMapping.where(receive_id: receive_ids)
       if sms_phone_mapping.present?
-        sms_phone_mapping.touch
+        sms_phone_mapping.touch_all
         render json: { is_success: true }, status: :ok
       else
         render json: { is_success: false }, status: :bad_request
