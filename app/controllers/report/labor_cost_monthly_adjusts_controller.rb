0c4d38e7772ff58bcb8e1a442d84e897ee4491dc
Eric-Guo <eric.guocz@gmail.com>
2021-06-24 17:16:03 +0800

    Allow adjust the record from forms.

--- app/controllers/report/labor_cost_monthly_adjusts_controller.rb
+++ app/controllers/report/labor_cost_monthly_adjusts_controller.rb
@@ -37,6 +37,59 @@ class Report::LaborCostMonthlyAdjustsController < Report::BaseController
   end
 
   def create
+    month_name = params[:month_name]&.strip
+    beginning_of_month = Date.parse(month_name).beginning_of_month
+
+    chinese_name = params[:chinese_name]
+    out_company_code = params[:out_company_code]
+    out_department_code = params[:out_department_code]
+    out_position_code = params[:out_position_code]
+    out_user_cost_type_id = params[:out_user_cost_type_id]
+
+    in_company_code = params[:in_company_code]
+    in_department_code = params[:in_department_code]
+    in_position_code = params[:in_position_code]
+    in_user_cost_type_id = params[:in_user_cost_type_id]
+
+    adjustment_amount = params[:adjustment_amount].to_f
+    adjustment_reason = params[:adjustment_reason]
+
+    user = User.find_by(clerk_code: params[:clerk_code])
+    if user.blank? || user.chinese_name != chinese_name
+      return redirect_to report_labor_cost_monthly_adjust_path(month_name: month_name), notice: '用户姓名与工号不匹配，或者任意一个为空'
+    end
+
+    out_position = Position.find_by(id: out_position_code)
+    if out_position.blank?
+      return redirect_to report_labor_cost_monthly_adjust_path(month_name: month_name), notice: '调出岗位不存在，请检查'
+    end
+    out_position_user = PositionUser.find_by(position_id: out_position.id, user_id: user.id)
+    out_user_job_type_id = if out_position_user.blank?
+      PositionUser.find_by(position_id: out_position.id)&.user_job_type_id
+    else
+      out_position_user.user_job_type_id
+    end
+
+    in_position = Position.find_by(id: in_position_code)
+    if in_position.blank?
+      return redirect_to report_labor_cost_monthly_adjust_path(month_name: month_name), notice: '调入岗位不存在，请检查'
+    end
+    in_position_user = PositionUser.find_by(position_id: out_position.id, user_id: user.id)
+    in_user_job_type_id = if in_position_user.blank?
+      PositionUser.find_by(position_id: in_position.id)&.user_job_type_id
+    else
+      in_position_user.user_job_type_id
+    end
+
+    SplitCost::UserSplitClassifySalaryPerMonth.create(user_id: user.id, adjust_user_id: current_user.id,
+      position_id: out_position.id, month: beginning_of_month,
+      user_job_type_id: out_user_job_type_id, user_cost_type_id: out_user_cost_type_id,
+      amount: -adjustment_amount)
+    SplitCost::UserSplitClassifySalaryPerMonth.create(user_id: user.id, adjust_user_id: current_user.id,
+      position_id: in_position.id, month: beginning_of_month,
+      user_job_type_id: in_user_job_type_id, user_cost_type_id: in_user_cost_type_id,
+      amount: adjustment_amount)
+    redirect_to report_labor_cost_monthly_adjust_path(month_name: month_name), notice: '成功创建人力成本调整记录'
   end
 
   def out_company_code_change
