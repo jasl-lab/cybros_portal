a2d30716541274d062533682acccc48687bf3e6b
Eric-Guo <eric.guocz@gmail.com>
2021-06-24 09:54:14 +0800

    Fix syntax.

--- app/policies/account/mergereport_account_fill_policy.rb
+++ app/policies/account/mergereport_account_fill_policy.rb
@@ -3,11 +3,13 @@
 module Account
   class MergereportAccountFillPolicy < Struct.new(:user, :dashboard)
     def show?
-      user.present? &&
-        (user.admin? || user.roles.pluck(:role_name).any? { |r| r.in?(%w[CW_财务分析部管理员]) } 
-        	|| user.roles.pluck(:role_name).any? { |r| r.in?(%w[CW_子公司财务经理]) } ||
-        	user.roles.pluck(:role_name).any? { |r| r.in?(%w[CW_财务管理员]) } ||
-          user.chinese_name.in?(%w(郭颖杰 陆文娟 毕赢 王艺 郑贤来 李玉鹏 王旭煜 曾思哲)))
+      return false unless user.present?
+
+      user.admin? \
+        || user.roles.pluck(:role_name).any? { |r| r.in?(%w[CW_财务分析部管理员]) } \
+        || user.roles.pluck(:role_name).any? { |r| r.in?(%w[CW_子公司财务经理]) } \
+        || user.roles.pluck(:role_name).any? { |r| r.in?(%w[CW_财务管理员]) } \
+        || user.chinese_name.in?(%w(郭颖杰 陆文娟 毕赢 王艺 郑贤来 李玉鹏 王旭煜 曾思哲))
     end
   end
 end
