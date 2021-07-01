66dce11e5ac91a283e349b0bcbe9dbad3cb654ce
Eric-Guo <eric.guocz@gmail.com>
2021-07-01 17:29:39 +0800

    Fix position_title empty problems.

--- app/policies/bi/key_customer_detail_policy.rb
+++ app/policies/bi/key_customer_detail_policy.rb
@@ -7,8 +7,8 @@ module Bi
 
       user.admin? \
         || user.roles.pluck(:large_customer_detail_viewser).any? \
-        || ((user.user_company_names & Role::CREATIVE_SECTION).any? && user.position_title.include?('总经理') && !user.position_title.include?('总经理助理')) \
-        || (user.user_company_short_names.include?('上海天华') && (user.position_title.include?('管理副所长')))
+        || ((user.user_company_names & Role::CREATIVE_SECTION).any? && user&.position_title&.include?('总经理') && !user&.position_title&.include?('总经理助理')) \
+        || (user.user_company_short_names.include?('上海天华') && (user&.position_title&.include?('管理副所长')))
     end
   end
 end
